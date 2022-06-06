cd11090efee769c7c560c2d4b82b80bd47728332
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-06-06 10:53:29 +0200

    rewrote solution to 1651C.py

--- codeforces/1651C.py
+++ codeforces/1651C.py
@@ -10,21 +10,20 @@ def best_choice(nums, curr):
             pos = idx
     return pos
 
-
-
 for i in range(1, len(lines), 3):
-    best = float("inf")
+    best_seen = float("inf")
     n = int(lines[i])
-    a = list(map(int, lines[i+1].split(" ")))
-    b = list(map(int, lines[i+2].split(" ")))
-    candidates1 = [0, best_choice(b, a[0]), n-1]
-    candidates2 = [0, best_choice(b, a[n-1]), n-1]
-    for var1 in candidates1:
-        for var2 in candidates2:
-            ans = abs(a[0] - b[var1]) + abs(a[n-1] - b[var2])
-            if var1 > 0 and var2 > 0:
-                ans += abs(b[0] - a[best_choice(a, b[0])])
-            if var1 < n-1 and var2 < n-1:
-                ans += abs(b[n-1] - a[best_choice(a, b[n-1])])
-            best = min(best, ans)
-    print(best)
+    top = list(map(int, lines[i+1].split(" ")))
+    bottom = list(map(int, lines[i+2].split(" ")))
+    ways_to_connect_first = [0, best_choice(bottom, top[0]), n-1]
+    ways_to_connect_last = [0, best_choice(bottom, top[-1]), n-1]
+    for first_conn in ways_to_connect_first:
+        for last_conn in ways_to_connect_last:
+            # Connect a[0] and a[-1] and add the cost
+            curr = abs(top[0] - bottom[first_conn]) + abs(top[-1] - bottom[last_conn])
+            if first_conn > 0 and last_conn > 0: # b[0] isn't connected
+                curr += abs(bottom[0] - top[best_choice(top, bottom[0])]) # Add the cost of connecting b[0]
+            if first_conn < n-1 and last_conn < n-1: # b[-1] isn't connected
+                curr += abs(bottom[-1] - top[best_choice(top, bottom[-1])]) # Add the cost of connecting b[-1]
+            best_seen = min(best_seen, curr)
+    print(best_seen)
