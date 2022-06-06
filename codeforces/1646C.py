060818ab9a27ae88df3f78a93171649906c00f84
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-06-06 13:47:23 +0200

    fixed 1646C, wasn't including the factorials themselves and also not checking that n-total is positive

--- codeforces/1646C.py
+++ codeforces/1646C.py
@@ -14,8 +14,8 @@ for i in range(50):
     pows_of_two.add(1 << i)
 
 sums_of_factorials = set([(1, 1)])
-for factorial in factorials[1:]:
-    temp_set = set()
+for factorial in factorials:
+    temp_set = set([(factorial, 1)])
     for total, count in sums_of_factorials:
         if factorial + total not in pows_of_two:
             temp_set.add((factorial + total, count + 1))
@@ -25,7 +25,6 @@ for i in range(1, len(lines)):
     n = int(lines[i])
     smallest = ones(n)
     for total, count in sums_of_factorials:
-        smallest = min(smallest, ones(n-total) + count)
+        if n - total >= 0:
+            smallest = min(smallest, ones(n-total) + count)
     print(smallest)
-
-print(factorials)
