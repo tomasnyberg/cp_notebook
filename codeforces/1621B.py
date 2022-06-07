77cd9f92e5f244ce87d122053b505071f4a2f902
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-06-07 11:05:12 +0200

    fixed 1621B, was doing it in a terrible way

--- codeforces/1621B.py
+++ codeforces/1621B.py
@@ -3,24 +3,22 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 i = 1
 while i < len(lines):
     s = int(lines[i])
-    intervals = []
-    smallest = float("inf")
-    biggest = 0
-    cost_of_smallest = 0
-    cost_of_biggest = 0
+    smallest = [float("inf"), float("inf")] # smallest integer and its cost
+    biggest = [0, float("inf")] # biggest integer and its cost
+    longest = [0, float("inf")] # Longest segment and the cost of it
     for j in range(i+1, i+s+1):
         start, end, cost = map(int, lines[j].split(" "))
-        if start <= smallest:
-            if start < smallest:
-                cost_of_smallest = cost 
-                smallest = start
-            else:
-                cost_of_smallest = min(cost, cost_of_smallest) # Change
-        if end >= biggest:
-            if end > biggest:
-                biggest = end
-                cost_of_biggest = cost
-            else:
-                cost_of_biggest = min(cost_of_biggest, cost) # Change
-        print(cost_of_biggest + cost_of_smallest)
+        # Longest segment
+        if end - start > longest[0]: longest = [end-start, cost]
+        if end - start == longest[0]: longest[1] = min(cost, longest[1])
+        # Smallest integer
+        if start < smallest[0]: smallest = [start, cost]
+        if start == smallest[0]: smallest[1] = min(cost, smallest[1])
+        # Biggest
+        if end > biggest[0]: biggest = [end, cost]
+        if end == biggest[0]: biggest[1] = min(cost, biggest[1])
+        if longest[0] == biggest[0] - smallest[0]:
+            print(min(smallest[1] + biggest[1], longest[1]))
+        else:
+            print(smallest[1] + biggest[1])    
     i += s+1
