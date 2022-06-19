d12c3af4a4e5519e149eea992e75fab9be5ee98b
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-06-19 21:42:26 +0200

    veryy close just slightly too slow on test 147 (who even makes that many tests??)

--- codeforces/1690G.py
+++ codeforces/1690G.py
@@ -1,7 +1,6 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-# Need to add more locomotives
 def slow_down_trains(trains):
     result = [trains[0]]
     for i in range(1, len(trains)):
@@ -24,31 +23,22 @@ while i < len(lines):
     queries = []
     for j in range(i+2, i+2+to_skip):
         queries.append(list(map(int, lines[j].split(" "))))
-    #  find such maximal index j≤k in the set, if the value a_k<a_j, then we should add the value k to the set, since it will start a new train
-    # for k, diff in queries:
-    #     k-=1
-    #     biggest = -1
-    #     add = False
-    #     for j in locomotives:
-    #         if k >= j and locomotives[j] > real_speeds[k]:
-    #             add = True
-    #             biggest = max(j, biggest)
-    #     if add: locomotives[k] = real_speeds[k]
-    #     to_delete = []
-    #     for j in locomotives:
-    #         if j >= k and real_speeds[j] > real_speeds[k]:
-    #             to_delete.append(j)
-    #     for td in to_delete:
-    #         del locomotives[td]
-    #     print(len(locomotives), end=" ")
-    print(real_speeds)
-    print(max_speeds)
-    print(locomotives)
-    print()
-
-        
-
-        
-
+    for k, diff in queries:
+        k-=1
+        biggest = -1
+        max_speeds[k] -= diff
+        real_speeds[k] = min(max_speeds[k], real_speeds[k])
+        for j in locomotives:
+            if k >= j:
+                biggest = max(j, biggest)
+        if locomotives[biggest] > real_speeds[k]:
+            locomotives[k] = real_speeds[k]
+        to_delete = []
+        for j in locomotives:
+            if j > k and real_speeds[j] >= real_speeds[k]:
+                to_delete.append(j)
+        for td in to_delete:
+            del locomotives[td]
+        print(len(locomotives), end=" ")
     print()
     i += to_skip + 3
