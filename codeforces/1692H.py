a6e9a4cb2ba536f41ce8a1bfa14950c261aca124
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-06-19 14:50:14 +0200

    changed pretty much nothing but now it works????? no clue

--- codeforces/1692H.py
+++ codeforces/1692H.py
@@ -3,7 +3,6 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 
 for line in lines[2::2]:
     nums = list(map(int, line.split(" ")))
-    uniques = set(nums)
     best_unique = -1
     biggest_unique = -1
     best_started = -1
@@ -21,26 +20,10 @@ for line in lines[2::2]:
         to_del = []
         for x in consecutive:
             if x != num:
-                if consecutive[x][0] - 1 == 0:
+                if consecutive[x][0] == 1:
                     to_del.append(x)
                 else:
-                    consecutive[x][0] = max(consecutive[x][0] - 1, 0)
+                    consecutive[x][0] -= 1
         for td in to_del:
             del consecutive[td]
     print(best_unique, best_started+1, best_ended+1)
-    # Find the best unique, then we can do this
-    # biggest = 0
-    # biggest_dp = []
-    # dp = [1 if nums[0] == best_unique else -1]
-    # for i in range(1, len(nums)):
-    #     match = 1 if nums[i] == best_unique else -1
-    #     dp.append(max(dp[i-1] + match, match))
-    # if max(dp) > biggest:
-    #     biggest = max(dp)
-    #     biggest_dp = dp
-    # idx = biggest_dp.index(biggest)
-    # right = idx
-    # while idx > 0 and biggest_dp[idx-1] >= 0:
-    #     idx -= 1
-    # print(best_unique, idx+1, right+1) 
-    
