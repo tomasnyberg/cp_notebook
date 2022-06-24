d7a4d112df62125c803e436cae26f741932966e6
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-06-24 13:37:03 +0200

    solve B

--- codeforces/1669/B.py
+++ codeforces/1669/B.py
@@ -1,2 +1,16 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for i in range(1, len(lines), 2):
+    n = int(lines[i])
+    nums = list(map(int, lines[i+1].split(" ")))
+    count = {}
+    found = False
+    for num in nums:
+        count[num] = count[num] + 1 if num in count else 1
+        if count[num] == 3:
+            print(num)
+            found = True
+            break
+    if not found:
+        print(-1)
