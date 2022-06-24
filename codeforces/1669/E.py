83a9d8029a6f0e886807a0f048cf991685958f2f
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-06-24 16:19:48 +0200

    solve E

--- codeforces/1669/E.py
+++ codeforces/1669/E.py
@@ -15,20 +15,18 @@ while i < len(lines):
     for str in strs:
         if str in counts:
             result += counts[str]
-            print("matches for", str, counts[str])
+            # print("matches for", str, counts[str])
         c = str[0]
-        charc = ord(str[0])
+        charc = ord(str[1])
         for j in range(97, 123):
             if j != charc:
                 counts[c + chr(j)] = counts[c + chr(j)] + 1 if c + chr(j) in counts else 1
         c = str[1]
-        charc = ord(str[1])
+        charc = ord(str[0])
         for j in range(97, 123):
             if j != charc:
                 counts[chr(j) + c] = counts[chr(j) + c] + 1 if chr(j) + c in counts else 1
     
-    print(counts)
-    print()
-    # print(result)
-    print(strs)
-    break
+    # print()
+    print(result)
+    # print(strs)
