6af4704a55f069d973b5548f1e68d71cb0e3fd67
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-06-26 15:08:29 +0200

    even closer but not quite there

--- codeforces/1352/G.py
+++ codeforces/1352/G.py
@@ -3,24 +3,14 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 
 for line in lines[1:]:
     n = int(line)
-    # special = []
-    # for i in range(1, min(n+1, 5)):
-    #     special.append(i)
-    # for i in range(n, max(n-4, 0), -1):
-    #     special.append(i)
-    possible_matches = {i:[] for i in range(1, n+1)}
-    for x in possible_matches:
-        for i in range(max(1, x - 4), min(n+1, x + 5)):
-            if i != x and abs(i-x) >= 2:
-                possible_matches[x].append(i)
     taken = set()
     result = []
     curr = 1
     while len(taken) < n:
         result.append(curr)
         taken.add(curr)
-        for pos_match in possible_matches[curr]:
-            if pos_match not in taken:
-                curr = pos_match
+        for i in range(max(1, curr - 4), min(n+1, curr + 5)):
+            if i not in taken and i != curr and abs(i-curr) >= 2:
+                curr = i
                 break
     print(result) 
