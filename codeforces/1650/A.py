d7f2ee82891cc417fefe3b54e4c1484a573d3136
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-03 13:11:06 +0200

    solve a from 1650

--- codeforces/1650/A.py
+++ codeforces/1650/A.py
@@ -1,3 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+for i in range(1, len(lines), 2):
+    line = lines[i]
+    c = lines[i+1]
+    if c not in line:
+        print("NO")
+        continue
+    found = False
+    for idx, char in enumerate(line):
+        if char == c and idx % 2 == 0:
+            found = True
+            break
+    print("YES" if found else "NO")
