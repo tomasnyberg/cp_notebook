8738b715a65e7c27e32ddc5e343a80bf8df5f54c
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-03 15:26:40 +0200

    slight change to A 1650

--- codeforces/1650/A.py
+++ codeforces/1650/A.py
@@ -4,9 +4,6 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 for i in range(1, len(lines), 2):
     line = lines[i]
     c = lines[i+1]
-    if c not in line:
-        print("NO")
-        continue
     found = False
     for idx, char in enumerate(line):
         if char == c and idx % 2 == 0:
