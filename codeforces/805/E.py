56383f2c7bce2d67d0ab922b0b9ebcebdee3cc26
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-10 17:45:52 +0200

    working on E

--- codeforces/805/E.py
+++ codeforces/805/E.py
@@ -1,3 +1,58 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+def solve(dominoes, one, two): # call this recursively
+    undecided = []
+    for a, b in dominoes:
+        if a == b:
+            print("NO")
+            return
+        if len(one) == 0 and len(two) == 0:
+            one.add(a)
+            one.add(b)
+            continue
+        
+        if (a not in one and a not in two) and (b not in one and b not in two):
+            undecided.append([a,b])
+        
+        if a in one:
+            if a in two or b in two:
+                print("NO")
+                return
+            else:
+                two.add(a)
+                two.add(b)
+                continue
+        
+        if a in two:
+            if a in one or b in one:
+                print("NO")
+                return
+            else:
+                one.add(a)
+                one.add(b)
+
+    if len(undecided) == 0:
+        print("YES")
+        return
+    if len(undecided) == len(dominoes): # We need to somehow choose a better candidate
+        a, b = undecided[0]
+        undecided = undecided[1:]
+        one.add(a)
+        one.add(b)
+    solve(undecided, one, two)
+            
+
+i = 1
+while i < len(lines):
+    n = int(lines[i])
+    i+=1
+    dominoes = []
+    while n > 0:
+        a, b = map(int, lines[i].split(" "))
+        dominoes.append([a,b])
+        i+=1
+        n-=1
+    solve(dominoes, set(), set())
+    
+
