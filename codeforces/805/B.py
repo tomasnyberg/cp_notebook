fb2729602642954c9fc046bc559ba7b1cd6cc718
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-10 16:45:59 +0200

    805 solve B

--- codeforces/805/B.py
+++ codeforces/805/B.py
@@ -1,3 +1,21 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+for line in lines[1:]:
+    seen = set()
+    i = 0
+    result = 1
+    while i < len(line):
+        curr = line[i]
+        if line[i] in seen:
+            i += 1
+        else:
+            if len(seen) < 3:
+                seen.add(curr)
+                i+=1
+            else:
+                result += 1
+                seen.clear()
+                seen.add(curr)
+                i+=1
+    print(result)
