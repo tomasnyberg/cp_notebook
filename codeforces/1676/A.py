adac993dfe956ee3020a0469bbc078bf85984cd9
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-10 14:50:01 +0200

    solve a

--- codeforces/1676/A.py
+++ codeforces/1676/A.py
@@ -1,3 +1,10 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+for line in lines[1:]:
+    a = sum(map(int, line[:3]))
+    b = sum(map(int, line[3:]))
+    if a == b:
+        print("YES")
+    else:
+        print("NO")
