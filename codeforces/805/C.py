91fea8fe02273254b562a99973f487e29c857529
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-11 07:54:02 +0200

    something wrong with C solution, no clue what. some nefarious input

--- codeforces/805/C.py
+++ codeforces/805/C.py
@@ -1,15 +1,6 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-def solve(stops, start, end):
-    if start not in stops or end not in stops:
-        print("NO")
-        return
-    if stops[start][0] <= stops[end][-1]:
-        print("YES")
-    else:
-        print("NO")
-
 i = 2
 while i < len(lines):
     n, queries = map(int, lines[i].split(" "))
@@ -17,15 +8,22 @@ while i < len(lines):
     nums = list(map(int, lines[i].split(" ")))
     stops = {}
     for idx, x in enumerate(nums):
-        if x not in stops: stops[x] = []
-        stops[x].append(idx)
-    # print(nums)
-    # print(stops)
-    # print()
+        if x not in stops:
+            stops[x] = [idx]
+            continue
+        if len(stops[x]) == 2:
+            stops[x][1] = idx
+        else:
+            stops[x].append(idx)
     i+=1
     while queries > 0:
         start, end = map(int, lines[i].split(" "))
-        solve(stops, start, end)
+        if start not in stops or end not in stops:
+            print("NO")
+        elif stops[start][0] <= stops[end][-1]:
+            print("YES")
+        else:
+            print("NO")
         i +=1
         queries -= 1
     i+=1
