83e1cec061a4caca8cbc053c58d0f330d80f6395
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-11 20:52:34 +0200

    gave up on c looked at answer, was pretty eas

--- codeforces/1660/C.py
+++ codeforces/1660/C.py
@@ -2,39 +2,14 @@ import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 for line in lines[1:]:
-    result = 0
-    prev = 'ä'
-    streak_of_ones = {} 
-    streak = 1
-    # print(line)
-    for idx, c in enumerate(line):
-        # print(c, streak, streak_of_ones, end="  ->  ")
-        if c == prev:
-            streak+=1
-            streak_of_ones.clear()
-            # print(streak, streak_of_ones)
-            continue
-        if c != prev and streak == 1: # need to handle what we started on somehow
-            if c in streak_of_ones:
-                result += idx - streak_of_ones[c] - 1
-                streak = 2
-                streak_of_ones.clear()
-                # print(streak, streak_of_ones)
-                continue
-            streak_of_ones[c] = idx
-            prev = c
+    prev = [False]*26
+    m = 0
+    for c in line:
+        if prev[ord(c) -97]:
+            m += 2
+            prev = [False]*26
         else:
-            if streak % 2 == 1:
-                result += 1
-            streak = 1
-            prev = c
-            streak_of_ones = {c:idx}
-        # print(streak, streak_of_ones)
-    if streak_of_ones:
-        result += len(streak_of_ones)
-    elif streak % 2 == 1:
-        result += 1
-    print(result)
-    
+            prev[ord(c) - 97] = True
+    print(len(line) - m)
             
 
