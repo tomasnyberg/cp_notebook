9402dc00799f4ad74eb3a843f0f6ba3011f9b93b
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-12 18:12:51 +0200

    solve f

--- codeforces/806/F.py
+++ codeforces/806/F.py
@@ -1,3 +1,15 @@
 import sys
+import bisect
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+for line in lines[2::2]:
+    nums = list(map(int, line.split(" ")))
+    cands = []
+    for idx, num in enumerate(nums):
+        if idx + 1 > num:
+            cands.append(idx + 1)
+    result = 0 
+    for idx, num in enumerate(nums):
+        if idx + 1 > num:
+            result += bisect.bisect_left(cands, num)
+    print(result)
