765f998d77a9c708a012c2f4c7124633b55b20fd
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-12 16:42:12 +0200

    solve b i think

--- codeforces/806/B.py
+++ codeforces/806/B.py
@@ -1,3 +1,13 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+for line in lines[2::2]:
+    seen = set()
+    result = 0
+    for c in line:
+        if c in seen:
+            result +=1
+        else:
+            result += 2
+        seen.add(c)
+    print(result)
