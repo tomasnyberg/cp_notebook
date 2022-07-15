5e6686b8cc6d673dd7415a07e30b659fdc41bac5
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-15 19:45:11 +0200

    solve A

--- codeforces/1674/A.py
+++ codeforces/1674/A.py
@@ -1,3 +1,17 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+def solve(x, y):
+    for b in range(1, 101):
+        for a in range(1, 101):
+            if b**a > 100:
+                break
+            if x * (b**a) == y:
+                print(a, b)
+                return
+    print(0, 0)
+
+
+for line in lines[1:]:
+    x, y = map(int, line.split(" "))
+    solve(x, y)
