7e76a5e5d8522799be5408379c44d9232e73e621
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-16 09:13:54 +0200

    clean up solution for E

--- codeforces/1674/E.py
+++ codeforces/1674/E.py
@@ -2,28 +2,30 @@ import sys, math
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 walls = list(map(int, lines[1].split(" ")))
-sorted_walls = list(sorted(walls))
-# Just take two random
-best = math.ceil(sorted_walls[0]/2) + math.ceil(sorted_walls[1]/2)
-# Shoot alternating
+swalls = list(sorted(walls))
+
+# Case 1: Shoot at two different ones far away
+best = math.ceil(swalls[0]/2) + math.ceil(swalls[1]/2)
+
+# Case 2: Two walls with one inbetween, we can always reduce them by 2 per shot.
+for i in range(1, len(walls) - 1):
+    left = walls[i-1]
+    right = walls[i+1]
+    best= min(best, math.ceil((left + right)/2))
+
+# Case 3: Two adjacent walls
 for i in range(1, len(walls)):
-    a = walls[i-1]
-    b = walls[i]
-    x = max(a,b)
-    y = min(a,b)
-    if x >= 2*y:
+    x = max(walls[i-1], walls[i])
+    y = min(walls[i-1], walls[i])
+    if x >= y*2: # Just shoot x until it's gone, y will be destroyed in the process
         best = min(best, math.ceil(x/2))
         continue
-    shots = x-y
+    # Shoot x until they are equal
+    shots = x - y
     x -= shots*2
     y -= shots
+    # We can distribute 3 damange across x and y for every shot
     shots += math.ceil((x+y)/3)
-    best = min(shots, best)
+    best = min(best, shots)
 
-#Two apart by one
-for i in range(1, len(walls) - 1):
-    a = walls[i-1]
-    b = walls[i+1]
-    best = min(best, math.ceil((a+b)/2))
-    
 print(best)
