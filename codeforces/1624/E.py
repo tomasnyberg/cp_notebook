b4ef18aa364a8fde45196a9ac852f3af2be7444b
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-18 21:46:05 +0200

    close but wrong answer on test case 2461 test 2 :(((

--- codeforces/1624/E.py
+++ codeforces/1624/E.py
@@ -1,6 +1,8 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+
+
 def all_two_or_three(others):
     seen = {}
     for i in range(2, 4):
@@ -33,10 +35,25 @@ while a < len(lines):
         if not (i == 0 or (i >= 2 and len(dp[i-2]) > 0) or (i >= 3 and len(dp[i-3]) > 0)): continue
         if friend[i:i+2] in seen:
             dp[i].append(seen[friend[i:i+2]])
-        if i == len(dp) - 1: break # Skip len 3 ones
+        if i == len(dp) - 2:
+            break # Skip len 3 ones
         if friend[i:i+3] in seen:
             dp[i].append(seen[friend[i:i+3]])
-    print(seen[friend[0:2]])
-    print(others)
-    print(friend)
-    print(dp)
+    i = len(friend)
+    to_print = []
+    while i > 0:
+        if len(dp[i-2]) > 0 and dp[i-2][0][1] - dp[i-2][0][0] == 1:
+            to_print.append(dp[i-2][0])
+            i -= 2
+        elif i >= 3 and len(dp[i-3]) > 0 and dp[i-3][-1][1] - dp[i-3][-1][0] == 2:
+            to_print.append(dp[i-3][-1])
+            i -= 3
+        else:
+            break
+    if i == 0: # Might have to reverse to_print
+        print(len(to_print))
+        to_print.reverse()
+        for first, second, third in to_print:
+            print(first, second, third)
+    else:
+        print(-1)
