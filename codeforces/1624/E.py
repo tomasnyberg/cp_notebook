b7e1c1eedb702629f7791de9fdcd7431e43660ef
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-18 07:44:11 +0200

    1624 working on E, think I am making progress

---
+++ codeforces/1624/E.py
@@ -0,0 +1,36 @@
+import sys
+lines = list(map(str.strip, sys.stdin.readlines()))
+
+def all_two_or_three(others):
+    seen = {}
+    for i in range(2, 4):
+        for idx, other in enumerate(others):
+            left = 0
+            right = i
+            while right <= len(other):
+                curr = other[left:right]
+                seen[curr] = [left + 1, right, idx + 1]
+                left +=1
+                right +=1
+    return seen
+
+i = 2
+while i < len(lines):
+    n, l = map(int, lines[i].split(" "))
+    i+=1
+    others = []
+    while n >= 0:
+        others.append(lines[i])
+        n-=1
+        i+=1
+    i+=1
+    friend = others.pop()
+    seen = all_two_or_three(others)
+    dp = [False]*len(friend)
+    if friend[:2] in seen:
+        for j in range(0, 2): dp[j] = True
+    if friend[:3] in seen:
+        for j in range(0, 3): dp[j] = True
+    print(friend)
+    print(dp)
+    print(seen)
