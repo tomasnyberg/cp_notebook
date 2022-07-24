36b00d08e406d1f0be7ce4b95194e736a9a1e8c4
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-24 08:51:59 +0200

    working on 1706C, new approach cause the old one doesn't work

--- codeforces/1706C.py
+++ codeforces/1706C.py
@@ -12,20 +12,22 @@ for line in lines[2::2]:
         print(0)
         continue
     best = 1000000000000000000
-    # If we have an even len nums, then we can check both even and odd indexes
-    if len(nums) % 2 == 0:
-        cost = 0
-        for i in range(2, len(nums) - 1, 2):
-            leftcost = 0 if nums[i] > nums[i-1] else nums[i-1] - nums[i] + 1
-            rightcost = 0 if nums[i] > nums[i+1] else nums[i+1] - nums[i] + 1
-            cost += max(leftcost, rightcost)
-        best = cost
-    cost = 0
-    for i in range(1, len(nums) - 1, 2):
-        leftcost = 0 if nums[i] > nums[i-1] else nums[i-1] - nums[i] + 1
-        rightcost = 0 if nums[i] > nums[i+1] else nums[i+1] - nums[i] + 1
-        cost += max(leftcost, rightcost)
-    best = min(cost, best)
-    print(best)
-
-
+    # If we have an even length, then we can skip ahead once at some point
+    dp = [-1]*len(nums)
+    for i in range(1, len(nums) - 1):
+        leftneeded = 0 if nums[i] > nums[i-1] else nums[i-1] - nums[i] + 1
+        rightneeded = 0 if nums[i] > nums[i+1] else nums[i+1] - nums[i] + 1
+        dp[i] = max(leftneeded, rightneeded)
+    # If we have an odd length, we simply have to take all the odd indexes, otherwise we aren't cool
+    if len(nums) % 2 == 1:
+        res = 0
+        for i in range(1, len(nums)-1,2):
+            res += dp[i]
+        print(res)
+        continue
+    print(dp)
+    runningsum = 0
+    running = []
+    for i in range(0, len(nums)):
+        if i % 2 == 0 and i != 0:
+            runningsum += nums[i]
