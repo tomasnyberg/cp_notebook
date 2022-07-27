d4529e318136cd0205fc05eeea1d8bfb620cfaad
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-27 21:28:00 +0200

    getting closer on 1696C i think... just slightly off

--- codeforces/1696C.py
+++ codeforces/1696C.py
@@ -11,30 +11,42 @@ def compareflats(xs, ys):
     a = 0
     b = 0
     while a < len(xs) and b < len(ys):
+        # print(a, b, xs[a], ys[b])
         # If both of them hit an array, check like this
         if type(ys[b]) == type(ys) and type(xs[a]) == type(xs): # If b is at an array as well 
             # If they have an equal amount of elements, just move both ahead
             if ys[b][0] == xs[a][0]:
                 a += 1
                 b += 1
+                continue
             if ys[b][0] > xs[a][0]:
                 ys[b][0] -= xs[a][0]
                 a+=1
             else:
                 xs[a][0] -= ys[b][0]
                 b += 1
+            continue
         # xs is an array but ys isn't
         if type(xs[a]) == type(xs):
             if ys[b] != m: return False
             xs[a][0] -= 1
+            b+=1
             if xs[a][0] == 0:
                 a += 1
+            continue
         if type(ys[b]) == type(xs):
             if xs[a] != m: return False
-            xs[a][0] -= 1
-            if xs[a][0] == 0:
-                a += 1           
-                        
+            ys[b][0] -= 1
+            a+=1
+            if ys[b][0] == 0:
+                b += 1           
+            continue
+        #Neither of them are at an array
+        if xs[a] != ys[b]:
+            return False
+        a += 1
+        b += 1
+        
     return a == len(xs) and b == len(ys)
 
 for i in range(1, len(lines), 4):
@@ -43,6 +55,6 @@ for i in range(1, len(lines), 4):
     b_nums = list(map(int, lines[i+3].split(" ")))
     aflat = flattenamap(a_nums, m)
     bflat = flattenamap(b_nums, m)
-    print(aflat)
-    print(bflat)
+    # print(aflat)
+    # print(bflat)
     print("YES" if compareflats(aflat, bflat) else "NO")
