cb06f435ae2594f0e5f20cdc35a92e6501fa38c5
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-29 17:35:20 +0200

    clean up 1691C solution

--- codeforces/1691C.py
+++ codeforces/1691C.py
@@ -7,33 +7,22 @@ def f(s):
         total += int(s[i-2:i])
     return total
 
-def move_back_cost(s):
-    for i in range(len(s)-1 ,-1, -1):
-        if s[i] == '1':
-            return(len(s) - i - 1)
-
-def move_front_cost(s):
-    for i in range(0, len(s)):
-        if s[i] == '1':
-            return i
-
 for i in range(1, len(lines), 2):
     n, k = map(int, lines[i].split(" "))
     s = lines[i+1]
     if k == 0 or all(c == '1' for c in s) or all(c == '0' for c in s): # If we can't change the string, just print its score
         print(f(s))
         continue
-    need_to_move_to_back = s[-1] != '1'
-    need_to_move_to_front = s[0] != '1'
-    cost_to_move_to_back = move_back_cost(s)
-    cost_to_move_to_front = move_front_cost(s)
+    # We can see how many swaps is needed to swap something to the front and to the back with
+    # split[0] and split[-1] respectively.
+    split = s.split("1")
     score = f(s)
-    if need_to_move_to_back and cost_to_move_to_back <= k:
+    if s[-1] != '1' and len(split[-1]) <= k:
         if score == 10: # We have literally only one 1 in the string and it is at the front
             print(1)
             continue
         score -= 10
-        k-=cost_to_move_to_back
-    if need_to_move_to_front and cost_to_move_to_front <= k and score != 1:
+        k-= len(split[-1])
+    if s[0] != '1' and len(split[0]) <= k and score != 1: # Last check is needed to not take a 1 out of the perfect last spot
         score -= 1
     print(score)
