fd8dfbb88d786c36410901b862cc55ce20c0513d
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-29 16:53:01 +0200

    define function for calculating score, and simple case for k == 0

--- codeforces/1691C.py
+++ codeforces/1691C.py
@@ -1,2 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+def f(s):
+    total = 0
+    for i in range(2, len(s)+1):
+        total += int(s[i-2:i])
+    return total
+
+for i in range(1, len(lines), 2):
+    n, k = map(int, lines[i].split(" "))
+    s = lines[i+1]
+    if k == 0: # If we can't change the string, just print its score
+        print(f(s))
+        continue    
