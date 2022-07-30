2c80e6e3b6f3ddf2496e959809de4279b5933d60
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-07-30 11:20:15 +0200

    works but TLE since it is n^2, gonna think about how to make it faster

--- codeforces/1679C.py
+++ codeforces/1679C.py
@@ -1,24 +1,27 @@
-from ftplib import all_errors
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 n, q = map(int, lines[0].split(" "))
 
-colsattacked = set()
-rowsattacked = set()
+rowsattacked = {} # Which row is attacked : set of cols 
+colsattacked = {} 
 for i in range(1, len(lines)):
     query = list(map(int, lines[i].split(" ")))
-    print(rowsattacked)
-    print(colsattacked)
-    print()
+    # print(rowsattacked)
+    # print(colsattacked)
+    # print()
     if query[0] == 1 or query[0] == 2:
         x, y = query[1:]
         if query[0] == 1:
-            colsattacked.add(y)
-            rowsattacked.add(x)
+            if y not in colsattacked: colsattacked[y] = set()
+            colsattacked[y].add(x)
+            if x not in rowsattacked: rowsattacked[x] = set()
+            rowsattacked[x].add(y)
         else:
-            colsattacked.remove(y)
-            rowsattacked.remove(x)
+            colsattacked[y].remove(x)
+            if len(colsattacked[y]) == 0: del colsattacked[y]
+            rowsattacked[x].remove(y)
+            if len(rowsattacked[x]) == 0: del rowsattacked[x]
         continue
     bad = False
     x1, y1, x2, y2 = query[1:]
