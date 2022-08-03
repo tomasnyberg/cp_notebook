9fdc9dda5dc26bbafe956970c4583161711b1162
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-03 19:20:42 +0200

    new implementation of fenwick tree which i like a lot more

--- algorithms/fenwick_tree.py
+++ algorithms/fenwick_tree.py
@@ -1,62 +1,42 @@
-def update(index, value, array, bi_tree):
-	"""
-	Updates the binary indexed tree with the given value
-	:param index: index at which the update is to be made
-	:param value: the new element at the index
-	:param array: the input array
-	:param bi_tree: the array representation of the binary indexed tree
-	:return: void
-	"""
-
-	while index < len(array):
-		bi_tree[index] += value
-		index += index & -index
-
-
-def get_sum(index, bi_tree):
-	"""
-	Calculates the sum of the elements from the beginning to the index
-	:param index: index till which the sum is to be calculated
-	:param bi_tree: the array representation of the binary indexed tree
-	:return: (integer) sum of the elements from beginning till index
-	"""
-	ans = 0
-
-	while index > 0:
-		ans += bi_tree[index]
-		index -= index & -index
-
-	return ans
-
-
-def get_range_sum(left, right, bi_tree):
-	"""
-	Calculates the sum from the given range
+class fenwick_tree:
+	def __init__(self, values) -> None:
+		values.insert(0, 0)
+		n = len(values)
+		self.tree = values.copy()
+		for i in range(1, n):
+			parent = i + (i & -i)
+			if parent < n: self.tree[parent] += self.tree[i]
 	
-	:param bi_tree: the array representation of the binary indexed tree
-	:param left: left index of the range (1-indexed)
-	:param right: right index of the range (1-indexed)
-	:return: (integer) sum of the elements in the range
-	"""
-	ans = get_sum(right, bi_tree) - get_sum(left - 1, bi_tree)
+	def prefixSum(self, i):
+		total = 0
+		while i != 0:
+			total += self.tree[i]
+			i &= ~(i&-i)
+		return total	
+			
+	def range_sum(self, left, right):
+		assert left <= right, "left is not leq right"
+		return self.prefixSum(right) - self.prefixSum(left - 1)
 	
-	return ans
+	def get(self, i):
+		return self.range_sum(i, i)
+	
+	def add(self, i, val):
+		while i < len(self.tree):
+			self.tree[i] += val
+			i += i & -i
+	
+	def set(self, i, val):
+		self.add(i, val - self.range_sum(i, i))
 
-#Example usage
-n = 8
-arr = [1,2,3,4,5,6,7,8]
-arr.insert(0, 0)   # insert dummy node for 1-based indexing
-bit = [0 for i in range(n+1)]
+# examples
+#      1 2 3 4 5 6 7
+arr = [1,2,3,4,5,6,8]
+fwt = fenwick_tree(arr)
+print(fwt.range_sum(1, 7))
+fwt.set(1, 15)
+print(fwt.range_sum(1, 7))
+fwt.set(1, 10)
+print(fwt.range_sum(1, 7))
 
-for index in range(1, n+1):
-    update(index, arr[index], arr, bit)
-
-print(get_range_sum(1, 5, bit))
-"""
-For range sum queries
-    l, r = map(int, input('Enter the left and right indices for the range sum: ').split())
-    print(get_range_sum(l, r, bit))
-
-For updating the binary indexed tree
-    update(index, new_value - arr[index], arr, bit)	
-"""
+fwt.range_sum(5, 2)
