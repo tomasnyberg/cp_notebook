8beff0f2a1cc32e6bfc95bd218422786acf6b057
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-11 16:49:59 +0200

    clean up 1634B

--- codeforces/1634B.py
+++ codeforces/1634B.py
@@ -1,22 +1,13 @@
-import sys, math
+import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 def solve(x, target, nums):
-    parity = x % 2 # 0 or 1 obviously
+    parity = x % 2
     for num in nums:
-        par = num % 2
-        sum = parity + par
-        if sum == 0 or sum == 2:
-            parity = 0
-        else:
-            parity = 1
+        parity = (num%2)^parity
     return target % 2 == parity
 
 for i in range(1, len(lines), 2):
     n, x, target = map(int, lines[i].split(" "))
     nums = list(map(int, lines[i+1].split(" ")))
-    binnums = list(map(lambda x: bin(x)[2:], nums))
-    if solve(x, target, nums):
-        print("Alice")
-    else:
-        print("Bob")
+    print("Alice" if solve(x, target, nums) else "Bob")
