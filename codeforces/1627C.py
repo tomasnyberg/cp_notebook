c2c03d8150286e008be2cf0e2da1ede9427e2cae
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-12 09:58:22 +0200

    solve 1627C, not ac because recursion limit but it works

--- codeforces/1627C.py
+++ codeforces/1627C.py
@@ -1,15 +1,18 @@
-from operator import truediv
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+sys.setrecursionlimit(10000)
 i = 1
 while i < len(lines):
     n = int(lines[i])
     i+=1
     degree = {j: 0 for j in range(1, n+1)}
+    adj_lists = {j: [] for j in range(1, n+1)}
     edges = []
     while n > 1:
         fr, to = map(int, lines[i].split(" "))
+        adj_lists[fr].append(to)
+        adj_lists[to].append(fr)
         degree[fr]+=1
         degree[to]+=1
         edges.append([fr, to])
@@ -19,16 +22,23 @@ while i < len(lines):
         print(-1)
         continue
     last_taken = {j:-1 for j in range(1, len(degree) + 1)}
-    # print(last_taken)
+    edge_weight = {}
+    visited = set()
+    def dfs(curr, previouslytaken):
+        visited.add(curr)
+        for nbr in adj_lists[curr]:
+            if nbr not in visited:
+                edgestr = str(curr) + " " + str(nbr)
+                edgestr2 = str(nbr) + " " + str(curr)
+                weight = 2 if previouslytaken == -1 or previouslytaken == 5 else 5
+                edge_weight[edgestr] = weight 
+                edge_weight[edgestr2] = weight
+                previouslytaken = weight
+                dfs(nbr, weight)
+    try:
+        dfs(1, -1)
+    except Exception as e:
+        print(e)
     for fr, to in edges:
-        if last_taken[fr] == -1 and last_taken[to] == -1 or (last_taken[fr] == 5 or last_taken[to] == 5):
-            last_taken[fr] = 2
-            last_taken[to] = 2
-            print("weight 2 assigned to edge from", fr, "to", to)
-            # print(2, end= " ")
-        else:
-            last_taken[fr] = 5
-            last_taken[to] = 5
-            print("weight 5 assigned to edge from", fr, "to", to)
-            # print(5, end= " ")
+        print(edge_weight[str(fr) + " " + str(to)], end=" ")
     print()
