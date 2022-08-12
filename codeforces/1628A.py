3884fb01bfbd6439482e9ddbc04d5b32e1e498ad
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-12 08:42:05 +0200

    solve 1628A

--- codeforces/1628A.py
+++ codeforces/1628A.py
@@ -1,33 +1,31 @@
 import sys
+import bisect
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 for line in lines[2::2]:
     nums = list(map(int, line.split(" ")))
-    kpos = 0
-    # print(nums)
-    candidates = []
-    while kpos < len(nums):
-        candidate = [-1, -1] # How big this k should be, the max
-        seen = set()
-        maxseen = -1
-        for i in range(kpos, len(nums)):
-            maxseen = max(nums[i], maxseen)
-            seen.add(nums[i])
-            if len(seen) == maxseen + 1 and candidate[-1] != maxseen:
-                candidate = [i-kpos+1, maxseen]
-        # print(candidate)
-        if candidate[0] == -1:
-            if 0 in seen:
-                candidates.append([-1, 0])
-            else:
-                candidates.append([-1, -1])
-            break
-        candidates.append(candidate)
-        kpos += candidate[0]
-    print(len(candidates))
-    for seen, big in candidates:
-        if big == -1:
-            print(0, end=" ")
+    occurrences = {}
+    for idx, x in enumerate(nums):
+        if x in occurrences:
+            occurrences[x].append(idx)
         else:
-            print(big + 1, end= " ")
+            occurrences[x] = [idx]
+    kpos = 0
+    ans = []
+    # print(occurrences)
+    while kpos < len(nums):
+        # print(kpos)
+        furthesttaken = kpos
+        for i in range(len(nums)+1):
+            if i not in occurrences or kpos > occurrences[i][-1]:
+                if kpos == furthesttaken: furthesttaken +=1
+                ans.append(i)
+                break
+            takeindex = occurrences[i][bisect.bisect_left(occurrences[i], kpos)]
+            furthesttaken = max(furthesttaken, takeindex+1)
+            # print("Take", i, "at index", takeindex, "furthesttaken", furthesttaken)
+        kpos = furthesttaken
+    print(len(ans))
+    for x in ans:
+        print(x, end=" ")
     print()
