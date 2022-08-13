cb131b778d973b08508e5ba2fcff1a6fd9c1390d
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-13 15:22:48 +0200

    solve b 762

--- codeforces/762/B.py
+++ codeforces/762/B.py
@@ -1,2 +1,18 @@
-import sys
+import sys, bisect
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+good = []
+seen = set()
+
+for i in range(1, 2*10**5):
+    if i**2 > 10**9: break
+    if i**2 not in seen:
+        good.append(i**2)
+        seen.add(i**2)
+    if i**3 not in seen:
+        good.append(i**3)
+        seen.add(i**3)
+good.sort()
+for line in lines[1:]:
+    n = int(line)
+    print(bisect.bisect(good, n))
