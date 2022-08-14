995184748eb40c91d20b5b6374cc84e3738c2cb1
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-14 08:48:40 +0200

    solve D, but don't know how it works lol

--- codeforces/762/D.py
+++ codeforces/762/D.py
@@ -1,31 +1,38 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+
 a = 2
 while a < len(lines):
     m, n = map(int, lines[a].split(" "))
     a+=1
     ncopy = m
     matrix = []
+    high = -1
     while ncopy > 0:
         matrix.append(list(map(int, lines[a].split(" "))))
+        high = max(max(list(map(int ,lines[a].split(" ")))), high)
         ncopy-=1
         a+=1
     a+=1
-    beststore = {i: [] for i in range(n)} # friend -> [score, store, secondbest]
-    for i in range(m):
-        for j in range(n):
-            if not beststore[j] or matrix[i][j] > beststore[j][0]:
-                last = -1 if not beststore[j] else beststore[j][2]
-                beststore[j] = [matrix[i][j], i, last]
-            elif beststore[j][2] < matrix[i][j]:
-                beststore[j][2] = matrix[i][j]
-    stores = set()
-    # print(beststore)
-    beststore = [beststore[key] for key in beststore]
-    for score, store, secondbest in beststore:
-        stores.add(store)
-    if len(stores) <= n - 1:
-        print(min([score for score, store, secondbest in beststore]))
-    else:
-        print(max([secondbest for score, store, secondbest in beststore]))
+    low = 1
+    # print("low and high", low, high)
+    def check(x):
+        able = [False]*n
+        pair = False
+        for i in range(m):
+            count = 0
+            for j in range(n):
+                if matrix[i][j] >= x:
+                    able[j] = True
+                    count +=1
+            if count > 1: pair = True
+        if not pair and m > 1: return False
+        return all(able)
+    while low <= high:
+        mid = (high + low) >> 1
+        if check(mid):
+            low = mid + 1
+        else:
+            high = mid - 1
+    print(low-1)
