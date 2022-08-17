828eb6914357394f94fdf947d22f51b084a1b327
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-17 17:46:09 +0200

    progress on 1535C, broke two cases into one

--- codeforces/1535C.py
+++ codeforces/1535C.py
@@ -2,22 +2,19 @@ import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 for line in lines[1:]:
-    streak = 0
+    streak = 1
     firstquestion = -1
     i = 1
     even = -1 # Should our ones be on even indexes?
     while i < len(line):
+        curr = line[i]
         if even == -1:
-            even = i % 2 == 0 and line[i] == '1'
-            if line[i] == '?': even = -1
-        if i % 2 == 0:
-            if (even and line[i] == '0') or (not even and line[i] == '1'):
+            even = i % 2 == 0 and curr == '1'
+            if curr == '?': even = -1
+        if curr != '?':
+            curr = int(curr)
+            if (even and curr == i%2) or (not even and curr == (i+1)%2):
                 print("streak broken at", i, "got to", streak)
-                even = not even
-                streak = 0
-        if i % 2 == 1:
-            if (even and line[i] == '1') or (not even and line[i] == '0'):
-                print("streak broken at", i, "got to", streak) 
                 even = not even
                 streak = 0
         streak += 1
