9b8c148cf909926d0549f45b989fbfcb6d6749b7
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-17 11:13:57 +0200

    solve 1559D

--- codeforces/1559D.py
+++ codeforces/1559D.py
@@ -1,30 +1,6 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-def union_find(adj_lists, n):
-    parent = [i for i in range(1, n+1)]
-    def union(a, b):
-        parent[find(a)-1] = parent[find(b)-1]
-    def find(a):
-        if parent[a-1] == a:
-            return a
-        p = find(parent[a-1])
-        parent[a-1] = p
-        return p
-    for i in range(1, n+1):
-        for nbr in adj_lists[i]:
-            if find(i) != find(nbr):
-                union(i, nbr)
-    sccs = {}
-    for idx, x in enumerate(parent):
-        idx += 1
-        p = find(x)
-        if p not in sccs: sccs[p] = []
-        sccs[find(x)].append(idx)
-    scc_arr = []
-    for key in sccs:
-        scc_arr.append(sccs[key])
-    return scc_arr
 
 n, a, b = map(int, lines[0].split(" "))
 adj_lists_M = {i:set() for i in range(1, n+1)}
@@ -42,8 +18,40 @@ while b > 0:
     adj_lists_D[to].add(fr)
     b-=1
     i+=1
-print(adj_lists_D)
-print(adj_lists_M)
-
-print(union_find(adj_lists_D, n))
-print(union_find(adj_lists_M, n))
+parent_M = [i for i in range(1, n+1)]
+def find_M(a):
+    if parent_M[a-1] == a:
+        return a
+    p = find_M(parent_M[a-1])
+    parent_M[a-1] = p
+    return p
+def union_M(a, b):
+    parent_M[find_M(a)-1] = parent_M[find_M(b)-1]
+for i in range(1, n+1):
+    for nbr in adj_lists_M[i]:
+        if find_M(i) != find_M(nbr):
+            union_M(i, nbr)
+parent_D = [i for i in range(1, n+1)]
+def find_D(a):
+    if parent_D[a-1] == a:
+        return a
+    p = find_D(parent_D[a-1])
+    parent_D[a-1] = p
+    return p
+def union_D(a, b):
+    parent_D[find_D(a)-1] = parent_D[find_D(b)-1]
+for i in range(1, n+1):
+    for nbr in adj_lists_D[i]:
+        if find_D(i) != find_D(nbr):
+            union_D(i, nbr)
+res = []
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i == j: continue
+        if find_M(i) != find_M(j) and find_D(i) != find_D(j):
+            union_M(i, j)
+            union_D(i, j)
+            res.append([i, j])
+print(len(res))
+for fr, to in res:
+    print(fr, to)
