b8828f18646cced85e7955095ce9d283b4f66764
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-17 09:21:28 +0200

    solve 1573B

--- codeforces/1573B.py
+++ codeforces/1573B.py
@@ -1,21 +1,19 @@
-from operator import index
 import sys, bisect
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 for i in range(2, len(lines), 3):
     a = list(map(int, lines[i].split(" ")))
     b = list(map(int, lines[i+1].split(" ")))
-    bsorted = list(sorted(b))
+    bsearcharray = []
+    bsearcharryindexes = []
     indexes = {}
-    for idx, x in enumerate(bsorted):
-        indexes[x] = idx
+    for idx, x in enumerate(b):
+        if not bsearcharray or x > bsearcharray[-1]:
+            bsearcharray.append(x)
+            bsearcharryindexes.append(idx)
     best = 10**9
     for idx, x in enumerate(a):
-        if x < b[0]:
-            best = min(best, idx)
-            continue
-        smallest_bigger_idx = bisect.bisect_left(bsorted, x+1)
-        candidate = idx + indexes[bsorted[smallest_bigger_idx]]
-        best = min(best, candidate)
+        bsearchidx = bisect.bisect_left(bsearcharray, x+1)
+        best = min(best, idx + bsearcharryindexes[bsearchidx])
     print(best)
 
