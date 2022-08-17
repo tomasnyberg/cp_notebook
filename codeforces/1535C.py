4adaff783a1e21c278f375a22b72a1cecb340e91
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-17 21:16:32 +0200

    solve 1535C with help, not sure how it works

--- codeforces/1535C.py
+++ codeforces/1535C.py
@@ -1,24 +1,17 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+from heapq import heappop, heappush
 
 for line in lines[1:]:
-    streak = 0
-    firstquestion = -1
-    i = 0
+    dp = [[0, 0] for i in range(len(line))]
+    dp[0][0] = 1 if line[0] == '0' or line[0] == '?' else 0
+    dp[0][1] = 1 if line[0] == '1' or line[0] == '?' else 0
+    for i in range(1, len(line)):
+        if line[i] != '1':
+            dp[i][0] = 1 + dp[i-1][1]
+        if line[i] != '0':
+            dp[i][1] = 1 + dp[i-1][0]
     result = 0
-    even = -1 # Should our ones be on even indexes?
-    while i < len(line):
-        curr = line[i]
-        if even == -1:
-            even = i % 2 == 0 and curr == '1'
-            if curr == '?': even = -1
-        if curr != '?':
-            curr = int(curr)
-            if (even and curr == i%2) or (not even and curr == (i+1)%2):
-                even = not even
-                result += (streak*(streak+1))//2
-                streak = 0
-        streak += 1
-        i+= 1
-    result += (streak*(streak+1))//2
+    for i in range(len(dp)):
+        result += max(dp[i][0], dp[i][1])
     print(result)
