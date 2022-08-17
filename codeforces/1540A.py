cf56769addf22020819aaf69c83616c1e1687283
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-17 16:39:47 +0200

    solve 1540A

---
+++ codeforces/1540A.py
@@ -0,0 +1,23 @@
+import sys
+lines = list(map(str.strip, sys.stdin.readlines()))
+
+def cum_sum(nums):
+    curr = 0
+    result = [0]*len(nums)
+    for idx, num in enumerate(nums):
+        curr += num
+        result[idx] = curr
+    return result
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split(" ")))
+    nums.sort()
+    result = nums[-1]
+    cs = cum_sum(nums)
+    # A number is going to contribute with the sum of all numbers after it,
+    # minus the amount of numbers after it - itself
+    for idx, x in enumerate(nums):
+        sum_after = cs[-1] - cs[idx]
+        nums_after = len(nums) - idx - 1
+        result += -(sum_after - nums_after*x)
+    print(result)
