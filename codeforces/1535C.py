87a7753527cdd96a25e7e85c7bc0d99c28b5b69f
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-17 17:55:45 +0200

    even closer on 1535C i think but need to redo my whole question mark shit to assign them to the longest streak...

--- codeforces/1535C.py
+++ codeforces/1535C.py
@@ -2,9 +2,10 @@ import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 for line in lines[1:]:
-    streak = 1
+    streak = 0
     firstquestion = -1
-    i = 1
+    i = 0
+    result = 0
     even = -1 # Should our ones be on even indexes?
     while i < len(line):
         curr = line[i]
@@ -14,8 +15,10 @@ for line in lines[1:]:
         if curr != '?':
             curr = int(curr)
             if (even and curr == i%2) or (not even and curr == (i+1)%2):
-                print("streak broken at", i, "got to", streak)
                 even = not even
+                result += (streak*(streak+1))//2
                 streak = 0
         streak += 1
         i+= 1
+    result += (streak*(streak+1))//2
+    print(result)
