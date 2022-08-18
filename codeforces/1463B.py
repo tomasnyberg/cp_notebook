245965b3b3db7d4a3030a87f14abe388b4f3b03a
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-18 21:42:42 +0200

    working on 1463B

---
+++ codeforces/1463B.py
@@ -0,0 +1,31 @@
+import sys, math, bisect
+lines = list(map(str.strip, sys.stdin.readlines()))
+
+def divisors(n):
+    result = []
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            result.append(i)
+    result.append(n)
+    return result
+
+
+for i in range(1, len(lines), 2):
+    nums = list(map(int, lines[i+1].split(" ")))
+    S = sum(nums)
+    ans = [nums[0]]
+    for j in range(1, len(nums)):
+        divs1 = divisors(nums[j])
+        divs2 = divisors(ans[j-1])
+        closest = (10**18, -1) # How close, what number
+        for divs in [divs1, divs2]:
+            for div in divs[::-1]:
+                if ans[j-1] % div == 0 or div % ans[j-1] == 0:
+                    if abs(nums[j] - ans[j-1]) < closest[0]:
+                        closest = (abs(nums[j] - div), div)
+        ans.append(closest[1])
+    bad = 2*(sum([abs(ans[j] - nums[j]) for j in range(len(ans))]))
+    # assert(bad <= S)
+    print(bad)
+    [print(x,end=" ") for x in ans]
+    print()
