7de593262d27fa6c56f292ec3f951f7a9b9b330d
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-18 07:55:11 +0200

    clean up 1535C solution slightly

--- codeforces/1535C.py
+++ codeforces/1535C.py
@@ -1,16 +1,13 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
-from heapq import heappop, heappush
 
 for line in lines[1:]:
     dp = [[0, 0] for i in range(len(line))]
-    dp[0][0] = 1 if line[0] == '0' or line[0] == '?' else 0
-    dp[0][1] = 1 if line[0] == '1' or line[0] == '?' else 0
-    for i in range(1, len(line)):
+    for i in range(len(line)):
         if line[i] != '1':
-            dp[i][0] = 1 + dp[i-1][1]
+            dp[i][0] = 1 + (dp[i-1][1] if i > 0 else 0)
         if line[i] != '0':
-            dp[i][1] = 1 + dp[i-1][0]
+            dp[i][1] = 1 + (dp[i-1][0] if i > 0 else 0)
     result = 0
     for i in range(len(dp)):
         result += max(dp[i][0], dp[i][1])
