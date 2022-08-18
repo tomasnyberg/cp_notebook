91a95a5f1684c1289def894ce0ea5d958553e202
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-18 21:48:28 +0200

    working solution, but didn't realize that there are also 1000 tests lmao thought there was max 50 n total

--- codeforces/1463B.py
+++ codeforces/1463B.py
@@ -17,6 +17,10 @@ for i in range(1, len(lines), 2):
     for j in range(1, len(nums)):
         divs1 = divisors(nums[j])
         divs2 = divisors(ans[j-1])
+        if nums[j] > ans[j-1]:
+            div = nums[j] // ans[j-1]
+            divs2.append(div*ans[j-1])
+            divs2.append((div+1)*ans[j-1])
         closest = (10**18, -1) # How close, what number
         for divs in [divs1, divs2]:
             for div in divs[::-1]:
@@ -26,6 +30,6 @@ for i in range(1, len(lines), 2):
         ans.append(closest[1])
     bad = 2*(sum([abs(ans[j] - nums[j]) for j in range(len(ans))]))
     # assert(bad <= S)
-    print(bad)
+    # print(bad)
     [print(x,end=" ") for x in ans]
     print()
