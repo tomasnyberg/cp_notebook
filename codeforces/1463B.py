85e5e404586e6dbd52d532da64fce3de189bcc34
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-20 09:45:09 +0200

    slightly more obfuscation :)

--- codeforces/1463B.py
+++ codeforces/1463B.py
@@ -3,8 +3,7 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 
 for line in lines[2::2]:
     nums = list(map(int, line.split(" ")))
-    candidates = [[nums[i] if i % 2 == j else 1 for i in range(len(nums))] for j in range(2)]
-    for candidate in candidates:
+    for candidate in [[nums[i] if i % 2 == j else 1 for i in range(len(nums))] for j in range(2)]:
         if 2*sum([abs(nums[i] - candidate[i]) for i in range(len(candidate))]) <= sum(nums):
             [print(x, end =" ") for x in candidate]
             break
