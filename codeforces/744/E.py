32f03e1455ea86c3efc279ea2efada423d9be01a
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-21 14:13:43 +0200

    solve E

--- codeforces/744/E.py
+++ codeforces/744/E.py
@@ -1,2 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+from queue import deque
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split(" ")))
+    dq = deque([])
+    for x in nums:
+        if not dq or x < dq[0]:
+            dq.appendleft(x)
+        else:
+            dq.append(x)
+    for x in dq:
+        print(x, end=" ")
+    print()
