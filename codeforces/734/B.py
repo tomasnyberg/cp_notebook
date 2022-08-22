542c1dde2c8459c18d95e2fe3735ee0cb8697c3e
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-22 10:15:16 +0200

    solve B

--- codeforces/734/B.py
+++ codeforces/734/B.py
@@ -1,2 +1,16 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    counts = {}
+    atleasttwo = 0
+    singles = 0
+    for c in line:
+        if c not in counts: counts[c] = 0
+        counts[c] +=1
+        if counts[c] == 1: singles +=1
+        if counts[c] == 2:
+            singles -=1
+            atleasttwo += 1
+    print(atleasttwo + (singles//2))
+
