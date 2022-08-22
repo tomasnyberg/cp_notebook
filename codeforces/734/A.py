0446d5605ebc9bad86087b7e14c8e8110ec8f451
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-22 10:01:27 +0200

    solve A

--- codeforces/734/A.py
+++ codeforces/734/A.py
@@ -1,2 +1,10 @@
-import sys
+import sys, math
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    n = int(line)
+    if math.floor(n/3)*2 + math.ceil(n/3) == n:
+        print(math.ceil(n/3), math.floor(n/3))
+    else:
+        print(math.floor(n/3), math.ceil(n/3))
+
