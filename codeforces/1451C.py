41fc9dabe4f83dc21bdf7dc7c65d24f1cac9b18d
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-25 13:46:38 +0200

    rewrite 1451C so i understand it better

--- codeforces/1451C.py
+++ codeforces/1451C.py
@@ -5,20 +5,20 @@ for i in range(1, len(lines),3):
     n, k = map(int, lines[i].split(" "))
     a = lines[i+1]
     b = lines[i+2]
-    have = [0]*27
-    need = [0]*27
-    for j in range(n):
-        have[ord(a[j]) - 97] += 1
-        need[ord(b[j]) - 97] += 1
-    print(have)
-    print(need)
+    afreq = [0]*27
+    bfreq = [0]*27
     bad = False
-    for j in range(25, -1, -1):
-        have[j]+=have[j+1]
-        need[j] += need[j+1]
-        if have[j] > need[j] or (need[j] - have[j]) % k != 0:
+    for j in range(len(a)):
+        afreq[ord(a[j])-97] += 1
+        bfreq[ord(b[j])-97] += 1
+    have = 0
+    for j in range(27):
+        have += afreq[j]
+        have -= bfreq[j]
+        if have < 0 or have % k != 0:
             bad = True
             break
+    if have > 0: bad = True 
     print("NO" if bad else "YES") 
 
     
