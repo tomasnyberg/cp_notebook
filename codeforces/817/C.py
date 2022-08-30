451164db8794e8f13741a1e9e5eefba35b891014
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-30 17:00:06 +0200

    solve C

--- codeforces/817/C.py
+++ codeforces/817/C.py
@@ -1,2 +1,18 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for i in range(1, len(lines), 4):
+    awords = lines[i+1].split(" ")
+    bwords = lines[i+2].split(" ")
+    cwords = lines[i+3].split(" ")
+    counts = {}
+    for arr in [awords, bwords, cwords]:
+        for word in arr:
+            counts[word] = 1 if word not in counts else counts[word] + 1
+    for arr in [awords, bwords, cwords]:
+        score = 0
+        for word in arr:
+            if counts[word] == 1: score+=3
+            elif counts[word] == 2: score +=1
+        print(score, end=" ")
+    print()
