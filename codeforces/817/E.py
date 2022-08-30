49351e382e1898b7bdb4f37fce0e963c2303bd55
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-30 17:22:36 +0200

    brute force for E, going to try think of something more clever

--- codeforces/817/E.py
+++ codeforces/817/E.py
@@ -1,2 +1,23 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+i = 1
+while i < len(lines):
+    n, q = map(int, lines[i].split(" "))
+    i+=1
+    rectangles = []
+    while n > 0:
+        rectangles.append(list(map(int, lines[i].split(" "))))
+        i+=1
+        n-=1
+    queries = []
+    while q > 0:
+        queries.append(list(map(int, lines[i].split(" "))))
+        i+=1
+        q-=1
+    for hs, ws, hb, wb in queries:
+        result = 0
+        for h, w in rectangles:
+            if h > hs and h < hb and ws < w < wb:
+                result += h*w
+        print(result)  
