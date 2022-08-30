becb92b2e6fcba0c03b0cfbb6e4c04d8f58e7c79
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-30 16:55:33 +0200

    solve B

--- codeforces/817/B.py
+++ codeforces/817/B.py
@@ -1,2 +1,14 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+same = set(['G', 'B'])
+
+for i in range(1, len(lines), 3):
+    a = lines[i+1]
+    b = lines[i+2]
+    bad = False
+    for j in range(len(a)):
+        if (a[j] == 'R' and b[j] in same) or (b[j] == 'R' and a[j] in same):
+            bad = True
+            break
+    print("YES" if not bad else "NO")
