13d8b726e6cb3d8eaa7821ecbdf020c93304335b
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-30 17:12:14 +0200

    solve D

--- codeforces/817/D.py
+++ codeforces/817/D.py
@@ -1,2 +1,26 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    potentialincreases = [0]*len(line)
+    result = 0
+    for i in range(len(line)):
+        leftscore = i
+        rightscore = len(line) - i - 1
+        currscore = leftscore if line[i] == 'L' else rightscore
+        result += currscore
+        otherscore = rightscore if line[i] == 'L' else leftscore
+        if otherscore > currscore:
+            potentialincreases[i] = otherscore - currscore
+        else:
+            potentialincreases[i] = -1
+    potentialincreases.sort()
+    # print(result, potentialincreases)
+    for i in range(len(line)):
+        popped = potentialincreases.pop()
+        if popped != -1:
+            result += popped
+        print(result, end= " ")
+    print()
+
+
