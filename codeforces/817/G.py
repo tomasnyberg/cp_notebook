85ec17697198c63e1f66994dac7969ab21bc74f0
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-31 19:55:22 +0200

    solution for G

--- codeforces/817/G.py
+++ codeforces/817/G.py
@@ -1,2 +1,18 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for x in lines[1:]:
+    x = int(x)
+    result = []
+    for i in range(x-3):
+        result.append(i+1)
+    result.append(1 << 29)
+    result.append(1 << 30)
+    last = 0
+    for x in result:
+        last ^= x
+    result.append(last)
+    print(*result)
+
+    
+    
