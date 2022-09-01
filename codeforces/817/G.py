67931b8195e7e49713f8db76cdddded5778be894
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-09-01 19:08:26 +0200

    clean up G solution slightly, not pretty still though

--- codeforces/817/G.py
+++ codeforces/817/G.py
@@ -1,13 +1,6 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-def xorall(xs, start):
-    total = 0
-    for x in xs[start::2]:
-        total ^= x
-    return total
-
-
 for x in lines[1:]:
     x = int(x)
     counter = 1
@@ -30,11 +23,7 @@ for x in lines[1:]:
             result[2] += (1 << 30)
             result[0] += (1 << 30)
             result[0] -= (1 << 29)
-    # print("even xor", xorall(result, 0), "odd xor", xorall(result, 1))
     print(*result)
-    # Print the binary representation of all numbers in result
-    # [print(bin(x), end=" ") for x in result]
-    # print()
 
 
     
