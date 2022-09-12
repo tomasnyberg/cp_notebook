698fd77f8768c91c144ff9748c086ce32e7ce2c2
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-09-12 17:15:48 +0200

    working on C, close i think?

--- codeforces/820/C.py
+++ codeforces/820/C.py
@@ -1,2 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    nums = list(map(lambda x: ord(x) - 97, line))
+    decreasing = nums[0] >= nums[-1]
+    for i in range(len(nums)):
+        nums[i] = [nums[i], i+1]
+    if decreasing:
+        nums = list(filter(lambda x: x[0] <= nums[0][0] and x[0] >= nums[-1][0], nums))
+    else:
+        nums = list(filter(lambda x: x[0] >= nums[0][0] and x[0] <= nums[-1][0], nums))
+    shortnums = nums[1:-1]
+    shortnums.sort(key=lambda x: x[0]-nums[0][0])
+    print(shortnums)
