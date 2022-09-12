23cfb0edc89afb09cb7195509c34218c66cfffd1
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-09-12 18:36:07 +0200

    solution to F is correct but too slow

--- codeforces/820/F.py
+++ codeforces/820/F.py
@@ -5,16 +5,20 @@ def subsums(s, w):
     left = 0
     right = 0
     curr = ""
+    possiblemods = {}
     result = []
     while right < len(s):
         curr += s[right]
         if len(curr) == w:
-            result.append((int(curr), left+1))
+            num = int(curr) % 9
+            if num not in possiblemods: possiblemods[num] = []
+            if len(possiblemods[num]) < 2:
+                possiblemods[num].append(left+1)
+                result.append(((left+1), num))
             curr = curr[1:]
             left += 1
         right += 1
-    result.sort()
-    return result
+    return [possiblemods, result]
 
 i = 1
 while i < len(lines):
@@ -26,7 +30,27 @@ while i < len(lines):
         queries.append(list(map(int, lines[i].split(" "))))
         m-=1
         i+=1
-    substringsums = subsums(s, w)
+    possiblemods, subsetsums = subsums(s, w)
+    # print(possiblemods)
+    # print(subsetsums)
     for l, r, k in queries:
-        print(l, r, k)
+        found = [-1, -1]
+        currval = int(s[l-1:r])
+        for idx, mod in subsetsums:
+            if found != [-1, -1]: break
+            currmod = (mod*currval) % 9
+            lookingfor = -1
+            if currmod == k:
+                lookingfor = 0
+            elif currmod >= k:
+                lookingfor = 9 - currmod + k
+            else:
+                lookingfor = k - currmod
+            if lookingfor in possiblemods:
+                for pos in possiblemods[lookingfor]:
+                    if pos != idx:
+                        found = [idx, pos]
+                        # print(possiblemods[lookingfor], found)
+                        break
+        print(*found)
 
