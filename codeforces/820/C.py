c532b6138d8a27266b8f526c8c843ca94b9793b6
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-09-12 17:21:22 +0200

    WA test 1 on C ??

--- codeforces/820/C.py
+++ codeforces/820/C.py
@@ -3,6 +3,7 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 
 for line in lines[1:]:
     nums = list(map(lambda x: ord(x) - 97, line))
+    length = len(nums)
     decreasing = nums[0] >= nums[-1]
     for i in range(len(nums)):
         nums[i] = [nums[i], i+1]
@@ -12,4 +13,11 @@ for line in lines[1:]:
         nums = list(filter(lambda x: x[0] >= nums[0][0] and x[0] <= nums[-1][0], nums))
     shortnums = nums[1:-1]
     shortnums.sort(key=lambda x: x[0]-nums[0][0])
-    print(shortnums)
+    result = [1]
+    totalcost = abs(nums[0][0] - nums[-1][0])
+    for _, index in shortnums:
+        result.append(index)
+    result.append(length)
+    print(abs(nums[0][0]-nums[-1][0]), len(result))
+    print(*result)
+
