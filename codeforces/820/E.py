c61e1fe4f1a5b7652ef3630d00abc963aa27cf0e
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-09-12 17:37:33 +0200

    solve Ega .ga .ga .!

--- codeforces/820/E.py
+++ codeforces/820/E.py
@@ -1,2 +1,29 @@
 import sys
-lines = list(map(str.strip, sys.stdin.readlines()))
+
+def handle_notfound(counter):
+    print("?", counter, 1)
+    sys.stdout.flush()
+    if int(input()) == -1:
+        return counter-1
+    else:
+        return counter
+
+
+counter = 1
+while(True):
+    print("?", counter, counter+1)
+    sys.stdout.flush()
+    a = int(input())
+    if a == 0: sys.exit(0)
+    if a == -1:
+        print("!", handle_notfound(counter))
+        sys.exit(0)
+    print("?", counter+1, counter)
+    sys.stdout.flush()
+    b = int(input())
+    if a == b:
+        counter += 2
+    else:
+        print("!", a+b)
+        sys.exit(0)
+
