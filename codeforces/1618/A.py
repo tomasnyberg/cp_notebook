56ce40ed6717fa24a0bceb1d5d91235e0c00d8dc
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-09-27 18:11:17 +0200

    solve A

--- codeforces/1618/A.py
+++ codeforces/1618/A.py
@@ -1,2 +1,12 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    nums = list(map(int, line.split(" ")))
+    res = [nums[0], nums[1]]
+    if nums[2] == nums[0] + nums[1]:
+        res.append(nums[3])
+    else:
+        res.append(nums[2])
+    print(*res)
+
