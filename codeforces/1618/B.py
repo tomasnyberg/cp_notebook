f78081cb19c431cbb6d8268d8c7ae75d1a26b1e7
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-09-27 18:21:37 +0200

    solve B

--- codeforces/1618/B.py
+++ codeforces/1618/B.py
@@ -1,2 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    split = line.split(" ")
+    result = split[0]
+    found = False
+    for i in range(1, len(split)):
+        if split[i][0] != split[i-1][1]:
+            result += split[i][0]
+            found = True
+        result += split[i][1]
+    if not found:
+        result += split[-1][1]
+    print(result)
