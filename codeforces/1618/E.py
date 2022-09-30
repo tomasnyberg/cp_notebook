20a580fc7b55706359a7766b97a7ed1d043fea19
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-09-30 21:05:30 +0200

    (up)solve E

--- codeforces/1618/E.py
+++ codeforces/1618/E.py
@@ -5,14 +5,19 @@ for line in lines[2::2]:
     b = list(map(int, line.split(" ")))
     n = len(b)
     S = 2*sum(b)/(n*(n+1))
+    d = (n*(n+1)) // 2
+    if sum(b) % d != 0:
+        print("NO")
+        continue
     result = [0]*n
     bad = False
-    for i in range(len(b)):
-        result[i] = int((S - b[i] + b[(i+n-1)%n]) // n)
-        if result[i] <= 0:
+    for i in range(len(b)-1,-1,-1):
+        res = int(b[i] - b[(i+1)%n] + S)
+        if res <= 0 or res%n != 0:
             print("NO")
             bad = True
             break
+        result[(i+1)%n] = res//n
     if bad: continue
     print("YES")
     print(*result)
