f0c68af7638931d14aa3f5ab05755a54491e7ad2
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-09-30 20:34:32 +0200

    remove unncessary stuff from E

--- codeforces/1618/E.py
+++ codeforces/1618/E.py
@@ -1,22 +1,6 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-def simulate(a):
-    result = [0]*len(a)
-    for idx, x in enumerate(a):
-        added = x
-        for i in range(idx, idx + len(result)):
-            result[i % len(result)] += added
-            added += x
-    print(result)
-
-def arithmetic_sum(a, n):
-    return (n*(a+a*n))//2
-# print(arithmetic_sum(6, 6))
-# # simulate([5,5,4,1,4,5])
-# simulate([5,5,5,5,5,5])
-# print(ä)
-
 for line in lines[2::2]:
     b = list(map(int, line.split(" ")))
     n = len(b)
