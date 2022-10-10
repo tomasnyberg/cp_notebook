3852670a3d714ca07d227bdbadb57af8adfe12d5
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-10-10 19:09:16 +0200

    solve D (i think?)

--- codeforces/1512/D.py
+++ codeforces/1512/D.py
@@ -1,2 +1,22 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    b = list(map(int, line.split(" ")))
+    total = sum(b)
+    b.sort()
+    # print(b)
+    badindex = -1
+    for candidate in b[len(b)-2:]:
+        for idx, num in enumerate(b[:len(b)-2]):
+            if total - num - candidate == candidate:
+                badindex = idx
+                break
+        if badindex != -1:
+            break
+    if badindex == -1:
+        print(-1)
+        continue
+    del b[badindex]
+    b.pop()
+    print(*b)
