af4ad45ce3ebab977f575cbca35411d09012c959
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-10-10 18:26:51 +0200

    solve A

--- codeforces/1512/A.py
+++ codeforces/1512/A.py
@@ -1,2 +1,12 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split(" ")))
+    count = {}
+    for x in nums:
+        count[x] = 1 if x not in count else count[x] + 1
+    for idx, x in enumerate(nums):
+        if count[x] == 1:
+            print(idx + 1)
+            break
