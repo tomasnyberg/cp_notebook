4c265a0b3491572aac0a9aa2385f7a8214627ef7
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-10-10 20:43:22 +0200

    solve D

--- codeforces/1512/D.py
+++ codeforces/1512/D.py
@@ -5,15 +5,18 @@ for line in lines[2::2]:
     b = list(map(int, line.split(" ")))
     total = sum(b)
     b.sort()
-    # print(b)
     badindex = -1
-    for candidate in b[len(b)-2:]:
-        for idx, num in enumerate(b[:len(b)-2]):
+    for idx1, candidate in enumerate(b[len(b)-5:]):
+        for idx, num in enumerate(b[:-2]):
             if total - num - candidate == candidate:
                 badindex = idx
                 break
         if badindex != -1:
             break
+    if sum(b[:-2]) == b[-2]:
+        badindex = len(b) - 2
+    if sum(b[:-2]) == b[-1]:
+        badindex = len(b) - 1
     if badindex == -1:
         print(-1)
         continue
