026bda7b83f2f6530dac2380062f013b774fa44f
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-10-11 16:43:49 +0200

    solve B

--- codeforces/826/B.py
+++ codeforces/826/B.py
@@ -1,2 +1,17 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    n = int(line)
+    if n == 3 or n == 1:
+        print(-1)
+        continue
+    if n == 2:
+        print(2, 1)
+        continue
+    result = [0]*n
+    for i in range(2, n):
+        result[i] = i - 1
+    result[1] = n
+    result[0] = n-1
+    print(*result) 
