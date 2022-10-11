8ef7b9affff43ea0cb12495727423c73b6a920cc
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-10-11 16:39:42 +0200

    solve A

--- codeforces/826/A.py
+++ codeforces/826/A.py
@@ -1,2 +1,22 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    a, b = line.split(" ")
+    axs = 0
+    bxs = 0
+    for c in a:
+        if c =='X': axs += 1
+    for c in b:
+        if c =='X': bxs += 1
+    if a[-1] != b[-1]:
+        print(">" if a[-1] < b[-1] else "<")
+    else:
+        if axs == bxs:
+            print("=")
+        else:
+            if a[-1] == 'S':
+                print(">" if axs < bxs else "<")
+            else:
+                print("<" if axs < bxs else ">")
+        
