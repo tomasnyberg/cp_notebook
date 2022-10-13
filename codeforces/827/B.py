d438a42ca02ac0a3cb9d75e63a88c5c265ba28df
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-10-13 16:38:02 +0200

    solve B

--- codeforces/827/B.py
+++ codeforces/827/B.py
@@ -1,2 +1,12 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split(" ")))
+    nums.sort()
+    bad = False
+    for i in range(len(nums) - 1):
+        if nums[i] >= nums[i+1]:
+            bad = True
+            break
+    print("NO" if bad else "YES")
