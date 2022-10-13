44492f28f48304c8417d8ed2f19288986a01298c
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-10-13 21:12:59 +0200

    closer on G, still something off

--- codeforces/827/G.py
+++ codeforces/827/G.py
@@ -11,19 +11,22 @@ for line in lines[2::2]:
     nums = list(map(int, line.split(" ")))
     for idx,x in enumerate(nums):
         highest_bit(x, bitsset, idx)
-    for key in bitsset:
-        bitsset[key].sort(key=lambda x: x[0])
     result = []
     taken = set([-1])
+    running = 0
     for i in range(31, -1, -1):
+        if running & (1 << i): continue
         if bitsset[i]:
+            bitsset[i].sort(key=lambda x: x[0]^running)
             num, idx = -1, -1
             while idx in taken and bitsset[i]:
                 num,idx = bitsset[i].pop()
             if idx in taken:
                 continue
+            running |= num
             result.append(num)
             taken.add(idx)
+    # print(taken)
     for idx, x in enumerate(nums):
         if idx in taken: continue
         result.append(x)
