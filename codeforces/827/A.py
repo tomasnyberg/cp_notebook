546e7b0b1a98163b950e751bf623981fd3fbfcc5
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-10-13 16:36:40 +0200

    solve A2

--- codeforces/827/A.py
+++ codeforces/827/A.py
@@ -1,2 +1,9 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    nums = list(map(int, line.split(" ")))
+    if nums[0] == nums[1] + nums[2] or nums[1] == nums[0] + nums[2] or nums[2] == nums[0] + nums[1]:
+        print("YES")
+    else:
+        print("NO")
