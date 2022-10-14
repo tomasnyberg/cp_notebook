d655daf862d1386cb8e152671c4ac668e3be98cc
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-10-14 16:44:25 +0200

    solve A

--- codeforces/710/A.py
+++ codeforces/710/A.py
@@ -1,9 +1,9 @@
-import sys
+import sys, math
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 for line in lines[1:]:
     n, m, x = map(int, line.split(" "))
-    row = x % (n+1)
-    col = x // (n+1)
-    print(row, col)
-    # print(row*m+col)
+    row = x % n if x % n != 0 else n
+    col = math.ceil(x / (n))
+    # print(x, "is at row", row, "col", col)
+    print((row-1)*m+col)
