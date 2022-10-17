fa00636c7fd769478f1f8c2afbc40575dedccdbd
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-10-17 18:25:33 +0200

    solve A

--- codeforces/828/A.py
+++ codeforces/828/A.py
@@ -1,2 +1,20 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for i in range(1, len(lines), 3):
+    nums = list(map(int, lines[i+1].split(" ")))
+    a = lines[i+2]
+    if len(nums) != len(a):
+        print("NO")
+        continue
+    seen = {}
+    bad = False
+    for j in range(len(nums)):
+        if nums[j] in seen:
+            if seen[nums[j]] != a[j]:
+                # print("bad at", j, seen)
+                bad = True
+                break
+        else:
+            seen[nums[j]] = a[j]
+    print("NO" if bad else "YES")
