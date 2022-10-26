62a654a8bd34565f0ccf54c561956bb9587c1b47
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-10-26 19:54:11 +0200

    resolve B

--- codeforces/719/B.py
+++ codeforces/719/B.py
@@ -1,16 +1,18 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-counts = {}
 for line in lines[1:]:
     x = int(line)
+    n = len(str(x))
     res = 0
-    for d in range(1, 10):
-        curr = 0
-        for k in range(9):
-            curr += 10**k
-            if d*curr <= x:
-                res += 1
+    res += (n-1)*9
+    res += int(line[0])-1
+    for c in line[1:]:
+        if c == line[0]: continue
+        if c < line[0]: break
+        if c > line[0]: 
+            res += 1
+            break
+    if all (c == line[0] for c in line[1:]):
+        res += 1
     print(res)
-
-    
