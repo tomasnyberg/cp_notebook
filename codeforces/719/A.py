4f6d0d8f803c488fdcb3b92ad8b57512e1290b23
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-10-26 17:52:21 +0200

    solve A

--- codeforces/719/A.py
+++ codeforces/719/A.py
@@ -1,2 +1,18 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    prev = line[0]
+    seen = set()
+    bad = False
+    for i in range(1, len(line)):
+        if line[i] != prev:
+            if line[i] in seen:
+                bad = True
+                break
+            seen.add(prev)
+            prev = line[i]
+    if bad:
+        print("NO")
+    else:
+        print("YES")
