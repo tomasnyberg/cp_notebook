d851d5c6b6365636e1a5c2a3903f0d65f8186c73
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-10-26 18:45:05 +0200

    good attempt on E, WA5 tho...

--- codeforces/719/E.py
+++ codeforces/719/E.py
@@ -1,2 +1,24 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+def costs(line):
+    sheep_seen = 0
+    running = 0
+    res = []
+    for c in line:
+        if c == '*':
+            sheep_seen += 1
+        else:
+            running += sheep_seen
+        res.append(running)
+    return res
+
+for line in lines[2::2]:
+    from_left = costs(line)
+    from_right = costs(line[::-1])
+    from_right.reverse()
+    res = 10**9
+    for i in range(len(from_left)):
+        res = min(res, from_left[i] + from_right[i])
+    print(res)
+            
