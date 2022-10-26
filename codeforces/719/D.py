6e83a78621b2d4d80a4d52d5ae5cbf9c4aac3244
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-10-26 18:21:38 +0200

    solve D

--- codeforces/719/D.py
+++ codeforces/719/D.py
@@ -1,2 +1,14 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split(" ")))
+    counts = {}
+    res = 0
+    for idx, x in enumerate(nums):
+        curr = idx - x
+        res += counts[curr] if curr in counts else 0
+        counts[curr] = 1 if curr not in counts else counts[curr] + 1
+    print(res)
+
+        
