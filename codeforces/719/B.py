0a8c3c1b01f3a99d232677f61915bdd37c544002
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-10-26 18:07:55 +0200

    solve B

--- codeforces/719/B.py
+++ codeforces/719/B.py
@@ -1,2 +1,16 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+counts = {}
+for line in lines[1:]:
+    x = int(line)
+    res = 0
+    for d in range(1, 10):
+        curr = 0
+        for k in range(9):
+            curr += 10**k
+            if d*curr <= x:
+                res += 1
+    print(res)
+
+    
