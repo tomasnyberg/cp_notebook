b6f44c9b21da4971956ed9cf4128a9efecd9fa51
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-08 15:29:59 +0100

    solve A

--- codeforces/702/A.py
+++ codeforces/702/A.py
@@ -1,2 +1,19 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split(" ")))
+    result = 0
+    for i in range(len(nums)-1):
+        x = nums[i]
+        y = nums[i+1]
+        if x >= y:
+            while x > (y << 1):
+                # print("index", i-1, i)
+                y <<= 1
+                result += 1
+        else:
+            while y > (x << 1):
+                x <<= 1
+                result += 1
+    print(result)
