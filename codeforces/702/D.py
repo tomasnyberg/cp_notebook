ef3e0b032e0466583981242a980a0034eb12bc85
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-08 15:51:41 +0100

    solve D

--- codeforces/702/D.py
+++ codeforces/702/D.py
@@ -1,2 +1,21 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+def recur(xs, result, depth):
+    if not xs: return
+    biggest = [-1, -1]
+    for idx, x in enumerate(xs):
+        if x > biggest[0]:
+            biggest = [x, idx]
+    result[biggest[0]] = depth
+    recur(xs[:biggest[1]], result, depth+1)
+    recur(xs[biggest[1]+1:], result, depth+1)
+
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split(" ")))
+    result = {}
+    recur(nums, result, 0)
+    for i in nums:
+        print(result[i], end=" ")
+    print()
