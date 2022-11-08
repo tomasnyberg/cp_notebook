51d29150d84129863565d3191ad2da57ddc8c35f
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-08 15:37:41 +0100

    solve B

--- codeforces/702/B.py
+++ codeforces/702/B.py
@@ -1,2 +1,19 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split(" ")))
+    counts = [0]*3
+    for x in nums:
+        counts[x % 3] += 1
+    result = 0
+    target = len(nums) // 3
+    i = 0
+    while counts != [target]*3:
+        while counts[i] > target:
+            counts[(i+1)%3] += 1
+            counts[i] -= 1
+            result += 1
+        i += 1
+        i %= 3
+    print(result)
