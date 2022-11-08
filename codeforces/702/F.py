6e7849a7dcd2fb4eb90cffa3b989d650cd541b1d
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-08 16:23:01 +0100

    solve F2

--- codeforces/702/F.py
+++ codeforces/702/F.py
@@ -6,19 +6,22 @@ for line in lines[2::2]:
     counts = {}
     for x in nums:
         counts[x] = 1 if x not in counts else counts[x] + 1
-    biggestamount = 0
+    amounts = set()
     for x, amount in counts.items():
-        biggestamount = max(biggestamount, amount)
+        amounts.add(amount)
     result = 10**9
     # print(counts)
-    for i in range(biggestamount+1):
+    looks = 0
+    for i in amounts:
         candidate = 0
         for x, amount in counts.items():
+            looks += 1
             if amount < i:
                 candidate += amount
             else:
                 candidate += amount - i
         # print(i, candidate)
         result = min(candidate, result)
+    # print(len(nums), looks)
     print(result)
     # print("\n\n")
