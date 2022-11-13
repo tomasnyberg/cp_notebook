4936cf704f45e8ba72f43b5e63b38634fea637aa
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-13 21:23:43 +0100

    solve A

--- kickstart/H2022/A.py
+++ kickstart/H2022/A.py
@@ -1,2 +1,38 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+i = 1
+test = 1
+while i < len(lines):
+    L, N = map(int, lines[i].split(" "))
+    i+=1
+    nums = []
+    dirs = []
+    for _ in range(N):
+        split = lines[i].split(" ")
+        nums.append(int(split[0]))
+        dirs.append(split[1])
+        i+=1
+    result = 0
+    dir = 0 # 0 = clockwise 1 = counter clockwise
+    pos = 0
+    for j in range(N):
+        if dirs[j] == 'C':
+            pos += nums[j]
+            if pos >= 0:
+                result += pos // L
+                pos %= L
+        else:
+            pos -= nums[j]
+            if pos <= 0:
+                result += (-pos)//L
+                pos = -pos
+                pos %= L
+                pos = -pos
+
+
+
+
+    print(f"Case #{test}: {result}")
+    test += 1
+    # print(nums, dirs)
