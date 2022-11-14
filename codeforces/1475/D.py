fef0854b69e5703e0eeb385436a4c85dd98e017b
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-14 17:33:21 +0100

    solve D

--- codeforces/1475/D.py
+++ codeforces/1475/D.py
@@ -3,6 +3,14 @@ from queue import deque
 from heapq import heappop, heappush
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+def cum_sum(nums):
+    curr = 0
+    result = [0]*len(nums)
+    for idx, num in enumerate(nums):
+        curr += num
+        result[idx] = curr
+    return result
+
 for i in range(1, len(lines), 3):
     n, m = map(int, lines[i].split(" "))
     memory = list(map(int, lines[i+1].split(" ")))
@@ -10,32 +18,37 @@ for i in range(1, len(lines), 3):
     if sum(memory) < m:
         print(-1)
         continue
-    xs = []
+    ones = []
+    twos = []
     for j in range(len(memory)):
-        xs.append((memory[j], memory[j]/convenience[j], convenience[j], j))
-    xs.sort(key=lambda x: (-x[1], x[2]))
-    dq = deque(xs)
+        if convenience[j] == 1:
+            ones.append(memory[j])
+        else:
+            twos.append(memory[j])
+    ones.sort(key=lambda x: -x)
+    twos.sort(key=lambda x: -x)
+    dq1 = deque(ones)
+    dq2 = deque(twos)
     gotten = 0
     result = 0
-    taken = set()
-    hq = []
-    for j in range(len(memory)):
-        # if convenience[j] == 2: continue
-        hq.append((-memory[j], convenience[j], j))
     while gotten < m:
-        good = False
-        if dq[0][0] + gotten < m:
-            while hq and -hq[0][0] + gotten >= m:
-                mem, con, idx = heappop(hq)
-                if idx in taken: continue 
-                result += con
-                good = True
-                break
-        if good:
+        if not dq1:
+            while gotten < m:
+                result+=2
+                gotten += dq2.popleft()
             break
-        inc, _, con, j = dq.popleft()
-        gotten += inc
-        result += con
-        taken.add(j)
-    # print(xs)
+        elif not dq2:
+            while gotten < m:
+                result+=1
+                gotten += dq1.popleft()
+            break
+        if dq1[0] + gotten >= m:
+            result += 1
+            break
+        if len(dq1) >= 2 and dq1[0] + dq1[1] >= dq2[0]:
+            result += 1
+            gotten += dq1.popleft()
+        else:
+            result += 2
+            gotten += dq2.popleft()
     print(result)
