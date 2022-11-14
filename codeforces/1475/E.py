48b57d64d07eb5e6ba91a551fbafb16b739f12f7
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-14 16:57:23 +0100

    solve E

--- codeforces/1475/E.py
+++ codeforces/1475/E.py
@@ -1,2 +1,22 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+def nCr(n,r):
+    import math
+    return math.factorial(n) // (math.factorial(r)*(math.factorial(n-r)))
+
+for i in range(1, len(lines),2 ):
+    n, k = map(int, lines[i].split(" "))
+    bloggers = list(map(int, lines[i+1].split(" ")))
+    bloggers.sort(key=lambda x: -x)
+    last = bloggers[0]
+    lastcount = 1
+    for j in range(1, k):
+        if bloggers[j] != last:
+            lastcount = 1
+            last = bloggers[j]
+        else:
+            lastcount += 1
+    amount = sum(1 for x in bloggers if x == last)
+    c = lastcount
+    print(nCr(amount, c) % (10**9+7))
