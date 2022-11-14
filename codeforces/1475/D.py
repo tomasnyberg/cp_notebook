fdb249c0e2bfa23d2ca57cee87a454799b4e39e2
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-14 16:47:59 +0100

    closer on D but still so far

--- codeforces/1475/D.py
+++ codeforces/1475/D.py
@@ -1,5 +1,6 @@
 import sys
 from queue import deque
+from heapq import heappop, heappush
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 for i in range(1, len(lines), 3):
@@ -11,14 +12,30 @@ for i in range(1, len(lines), 3):
         continue
     xs = []
     for j in range(len(memory)):
-        xs.append((memory[j], memory[j]/convenience[j], convenience[j]))
+        xs.append((memory[j], memory[j]/convenience[j], convenience[j], j))
     xs.sort(key=lambda x: (-x[1], x[2]))
     dq = deque(xs)
     gotten = 0
     result = 0
+    taken = set()
+    hq = []
+    for j in range(len(memory)):
+        # if convenience[j] == 2: continue
+        hq.append((-memory[j], convenience[j], j))
     while gotten < m:
-        inc, _, con = dq.popleft()
+        good = False
+        if dq[0][0] + gotten < m:
+            while hq and -hq[0][0] + gotten >= m:
+                mem, con, idx = heappop(hq)
+                if idx in taken: continue 
+                result += con
+                good = True
+                break
+        if good:
+            break
+        inc, _, con, j = dq.popleft()
         gotten += inc
         result += con
-    print(xs)
+        taken.add(j)
+    # print(xs)
     print(result)
