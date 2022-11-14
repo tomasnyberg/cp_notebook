02fa13d37e27142f5e1937881acd977978121973
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-14 16:13:54 +0100

    solve B

--- codeforces/1475/B.py
+++ codeforces/1475/B.py
@@ -1,2 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+possible = set()
+for i in range(1000):
+    for j in range(1000):
+        if i*2020 + j*2021 > 2020*2021 - 2020-2021:
+            break
+        possible.add(i*2020 + j*2021)
+for line in lines[1:]:
+    n = int(line)
+    if n >= 2020*2021 - 2020-2021 or n in possible:
+        print("YES")
+    else:
+        print("NO")
