52144bb1ccca59b9f84784e0c2d42d7d24adeed9
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-14 16:11:18 +0100

    solve A (finally)

--- codeforces/1475/A.py
+++ codeforces/1475/A.py
@@ -1,2 +1,13 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    n = int(line)
+    good = False
+    while n != 1:
+        if n % 2 == 1:
+            good = True
+            break
+        n //= 2
+    print("YES" if good else "NO")
+        
