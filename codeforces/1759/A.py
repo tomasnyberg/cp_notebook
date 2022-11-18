b2f907c768cc8d2e41d99039348539adedcf73d0
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-18 15:39:34 +0100

    solve A

--- codeforces/1759/A.py
+++ codeforces/1759/A.py
@@ -1,2 +1,23 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    if line[0] not in ['Y', 'e', 's']:
+        print("NO")
+        continue
+    idx = -1
+    expect = "Yes"
+    if line[0] == 'Y':
+        idx = 1
+    if line[0] == 'e':
+        idx = 2
+    if line[0] == 's':
+        idx = 0
+    bad = False
+    for c in line[1:]:
+        if c != expect[idx]:
+            bad = True
+            break 
+        idx+=1
+        idx %= 3
+    print("NO" if bad else "YES")
