97484f861653ffe4d77d798a1398ad4d871be075
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-18 16:11:40 +0100

    attempt C, WA2

--- codeforces/1759/C.py
+++ codeforces/1759/C.py
@@ -1,2 +1,26 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+# def solve(low, high):
+
+
+for i in range(1, len(lines), 2):
+    l, r, x = map(int, lines[i].split(" "))
+    a, b = map(int, lines[i+1].split(" "))
+    # Always change the low to the high
+    low = min(a,b)
+    high = max(a,b)
+    if high == low:
+        print(0)
+        continue
+    if (high + x > r and high - x < l) or (low + x > r and low - x < l):
+        print(-1)
+        continue
+    if high - low >= x:
+        print(1)
+        continue
+    if r - high <= x:
+        print(3)
+    else:
+        print(2)
+    # print("dunno")
