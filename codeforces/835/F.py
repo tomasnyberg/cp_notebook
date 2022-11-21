18917377910f1880b8f0a33cfcff7705ef5842e5
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-21 16:05:31 +0100

    working on F

--- codeforces/835/F.py
+++ codeforces/835/F.py
@@ -1,2 +1,24 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+def cum_sum(nums):
+    curr = 0
+    result = [0]*len(nums)
+    for idx, num in enumerate(nums):
+        curr += num
+        result[idx] = curr
+    return result
+
+for i in range(1, len(lines), 2):
+    n, c, d = map(int, lines[i].split(" "))
+    nums = list(map(int, lines[i+1].split(" ")))
+    nums.sort(key=lambda x: -x)
+    CS = cum_sum(nums)
+    if CS[d-1 if d-1 < len(CS) else -1] >= c:
+        print("Infinity")
+        continue
+    if nums[0] * d < c:
+        print("Impossible")
+        continue
+    print(0)
+    
