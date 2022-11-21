c66c2ce2aa3d276674363d0421805075c0ed9556
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-21 15:51:34 +0100

    attempt D

--- codeforces/835/D.py
+++ codeforces/835/D.py
@@ -1,2 +1,16 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split(" ")))
+    increasing = False
+    bad = False
+    for i in range(len(nums)-1):
+        if nums[i+1] > nums[i] and not increasing:
+            increasing = True
+        if nums[i+1] < nums[i] and increasing:
+            bad = True
+            break
+    print("NO" if bad else "YES")
+
+
