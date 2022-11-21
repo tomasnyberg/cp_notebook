c5bf5267e8226b59c63a42ac79c453b07a909289
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-21 15:38:36 +0100

    solve B

--- codeforces/835/B.py
+++ codeforces/835/B.py
@@ -1,2 +1,9 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    listed = list(map(lambda x: ord(x) - 96, line))
+    # print(line)
+    # print(listed)
+
+    print(max(listed))
