473a9596a8bd5085ffb616ed8d3db126ef95dbfc
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-21 15:42:30 +0100

    solve C

--- codeforces/835/C.py
+++ codeforces/835/C.py
@@ -1,2 +1,13 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split(" ")))
+    withidx = list(enumerate(nums))
+    withidx.sort(key=lambda x: x[1])
+    for idx, x in enumerate(nums):
+        if idx == withidx[-1][0]:
+            print(x - withidx[-2][1], end=" ")
+        else:
+            print(x - withidx[-1][1], end=" ")
+    print()
