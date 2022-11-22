99358fd0d60ccf98ffc8bb72446890662f902626
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-22 17:35:03 +0100

    WA2 for some reason, trying to figure it out

--- codeforces/835/G.cpp
+++ codeforces/835/G.cpp
@@ -13,6 +13,23 @@ void dfs_b(int curr, int score ,map<int, vector<pair<int, int>>> &adj_lists, set
     }
 }
 
+bool dfs_a(int curr, int score, map<int, vector<pair<int, int>>> &adj_lists, set<int> &bsums, set<int> &aseen, int b){
+    aseen.insert(curr);
+    if(bsums.find(score) != bsums.end()){
+        return true;
+    }
+    for(auto &p: adj_lists[curr]){
+        if(p.first == b){
+            if(score^p.second == 0) return true;
+        } else {
+            if(aseen.find(p.first) != aseen.end()) continue;
+            if(bsums.find(score^p.second) != bsums.end()) return true;
+            if(dfs_a(p.first, score^p.second, adj_lists, bsums, aseen, b)) return true;
+        }
+    }
+    return false;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
@@ -31,11 +48,27 @@ int main() {
         set<int> bsums;
         set<int> bseen;
         dfs_b(b, 0, adj_lists, bsums, bseen);
-        // Print every value in the bsums set
+        set<int> aseen;
+        if(dfs_a(a, 0, adj_lists, bsums, aseen, b)){
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+        // Print every element in bsums
         for(auto &x: bsums){
             cout << x << " ";
         }
-        cout << "\n";
+        cout << endl;
+        // Print every element in aseen
+        for(auto &x: aseen){
+            cout << x << " ";
+        }
+        cout << endl;
+        // Print every element in bseen
+        for(auto &x: bseen){
+            cout << x << " ";
+        }
+        cout << endl;
     }
     return 0;
 }
