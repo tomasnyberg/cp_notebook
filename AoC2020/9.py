ce8eaeffedd83c72f70ba28ad68870b93d398830
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-28 21:08:39 +0100

    clean up day 9

--- AoC2020/9.py
+++ AoC2020/9.py
@@ -5,33 +5,31 @@ from queue import deque
 nums = []
 for line in lines:
     nums.append(int(line))
-    
-def check(dq, target):
-    for i in range(len(dq)):
-        for j in range(len(dq)):
-            if i != j and dq[i] + dq[j] == target:
-                return True
-    return False
 
-dq = deque()
-for i in range(25):
-    dq.append(nums[i])
-result = 0
-for i in range(25, len(nums)):
-    if not check(dq, nums[i]):
-        print(nums[i])
-        break
-        result += 1
-    dq.popleft()
-    dq.append(nums[i])
+def part_one():
+    def check(dq, target):
+        for i in range(len(dq)):
+            for j in range(len(dq)):
+                if i != j and dq[i] + dq[j] == target:
+                    return True
+        return False
+    dq = deque()
+    for i in range(25):
+        dq.append(nums[i])
+    for i in range(25, len(nums)):
+        if not check(dq, nums[i]):
+            return nums[i]
+        dq.popleft()
+        dq.append(nums[i])
 
-broken = False
-for i in range(2, len(nums)):
-    if broken: break
-    for j in range(i, len(nums)):
-        curr = sum(nums[j-i:j])
-        if curr == 41682220:
-            print(min(nums[j-i:j]) + max(nums[j-i:j]))
-            broken = True
-            break
-    curr = nums[i-2:2]
+def part_two():
+    for i in range(2, len(nums)):
+        for j in range(i, len(nums)):
+            curr = sum(nums[j-i:j])
+            if curr == 41682220:
+                return (min(nums[j-i:j]) + max(nums[j-i:j]))
+        curr = nums[i-2:2]
+
+part_one_ans = part_one()
+print("Part one:", part_one_ans)
+print("Part two:", part_two())
