602005f7467a6d08ac6026575cdb776d6b9cc939
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-28 21:36:17 +0100

    clean up day 10 2020

--- AoC2020/10.py
+++ AoC2020/10.py
@@ -6,29 +6,28 @@ for line in lines:
     nums.append(int(line))
 nums.sort()
 nums.append(nums[-1] + 3)
-ones = 0
-threes = 0
-for i in range(1, len(nums)):
-    if nums[i] - nums[i-1] == 1:
-        ones += 1
-    elif nums[i] - nums[i-1] == 3:
-        threes += 1
-print(threes, ones)
-print(threes * ones)
 
-print(len(nums))
-dp = [-1]*len(nums)
-def dfs(i):
-    if i == len(nums) - 1:
-        return 1
-    if dp[i] != -1:
-        return dp[i]
-    total = 0
-    idx = i+1
-    while idx < len(nums) and nums[idx] - nums[i] <= 3:
-        total += dfs(idx)
-        idx += 1
-    dp[i] = total
-    return total
+def part_one():
+    counts = {0: 0, 1: 0, 2:0, 3:0}
+    for i in range(1, len(nums)):
+        counts[nums[i] - nums[i-1]] +=1
+    return (counts[3] * counts[1])
 
-print(dfs(0)) 
+def part_two():
+    dp = [-1]*len(nums)
+    def dfs(i):
+        if i == len(nums) - 1:
+            return 1
+        if dp[i] != -1:
+            return dp[i]
+        total = 0
+        idx = i+1
+        while idx < len(nums) and nums[idx] - nums[i] <= 3:
+            total += dfs(idx)
+            idx += 1
+        dp[i] = total
+        return total
+    return dfs(0)
+
+print("Part one:", part_one())
+print("Part two:", part_two())
