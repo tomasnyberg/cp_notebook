5d2c269a0d4ce184e3ca0e61c2ccd48b5fd82526
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-28 17:00:34 +0100

    clean up day 7

--- AoC2020/7.py
+++ AoC2020/7.py
@@ -4,8 +4,15 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 def dfs(curr):
     if curr == "shinygold": return True
     for nbr in adj_lists[curr]:
-        if dfs(nbr):
-            return True
+        if dfs(nbr): return True
+    return False
+
+def part_two(curr):
+    result = 0
+    for nbr in adj_lists[curr]:
+        to_add = adj_lists[curr][nbr] + adj_lists[curr][nbr]*part_two(nbr)
+        result += to_add
+    return result
 
 adj_lists = {}
 for line in lines:
@@ -18,10 +25,13 @@ for line in lines:
         count = split[0]
         if count == "no": continue
         to = split[1] + split[2]
-        adj_lists[fr][to] = count
+        adj_lists[fr][to] = int(count)
+
 
 result = 0
 for key in adj_lists:
     if key == "shinygold": continue
     if dfs(key): result +=1
-print(result)
+
+print("Part one:", result)
+print("Part one:", part_two("shinygold"))
