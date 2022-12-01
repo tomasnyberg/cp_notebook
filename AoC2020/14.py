fa547f9aa489c198aea6d2b97e5070f170f2107e
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-01 21:27:26 +0100

    solve day 14 2020

--- AoC2020/14.py
+++ AoC2020/14.py
@@ -3,7 +3,6 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 
 def apply_mask(mask, num):
     s = mask[::-1]
-    print(s)
     for i in range(len(s)):
         if s[i] == 'X':
             continue
@@ -14,6 +13,26 @@ def apply_mask(mask, num):
             num |= (1 << i)
     return num
 
+def generate_all_memory(mask, address):
+    s = mask[::-1]
+    for i in range(len(s)):
+        if s[i] == '1':
+            address |= (1 << i)
+    result = set([address])
+    for i in range(len(s)):
+        print(len(result))
+        if s[i] == 'X':
+            to_add = set()
+            for sub in result:
+                if sub & (1 << i):
+                    to_add.add(sub & (~(1 << i)))
+                else:
+                    to_add.add(sub | (1 << i))
+            result |= to_add
+    return result
+new = generate_all_memory("000000000000000000000000000000X1001X", 42)
+print(list((x) for x in new))
+
 currmask = ''
 mem = {}
 for line in lines:
@@ -23,7 +42,11 @@ for line in lines:
     else:
         addressbit = int(split[0][4:-1])
         value = int(split[-1])
-        mem[addressbit] = apply_mask(currmask, value)
+        addresses = generate_all_memory(currmask, addressbit)
+        print(len(addresses))
+        for address in addresses:
+            mem[address] = value
+        # mem[addressbit] = apply_mask(currmask, value)
 
 result = 0
 for key,val in mem.items():
