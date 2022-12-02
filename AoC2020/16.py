b696283f5fb5e4ab897d31d8201af315721ef094
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-02 21:14:59 +0100

    close to solve day 16

--- AoC2020/16.py
+++ AoC2020/16.py
@@ -1,20 +1,24 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-def input_range(s, ranges):
+def input_range(s, ranges, field, range_dict):
     split= s.split("-")
-    ranges.append([int(split[0]), int(split[1])])
+    ranges.append([int(split[0]), int(split[1]), field])
+    if field not in range_dict:
+        range_dict[field] = []
+    range_dict[field].append([int(split[0]), int(split[1])])
 
+rd = {}
 
 ranges = []
 i = 0
 while lines[i] != "":
     curr= lines[i]
     split = curr.split(" ")
-    print(split[-1], split[-3])
+    field = curr.split(":")[0]
     a, b= (split[-1], split[-3])
-    input_range(a, ranges)
-    input_range(b, ranges)
+    input_range(a, ranges, field, rd)
+    input_range(b, ranges, field, rd)
     i+=1
 i+=1
 while lines[i] != "":
@@ -26,14 +30,19 @@ while i < len(lines):
     tickets.append(list(map(int, lines[i].split(","))))
     i+=1
 
+
+possible = []
+for j in range(len(tickets[0])):
+    possible.append(set())
+
 def check_ticket(ticket, ranges):
     for j in range(len(ticket)):
         curr = ticket[j]
         good = False
-        for start, end in ranges:
+        for start, end, field in ranges:
             if start <= curr <= end:
                 good = True
-                break
+                possible[j].add(curr)
         if not good:
             ticket[j] = -ticket[j]
 
@@ -43,7 +52,38 @@ for ticket in tickets:
     check_ticket(ticket, ranges)
     if all(x >= 0 for x in ticket):
         new_tickets.append(ticket)
-print(new_tickets)
-    
+
+print(rd)
+result = []
+for j in range(len(possible)):
+    result.append(set())
+for j in range(len(possible)):
+    s = possible[j]
+    for field, rngs in rd.items():
+        good = True
+        for x in s:
+            if not any(start <= x <= end for start, end in rngs):
+                good = False
+                break
+        if good:
+            result[j].add(field)
+taken = set()
+while True:
+    if all(len(s) == 1 for s in result):
+        break
+    for j in range(len(result)):
+        if len(result[j]) == 1:
+            taken |= result[j]
+            continue
+        for x in taken:
+            if x in result[j]:
+                result[j].remove(x)
+
+mul = 1
+print(result)
+        
+        
+
+
 
 
