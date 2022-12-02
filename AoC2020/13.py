35be45a28a6e64c840458f12f8c28525b89e2576
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-02 15:17:58 +0100

    clean up day 13 2020

--- AoC2020/13.py
+++ AoC2020/13.py
@@ -10,14 +10,29 @@ for i in range(len(split)):
         times.append(b)
     else:
         times.append(-1)
-print(times)
-step = 1
-n = 0
-for i in range(len(times)):
-    x = times[i]
-    if x == -1:
-        continue
-    while (n + i) % x != 0:
-        n += step
-    step *= x
-print(n)
+
+def part_one():
+    num = int(lines[0])
+    smallest = [10**9, -1]
+    for i in range(len(times)):
+        if times[i] == -1:
+            continue
+        wait = (math.ceil(num / times[i]) * times[i]) - num
+        if wait < smallest[0]:
+            smallest = [wait, times[i]]
+    return smallest[0] * smallest[1]
+
+def part_two():
+    step = 1
+    n = 0
+    for i in range(len(times)):
+        x = times[i]
+        if x == -1:
+            continue
+        while (n + i) % x != 0:
+            n += step
+        step *= x
+    return n
+
+print("Part one:", part_one())
+print("Part two:", part_two())
