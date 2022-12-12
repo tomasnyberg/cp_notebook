7afed8859ef9961d2c234b61e332c741042cc85c
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-12 17:58:49 +0100

    solve A

--- codeforces/693/A.py
+++ codeforces/693/A.py
@@ -1,2 +1,13 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    w, h, n= map(int, line.split(" "))
+    sheets = 1
+    while w % 2 == 0:
+        w //= 2
+        sheets *= 2
+    while h % 2 == 0:
+        h //= 2
+        sheets *=2
+    print("YES" if sheets >= n else "NO")
