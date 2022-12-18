accdd0b0bf838ceb0fdcf80c51064040611c5bad
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-12-18 15:57:12 +0100

    solve C

--- codeforces/839/C.py
+++ codeforces/839/C.py
@@ -1,2 +1,18 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    k, n = map(int, line.split())
+    gap = 1
+    curr = 1
+    result = [1]
+    k-=1
+    while k:
+        if curr + gap <= n - k + 1:
+            curr += gap
+            result.append(curr)
+        else:
+            result.append(result[-1] + 1)
+        gap += 1
+        k-=1
+    print(*result)
