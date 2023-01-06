feb5f0e41f6664b814c437a52ce301031a9d8979
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-06 09:25:47 +0100

    cleanup 1767D

--- codeforces/1767D.py
+++ codeforces/1767D.py
@@ -1,16 +1,11 @@
 import sys
-from collections import deque
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-n = int(lines[0])
-s = lines[1]
+n, s = int(lines[0]), lines[1]
 
-ones = sum(1 for c in s if c == '1')
-zeroes = sum(1 for c in s if c == '0')
-a = 2**ones
-b = 2**zeroes
+a = 2**sum(1 for c in s if c == '1')
+b = 2**sum(1 for c in s if c == '0')
 for i in range(1, 2**n + 1):
-    # print(i - a, 2**n - i - 1, "for number", i)
     if i - a >= 0 and 2**n - b - i + 1 >= 0:
         print(i, end=" ")
 print()
