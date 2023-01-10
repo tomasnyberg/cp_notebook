56dfef22a3cc4771123c3255853b4bae2ee998fa
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-10 19:53:02 +0100

    solve A

--- codeforces/1766/A.py
+++ codeforces/1766/A.py
@@ -1,2 +1,12 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for x in lines[1:]:
+    x = int(x)
+    count = 0
+    while x > 10:
+        count += 9
+        x //= 10
+    count += x
+    print(count)
+        
