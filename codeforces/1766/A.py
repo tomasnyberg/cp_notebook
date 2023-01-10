239ae27416709a1ac64bd8f1facb82b72df80a1b
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-10 19:54:32 +0100

    redo solution of A, was quite a bit simpler

--- codeforces/1766/A.py
+++ codeforces/1766/A.py
@@ -2,11 +2,4 @@ import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 for x in lines[1:]:
-    x = int(x)
-    count = 0
-    while x > 10:
-        count += 9
-        x //= 10
-    count += x
-    print(count)
-        
+    print(9 * (len(x) - 1) + int(x[0]))
