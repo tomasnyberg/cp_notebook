9ad590ba7b35151927554f262daf5601965384e1
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-12 17:43:18 +0100

    attempt D, missing something

--- codeforces/1766/D.py
+++ codeforces/1766/D.py
@@ -1,2 +1,36 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+for line in lines[1:]:
+    a, b = map(int, line.split())
+    if abs(a - b) == 1:
+        print(-1)
+        continue
+    if gcd(a, b) != 1:
+        print(0)
+        continue
+    a, b = min(a, b), max(a, b)
+    ps = prime_factors(b-a)
+    ans = 10**9
+    for p in ps:
+        ans = min(ans, p-(a%p))
+    print(ans)
+
