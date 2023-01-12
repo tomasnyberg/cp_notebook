59d45112345d003e378219966bc484ca051813a6
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-12 16:10:32 +0100

    solve C

--- codeforces/1766/C.py
+++ codeforces/1766/C.py
@@ -1,26 +1,36 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+def dfs(matrix, i, j, visited, blacks):
+    if matrix[i][j] != 'B':
+        return False
+    visited.add((i, j))
+    while True:
+        good = False
+        for di, dj in [[1,0], [-1, 0], [0,1]]:
+            if 0 <= i+di < len(matrix) and 0 <= j+dj < len(matrix[0]) and (i+di, j+dj) not in visited and matrix[i+di][j+dj] == 'B':
+                good = True
+                i += di
+                j += dj
+                break
+        visited.add((i, j))
+        if len(visited) == blacks:
+            return True
+        if not good: return False
+
 for i in range(1, len(lines), 3):
     n = int(lines[i])
     matrix = [lines[i+1], lines[i+2]]
-    bad = False
-    sums = []
-    for j in range(n):
-        tot = 0
-        for k in range(2):
-            tot += 1 if matrix[k][j] == 'B' else 0
-        if tot == 0:
-            bad = True 
-        sums.append(tot)
-    for j in range(1, n - 1):
-        if sums[j] == 2 and sums[j-1] == 1 and sums[j+1] == 1:
-            bad = True
-    for j in range(n-1):
-        if sums[j] == 1 and sums[j+1] == 1:
-            if matrix[j][0] != matrix[j+1][0]:
-                bad = True
-    print("YES" if not bad else "NO")
+    blacks = 0
+    for row in matrix:
+        for c in row:
+            if c == 'B':
+                blacks += 1
+    a = dfs(matrix, 0, 0, set(), blacks)
+    b = dfs(matrix, 1, 0, set(), blacks)
+    print("YES" if a or b else "NO")
+
+
 
     
     
