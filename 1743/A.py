11203096de8f6e05d3f6e46a2c046468ca0b3b9f
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-15 14:26:00 +0100

    redo A solution

--- 1743/A.py
+++ 1743/A.py
@@ -1,2 +1,9 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+def nCr(n,r):
+    import math
+    return math.factorial(n) // (math.factorial(r)*(math.factorial(n-r)))
+
+for n in lines[1::2]:
+    print(6*nCr(10 - int(n), 2))
