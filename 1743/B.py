5b037785abd418eed222d4188830ae0ba2219ce5
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-15 14:37:31 +0100

    solve B

--- 1743/B.py
+++ 1743/B.py
@@ -1,2 +1,17 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    n = int(line)
+    left = 1
+    right = n
+    result = []
+    while left < right:
+        result.append(left)
+        left += 1
+        result.append(right)
+        right -= 1
+    if n % 2 == 1:
+        result.append(left)
+    print(*result)
+
