3b6f0bfdf2bb7baff92da78b2567b2ba0228b57f
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-15 14:39:23 +0100

    redo solution slighlty

--- 1743/B.py
+++ 1743/B.py
@@ -2,16 +2,4 @@ import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 for line in lines[1:]:
-    n = int(line)
-    left = 1
-    right = n
-    result = []
-    while left < right:
-        result.append(left)
-        left += 1
-        result.append(right)
-        right -= 1
-    if n % 2 == 1:
-        result.append(left)
-    print(*result)
-
+    print(*([1,int(line)] + [i for i in range(2,int(line))]))
