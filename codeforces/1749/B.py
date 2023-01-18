ec81cf62e42f0a7822855e17418a82cb35190645
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-18 20:04:07 +0100

    solve B 1749

--- codeforces/1749/B.py
+++ codeforces/1749/B.py
@@ -1,2 +1,8 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for i in range(1, len(lines), 3):
+    n = int(lines[i])
+    a = list(map(int, lines[i+1].split()))
+    b = list(map(int, lines[i+2].split()))
+    print(sum(a) + sum(b) - max(b))
