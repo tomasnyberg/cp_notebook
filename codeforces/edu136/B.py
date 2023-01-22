7b2c9332796e20b26ee180ed28c82f91b3d3380e
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-22 21:22:46 +0100

    redo solution for B

--- codeforces/edu136/B.py
+++ codeforces/edu136/B.py
@@ -4,21 +4,11 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 
 for line in lines[2::2]:
     nums = list(map(int, line.split()))
-    ends = set()
-    @lru_cache(None)
-    def dfs(i, taken):
-        if len(ends) > 1:
-            return
-        if i == len(nums):
-            ends.add(taken)
-            return 
-        a = nums[i] + taken[-1]
-        b = taken[-1] - nums[i]
-        if a >= 0:
-            dfs(i+1, taken + (a,))
-        if b >= 0:
-            dfs(i+1, taken + (b,))
-        return 
-    dfs(1, (nums[0],))
-    # print(ends)
-    print(*ends.pop() if len(ends) == 1 else [-1])
+    candidate = [nums[0]]
+    for x in nums[1:]:
+        if x != 0 and candidate[-1] - x >= 0:
+            print(-1)
+            break
+        candidate.append(x + candidate[-1])
+    else:
+        print(*candidate)
