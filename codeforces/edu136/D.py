651fb2d171ec9bf8acdd3106ee0d23e32dd00334
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-23 21:32:05 +0100

    working on D edu136 not going great

--- codeforces/edu136/D.py
+++ codeforces/edu136/D.py
@@ -1,2 +1,50 @@
-import sys
-lines = list(map(str.strip, sys.stdin.readlines()))
+from functools import lru_cache
+def solve(n, k, p):
+    p = [0] + p
+    for i in range(1, len(p)):
+        p[i] -= 1
+    # Find all the leaves in the tree given the parent array
+    def find_leaves(p):
+        pointed_to = [0] * len(p)
+        for i in range(1, len(p)):
+            pointed_to[p[i]] += 1
+        leaves = []
+        for i in range(len(p)):
+            if pointed_to[i] == 0:
+                leaves.append(i)
+        return leaves
+    leaves = set(find_leaves(p))
+    def check(parents, mid):
+        parents = parents.copy()
+        def walk_tree(i):
+            icopy = i
+            ops = 0
+            stack = []
+            while i != 0:
+                stack.append(i)
+                if len(stack) == mid and parents[i] != 0:
+                    ops += 1
+                    # print("current stack", stack, "node", i)
+                    stack = []
+                i = parents[i]
+            return ops
+        walks = [walk_tree(i) for i in range(n) if i in leaves]
+        # print(walks)
+        return sum(walks) <= k  
+    low = 1
+    high = n
+    while low < high:
+        mid = (low + high) // 2
+        res = check(p, mid)
+        # print(mid, res)
+        if res:
+            high = mid
+        else:
+            low = mid + 1
+    return low
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    p = list(map(int, input().split()))
+    print(solve(n, k, p))
