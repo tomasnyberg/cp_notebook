0e87977fc89dae3ea1adf95665b1a858486e2575
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-23 19:55:09 +0100

    solve C with my own solution instaed

--- codeforces/edu136/C.py
+++ codeforces/edu136/C.py
@@ -9,13 +9,35 @@ def nCr(n,r):
     import math
     return math.factorial(n) // (math.factorial(r)*(math.factorial(n-r)))
 
-def calc(n):
-    if n == 2:
-        return [1, 0, 1]
-    a = calc(n-2)
-    return [nCr(n-1, n//2) + a[1], nCr(n-2, n//2) + a[0], 1] 
-
 for line in lines[1:]:
     n = int(line)
-    print(*list(map(lambda x: x % MOD, calc(n))))
+    template = "AABB"
+    idx = 3
+    draw_line = ""
+    for _ in range(n):
+        draw_line += template[idx]
+        idx = (idx + 1) % 4
+    assert(len(draw_line) == n)
+    a_win = 0
+    b_win = 0
+    a_remaining = n // 2
+    for i in range(n):
+        if not a_remaining:
+            break
+        remaining = n - i - 1
+        if draw_line[i] == 'A':
+            a_remaining -= 1
+        else:
+            a_win += nCr(remaining, a_remaining-1)
+    b_remaining = n // 2
+    for i in range(n):
+        if not b_remaining:
+            break
+        remaining = n - i - 1
+        if draw_line[i] == 'B':
+            b_remaining -= 1
+        else:
+            b_win += nCr(remaining, b_remaining-1)
+    print(a_win % MOD, b_win % MOD, 1)
+
 
