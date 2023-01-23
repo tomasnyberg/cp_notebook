9e7ff7294b1180790703d06dfd7eb0521526a31e
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-23 19:56:58 +0100

    clean up that solution

--- codeforces/edu136/C.py
+++ codeforces/edu136/C.py
@@ -1,12 +1,9 @@
-import sys
-import itertools
-import math
+import sys, math
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 MOD = 998244353
 
 def nCr(n,r):
-    import math
     return math.factorial(n) // (math.factorial(r)*(math.factorial(n-r)))
 
 for line in lines[1:]:
@@ -21,23 +18,17 @@ for line in lines[1:]:
     a_win = 0
     b_win = 0
     a_remaining = n // 2
+    b_remaining = n // 2
     for i in range(n):
-        if not a_remaining:
+        if not a_remaining or not b_remaining:
             break
         remaining = n - i - 1
         if draw_line[i] == 'A':
             a_remaining -= 1
-        else:
-            a_win += nCr(remaining, a_remaining-1)
-    b_remaining = n // 2
-    for i in range(n):
-        if not b_remaining:
-            break
-        remaining = n - i - 1
-        if draw_line[i] == 'B':
-            b_remaining -= 1
-        else:
             b_win += nCr(remaining, b_remaining-1)
+        else:
+            b_remaining -= 1
+            a_win += nCr(remaining, a_remaining-1)
     print(a_win % MOD, b_win % MOD, 1)
 
 
