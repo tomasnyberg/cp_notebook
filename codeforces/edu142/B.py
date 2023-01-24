1f3d5eb43c9418295a3a16aad8be4de6b50c9336
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-24 15:49:24 +0100

    solve B

--- codeforces/edu142/B.py
+++ codeforces/edu142/B.py
@@ -1,2 +1,20 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    both, alice, bob, neither = map(int, line.split())
+    if both == 0:
+        print(1 if alice + bob + neither > 0 else 0)
+        continue
+    result = both
+    result += min(alice, bob)*2
+    small = min(alice, bob)
+    alice -= small
+    bob -= small
+    remaining = alice + bob + neither
+    result += min(both + 1, remaining)
+    # print()
+    # print(both, alice, bob, neither)
+    print(result)
+    # print()
+    
