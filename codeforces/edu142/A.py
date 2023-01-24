0842c6ef8507bc2848be8990d4a3db8d246c1b59
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-24 15:36:57 +0100

    solve A

--- codeforces/edu142/A.py
+++ codeforces/edu142/A.py
@@ -1,3 +1,9 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+for line in lines[2::2]:
+    nums = list(map(int, line.split()))
+    one_count = nums.count(1)
+    result = len(nums)
+    result -= one_count//2
+    print(result)
