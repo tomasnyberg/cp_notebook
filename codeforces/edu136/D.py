9a2970ef0415ae425d6520fc9a6d58fd6a5d5f13
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-26 21:54:46 +0100

    upsolve D edu136

--- codeforces/edu136/D.py
+++ codeforces/edu136/D.py
@@ -2,52 +2,26 @@ from functools import lru_cache
 
 def solve(n, k, p):
     p = [0] + p
-    for i in range(1, len(p)):
+    for i in range(1, n):
         p[i] -= 1
-    # Find all the leaves in the tree given the parent array
-    def find_leaves(p):
-        pointed_to = [0] * len(p)
-        for i in range(1, len(p)):
-            pointed_to[p[i]] += 1
-        leaves = []
-        for i in range(len(p)):
-            if pointed_to[i] == 0:
-                leaves.append(i)
-        return leaves
-    leaves = set(find_leaves(p))
-    # print(p)
-    def check(parents, mid):
-        parents = parents.copy()
-        removed = set()
-        def walk_tree(i):
-            ops = 0
-            stack = []
-            while i != 0:
-                if i in removed:
-                    return ops
-                stack.append(i)
-                if len(stack) == mid and parents[i] != 0:
-                    for j in stack:
-                        removed.add(j)
-                    ops += 1
-                    # print("current stack", stack, "node", i, "parent", parents[i])
-                    stack = []
-                i = parents[i]
-            return ops
-        walks = [walk_tree(i) for i in range(n) if i in leaves]
-        # print(walks)
-        return sum(walks) <= k  
-    low = 1
-    high = n
+    
+    def check(h):
+        d = [1] * n
+        ops = 0
+        for i in range(n-1, 0, -1):
+            if d[i] == h and p[i] != 0:
+                ops += 1
+            else:
+                d[p[i]] = max(d[p[i]], d[i] + 1)
+        return ops <= k
+    
+    low, high = 1, n-1
     while low < high:
-        mid = (low + high) // 2
-        res = check(p, mid)
-        # print(mid, res)
-        if res:
+        mid = (low + high) >> 1
+        if check(mid):
             high = mid
         else:
             low = mid + 1
-    # print("----\n\n")
     return low
 
 t = int(input())
