e531950004d0a37311d2acaf3c53d86b4d1d6bc1
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-27 15:38:08 +0100

    solve A

--- codeforces/847/A.py
+++ codeforces/847/A.py
@@ -1,2 +1,13 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+
+pi = "3141592653589793238462643383279"
+for line in lines[1:]:
+    score = 0
+    for i in range(min(len(pi), len(line))):
+        if pi[i] == line[i]:
+            score += 1
+        else:
+            break
+    print(score)
