622b0c2d9884e76d678f7794d76a03663a81fa7d
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-29 16:20:34 +0100

    redo solution to A

--- codeforces/edu135/A.py
+++ codeforces/edu135/A.py
@@ -3,20 +3,9 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 
 for line in lines[2::2]:
     nums = list(map(int, line.split()))
-    remaining = set([i+1 for i in range(len(nums))])
-    while len(remaining) > 1:
-        a = -1
-        b = -1
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[i] -= 1
-                if a == -1:
-                    a = i
-                else:
-                    b = i
-                    break
-        if nums[a] == 0:
-            remaining.remove(a+1)
-        if nums[b] == 0:
-            remaining.remove(b+1)
-    print(remaining.pop())
+    biggest = (-1, -1)
+    for i in range(len(nums)):
+        if nums[i] > biggest[0]:
+            biggest = (nums[i], i+1)
+    print(biggest[1])
+    
