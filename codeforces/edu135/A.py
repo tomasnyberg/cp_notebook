0150df20ace5a8315bb06f10061707e225eb6709
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-01-29 16:15:36 +0100

    solve A

--- codeforces/edu135/A.py
+++ codeforces/edu135/A.py
@@ -1,2 +1,22 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split()))
+    remaining = set([i+1 for i in range(len(nums))])
+    while len(remaining) > 1:
+        a = -1
+        b = -1
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i] -= 1
+                if a == -1:
+                    a = i
+                else:
+                    b = i
+                    break
+        if nums[a] == 0:
+            remaining.remove(a+1)
+        if nums[b] == 0:
+            remaining.remove(b+1)
+    print(remaining.pop())
