92a63083d854cdbac459ff29a3a15504848f791b
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-03 16:55:57 +0100

    solve G1 i think

--- codeforces/849/G.py
+++ codeforces/849/G.py
@@ -1,2 +1,20 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for i in range(1, len(lines), 2):
+    n, k = map(int, lines[i].split())
+    line = lines[i+1]
+    nums = list(map(int, line.split()))
+    for i in range(len(nums)):
+        nums[i] += i + 1
+        nums[i] = (nums[i], i)
+    nums.sort(key=lambda x: (-x[0], x[1]))
+    result = 0 
+    while nums and k >= nums[-1][0]:
+        result += 1
+        cost, idx = nums.pop()
+        # print(cost, idx)
+        k -= cost
+    print(result)
+
+    
