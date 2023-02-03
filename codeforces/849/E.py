fc644e6b9f0426f917bc06cdfdc9551cfc4bf2d0
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-03 15:53:44 +0100

    solve E I think?

--- codeforces/849/E.py
+++ codeforces/849/E.py
@@ -1,2 +1,12 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split()))
+    for i in range(1, len(nums)):
+        a = nums[i]
+        b = nums[i-1]
+        if -a + (-b) > a + b:
+            nums[i] = -a
+            nums[i-1] = -b
+    print(sum(nums))
