66c756745911d8366e47a0b6db6b3d564a5102f9
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-03 15:42:52 +0100

    solve D

--- codeforces/849/D.py
+++ codeforces/849/D.py
@@ -1,2 +1,17 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for s in lines[2::2]:
+    counts = {}
+    for c in s:
+        counts[c] = counts.get(c, 0) + 1
+    othercounts = {}
+    result = 0
+    for c in s:
+        othercounts[c] = othercounts.get(c, 0) + 1
+        counts[c] -= 1
+        if counts[c] == 0:
+            del counts[c]
+        result = max(result, len(counts) + len(othercounts))
+    print(result)
+        
