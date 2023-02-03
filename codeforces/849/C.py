82d82a80215e7c633a7395f33a41d35f870f4d3b
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-03 15:40:29 +0100

    solve C

--- codeforces/849/C.py
+++ codeforces/849/C.py
@@ -1,2 +1,14 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+from collections import deque
+
+for s in lines[2::2]:
+    s = deque(s)
+    while len(s) > 1:
+        if s[0] != s[-1]:
+            s.popleft()
+            s.pop()
+        else:
+            break
+    print(len(s))
+     
