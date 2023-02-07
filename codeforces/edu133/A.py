49f1c89a118909e6ad2bf912eb74c55e664ac8a4
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-07 16:23:30 +0100

    solve A

--- codeforces/edu133/A.py
+++ codeforces/edu133/A.py
@@ -1,2 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    n = int(line)
+    mod = n % 3
+    if n == 1:
+        print(2)
+    elif n == 4:
+        print(2)
+    elif mod == 0:
+        print(n//3)
+    else:
+        print(n// 3 + 1)
+        
