7a68d793d441bbf7b781d2adb46b2e54942ac2d3
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-07 16:28:19 +0100

    solve B

--- codeforces/edu133/B.py
+++ codeforces/edu133/B.py
@@ -1,2 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    n = int(line)
+    perms = [[i+1 for i in range(n)]]
+    i = 0
+    for _ in range(n-1):
+        copy = perms[-1][:]
+        copy[i], copy[i+1] = copy[i+1], copy[i]
+        perms.append(copy)
+        i+=1
+    print(len(perms))
+    for perm in perms:
+        print(*perm)
