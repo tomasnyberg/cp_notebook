52692734f5a02c644a5a4d3b7f5ebda1b7dd8135
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-08 03:10:24 +0100

    add script for creating c++ template

---
+++ newC.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: newC <filename>, creates a new C++ file with a template"
+    exit 1
+fi
+filename=$1.cpp
+
+cat > $filename <<EOF
+#include  <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+
+
+int main() {
+    cout << "Hello world\n";
+}
+EOF
