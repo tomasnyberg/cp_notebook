e89401c92c7b71b826c1da62c89ef1ff08aa59d7
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-08 04:36:45 +0100

    redo solution

--- codeforces/835/C.cpp
+++ codeforces/835/C.cpp
@@ -22,36 +22,23 @@ string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a
 string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
 bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
 
-
 int main() {
     int t;
     cin >> t;
     while (t--) {
         int n;
         cin >> n;
-        vector<int> v(n);
-        for (int i=0;i<n;++i) cin >> v[i];
-        // Copy the vector v to a new vector
-        vector<pair<int,int>> v2(n);
-        for (int i=0;i<n;++i){
-            v2[i].first = v[i];
-            v2[i].second = i;
-        };
-        // Sort the new vector
-        sort(v2.begin(), v2.end(), [](const auto &a, const auto &b) {
-            return a.first < b.first;
-        });
-        auto biggest = v2[n-1];
-        auto secondbiggest = v2[n-2];
+        vector<int> a(n);
+        for (int i=0;i<n;++i) cin >> a[i];
+        vector<int> b(a);
+        sort(b.begin(), b.end());
         for(int i = 0; i < n; i++){
-            if(i == biggest.second){
-                cout << biggest.first - secondbiggest.first << " ";
-            } else if (i == secondbiggest.second){
-                cout << secondbiggest.first - biggest.first << " ";
+            if (a[i] == b[n-1]){
+                cout << a[i] - b[n-2] << " ";
             } else {
-                cout << v[i] - biggest.first << " ";
+                cout << a[i] - b[n-1] << " ";
             }
         }
-        cout << endl;
+        cout << "\n";
     }
 }
