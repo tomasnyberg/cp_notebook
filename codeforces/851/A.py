8fca7da55717445d3cd1590d7184ae952088598b
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-09 15:59:26 +0100

    solve A for real this time

--- codeforces/851/A.py
+++ codeforces/851/A.py
@@ -1,21 +1,16 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-def check(k, nums):
-    prod1 = nums[0]
-    for i in range(1, k):
-        prod1 *= nums[i]
-    prod2 = 1
-    for i in range(k, len(nums)):
-        prod2 *= nums[i]
-    # print(k, prod1, prod2)
-    return prod1 == prod2
-
 for line in lines[2::2]:
     nums = list(map(int, line.split()))
-    for k in range(1, len(nums)):
-        if check(k, nums):
-            print(k)
+    prod = 1
+    cum_prod = []
+    for num in nums:
+        prod *= num
+        cum_prod.append(prod)
+    for k in range(len(nums)):
+        if cum_prod[k]**2 == cum_prod[-1]:
+            print(k+1)
             break
     else:
         print(-1)
