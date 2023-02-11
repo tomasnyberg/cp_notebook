7c6987611810a83d768f3881650e8fdcf7a2b22e
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-11 19:26:44 +0100

    solve A

--- codeforces/edu132/A.py
+++ codeforces/edu132/A.py
@@ -1,2 +1,14 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for i in range(1, len(lines), 2):
+    x = int(lines[i])
+    doors = list(map(int, lines[i+1].split()))
+    visited = 0
+    x -= 1
+    for i in range(len(doors)):
+        doors[i] -= 1
+    while x != -1:
+        x = doors[x]
+        visited += 1
+    print("YES" if visited == len(doors) else "NO")
