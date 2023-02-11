c8cf8b8b0b6dcc56cf4e80710f0a45d4476c523a
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-11 20:40:15 +0100

    WA4 on D

--- codeforces/edu132/D.py
+++ codeforces/edu132/D.py
@@ -1,2 +1,74 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+from math import ceil, log2
+
+class segment_tree:
+    # merge(left, right): function used to merge the two halves
+    # basef(value): function applied on individual values
+    # basev: identity for merge function, merger(value, basev) = value
+    # update(node_value, old, new): function to update the nodes
+    def __init__(self, array, merge=lambda x,y:max(x, y), basef=lambda x:x, basev = 0):
+        self.merge = merge
+        self.basef = basef
+        self.basev = basev
+        self.n = len(array)
+        self.array = array
+        self.tree = [0] * ( 2**ceil(log2(len(array))+1) - 1 )
+        self.build(array)
+    
+    def __str__(self):
+        return ' '.join([str(x) for x in self.tree])
+
+    def _build_util(self, l, r, i, a):
+        if(l==r):
+            self.tree[i] = self.basef(a[l])
+            return self.tree[i]
+        mid = (l+r)//2
+        self.tree[i] = self.merge(self._build_util(l,mid, 2*i+1, a), self._build_util(mid+1, r, 2*i+2, a))
+        return self.tree[i]
+
+    def build(self, a):
+        self._build_util(0, len(a)-1, 0, a)
+
+    def _query_util(self, i, ln, rn, l, r):
+        if ln>=l and rn<=r:
+            return self.tree[i]
+        if ln>r or rn<l:
+            return self.basev
+        return self.merge( self._query_util( 2*i+1, ln, (ln+rn)//2, l, r ), self._query_util( 2*i+2, (ln+rn)//2+1, rn, l, r ) )
+
+    def query(self, l, r):
+        return self._query_util( 0, 0, self.n-1, l, r )
+
+    def _update_util(self, i, ln, rn, x, v):
+        if x>=ln and x<=rn:
+            if ln != rn:
+                self._update_util( 2*i+1, ln, (ln+rn)//2, x, v )
+                self._update_util( 2*i+2, (ln+rn)//2 + 1, rn, x, v )
+                self.tree[i] = self.merge(self.tree[2*i+1], self.tree[2*i+2])
+            else:
+                self.tree[i] = self.basef(v)
+
+    def update(self, x, v):
+        self._update_util( 0, 0, self.n-1, x, v )   
+        self.array[x] =v
+
+n, m = map(int, lines[0].split())
+a = list(map(int, lines[1].split()))
+q = int(lines[2])
+queries = [list(map(int, line.split())) for line in lines[3:]]
+st = segment_tree(a)
+for q in queries:
+    sx, sy, ex, ey, k = q # x = row, y = column
+    if abs(sx-ex) % k != 0 or abs(sy-ey) % k != 0:
+        print("NO")
+        continue
+    height = sx + ((n - sx)//k)*k
+    # print("We can at most go to", height, sx, k)
+    highestwall = st.query(sy-1, ey-1)
+    # print("the highest wall in the interval is", highestwall)
+    if highestwall >= height:
+        print("NO")
+    else:
+        print("YES")
