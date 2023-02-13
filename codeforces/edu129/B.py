910b626bbadea8a7e86bab8718e1fd83f717477b
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-13 13:51:57 +0100

    solve B

--- codeforces/edu129/B.py
+++ codeforces/edu129/B.py
@@ -1,2 +1,9 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for i in range(1, len(lines), 4):
+    n = int(lines[i])
+    m = int(lines[i+2])
+    nums = list(map(int, lines[i+1].split()))
+    shuffles = list(map(int, lines[i+3].split()))
+    print(nums[sum(shuffles) % n])
