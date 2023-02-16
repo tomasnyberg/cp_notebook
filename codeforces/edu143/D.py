a1a5a24e2a7df28538049b929dd2b24fbb831f2e
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-16 17:19:55 +0100

    solve D

--- codeforces/edu143/D.py
+++ codeforces/edu143/D.py
@@ -1,2 +1,23 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+def nCr(n,r):
+    import math
+    return math.factorial(n) // (math.factorial(r)*(math.factorial(n-r)))
+n = int(lines[0])
+weights = list(map(int, lines[1].split()))
+prod = 1
+for i in range(0, len(weights), 3):
+    a = weights[i]
+    b = weights[i+1]
+    c = weights[i+2]
+    one = a + b
+    two = a + c
+    three = b + c
+    m = max(one, two, three)
+    maxcount = 0
+    for x in [one, two, three]:
+        if x == m:
+            maxcount += 1
+    prod *= maxcount
+print((nCr((n//3), (n//6)) * prod) % 998244353)
+    
