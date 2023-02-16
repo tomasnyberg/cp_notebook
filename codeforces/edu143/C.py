91ef0b4c6c0c8d7412bed352cbefd22b77cf4fec
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-16 16:41:00 +0100

    solve C

--- codeforces/edu143/C.py
+++ codeforces/edu143/C.py
@@ -22,18 +22,18 @@ for i in range(1, len(lines), 3):
             # print("index", j, "can drink the whole thing")
             result[j] += a[j]
             continue
-        look_for = a[j] + CS[j]
+        look_for = a[j] + (CS[j-1] if j > 0 else 0)
         # binary search for the first index where CS[idx] >= look_for
         idx = bisect.bisect_left(CS, look_for)
         line[j] += 1
         if idx == len(CS):
             # print("Everyone will drink full from", j)
-            line[j] += 1
             continue
         line[idx] -= 1
         remaining = a[j] - (CS[idx-1] - (CS[j-1] if j > 0 else 0))
         result[idx] += remaining
         # print("index", idx, "will drink the remaining", remaining)
+    # print("line", line)
     running = 0
     for j in range(len(a)):
         running += line[j]
