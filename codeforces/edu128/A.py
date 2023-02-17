62eda494a607d89e5a1434a937836ba7d31c1de9
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-17 13:40:58 +0100

    solve A

--- codeforces/edu128/A.py
+++ codeforces/edu128/A.py
@@ -1,2 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    l1, r1, l2, r2 = map(int, line.split())
+    smallest_in_interval = None
+    for i in range(l1, r1+1):
+        if l2 <= i <= r2:
+            smallest_in_interval = i
+            break
+    if smallest_in_interval is not None:
+        print(smallest_in_interval)
+    else:
+        print(l1 + l2)
+            
