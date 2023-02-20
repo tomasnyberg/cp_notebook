0bb98ae85f66238ba8b7e1500ec9b0ec4d9ada2c
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-20 12:29:24 +0100

    modify cr script to be able to run without piping in an infile

--- cr.sh
+++ cr.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-# Take 1 argument
-if [ $# -ne 1 ]; then
-  echo "Usage: cr <filename>, compiles and runs with "in" as input, removes afterwards"
+# If we got no arguments, print usage and exit
+if [ $# -eq 0 ]; then
+  echo "Usage: cr.sh <file> [-noin]"
   exit 1
 fi
-# compile the file with the name given with g++
-g++ -O1 $1 && ./a.out < in && rm a.out
+
+# If the -noin flag is set, run without piping in the in file, else pipe it in
+if [ "$2" = "-noin" ]; then
+  g++ -O1 $1 && ./a.out && rm a.out
+else
+  g++ -O1 $1 && ./a.out < in && rm a.out
+fi
