340386864dee2fc34568e6111944ad3fe576f376
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-21 19:27:52 +0100

    solve A

--- codeforces/edu127/A.py
+++ codeforces/edu127/A.py
@@ -1,2 +1,12 @@
-import sys
+import sys, re
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    # Get all the occurences of multiple as or bs
+    matches = re.findall(r'(a+|b+)', line)
+    for x in matches:
+        if len(x) == 1:
+            print("NO")
+            break
+    else:
+        print("YES")
