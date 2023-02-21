d54184109e0de56661bf85fef71ba491b9fd8545
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-21 19:34:43 +0100

    solve B

--- codeforces/edu127/B.py
+++ codeforces/edu127/B.py
@@ -1,2 +1,23 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+def test(add_to_first, nums):
+    nums[0] += add_to_first
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i - 1]:
+            nums[i] +=1
+        elif nums[i] - nums[i-1] > 1:
+            nums[i] -= 1
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i-1] + 1:
+            return False
+    return True
+
+            
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split(" ")))
+    if any(test(i, [x for x in nums]) for i in [-1,0,1]):
+        print("YES")
+    else:
+        print("NO")
