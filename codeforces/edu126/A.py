7ca696a77b7838ff055603e87ba82a7f0ac2ac67
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-26 19:21:46 +0100

    solve A

--- codeforces/edu126/A.py
+++ codeforces/edu126/A.py
@@ -1,2 +1,10 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for i in range(1, len(lines), 3):
+    a = list(map(int, lines[i+1].split()))
+    b = list(map(int, lines[i+2].split()))
+    res = 0
+    for j in range(1, len(a)):
+        res += min(abs(a[j] - a[j-1]) + abs(b[j] - b[j-1]), abs(a[j] - b[j-1]) + abs(b[j] - a[j-1]))
+    print(res)
