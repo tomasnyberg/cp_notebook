8c18abbeada7974184086ee0cd56958da76fba83
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-28 15:38:58 +0100

    solve A

--- edu144/A.py
+++ edu144/A.py
@@ -1,2 +1,16 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+fbstring = ""
+for i in range(100000):
+    if i % 3 == 0:
+        fbstring += "F"
+    if i % 5 == 0:
+        fbstring += "B"
+
+
+for line in lines[2::2]:
+    if line in fbstring:
+        print("YES")
+    else:
+        print("NO")
