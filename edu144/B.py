edf53b4741131679fe429a8a698b5b9425bd048c
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-02-28 15:47:51 +0100

    solve B

--- edu144/B.py
+++ edu144/B.py
@@ -1,2 +1,27 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for i in range(1, len(lines), 2):
+    a = lines[i]
+    b = lines[i+1]
+    if a[0] == b[0]:
+        print("YES")
+        print(a[0] + "*")
+        continue
+    if a[-1] == b[-1]:
+        print("YES")
+        print("*" + a[-1])
+        continue
+    # Find every substring of a and b of length 2
+    # If they are the same, print YES and the substring
+    seen = set()
+    for j in range(2, len(a) + 1):
+        seen.add((a[j-2:j]))
+    for j in range(2, len(b) + 1):
+        if (b[j-2:j]) in seen:
+            print("YES")
+            print("*" + b[j-2:j] + "*")
+            break
+    else:
+        print("NO")
+    
