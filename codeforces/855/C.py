9232667307e3ef138ef6e93128087cbe74a84a60
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-02 02:56:43 +0100

    solve C1 C2

--- codeforces/855/C.py
+++ codeforces/855/C.py
@@ -1,2 +1,20 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+from collections import deque
+from heapq import heappush, heappop
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split(" ")))
+    hq = []
+    result = 0
+    for i in range(len(nums)):
+        if not nums[i]:
+            if hq:
+                result += -heappop(hq)
+        else:
+            heappush(hq, -nums[i])
+    print(result)
+
+    
+        
+
