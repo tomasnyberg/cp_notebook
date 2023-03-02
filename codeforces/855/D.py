e3c77563e2b66fe971167e64c8e5b8fb6b56c51e
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-02 03:10:43 +0100

    solve D

--- codeforces/855/D.py
+++ codeforces/855/D.py
@@ -1,2 +1,10 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    bad = set()
+    result = 0
+    for i in range(2, len(line)):
+        if line[i-2] != line[i]:
+            result += 1
+    print(result + 1)
