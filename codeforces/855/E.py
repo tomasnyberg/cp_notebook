b4407fcedb1356ce1094a24fdb50d817fe07cc14
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-02 03:55:15 +0100

    solve E2

--- codeforces/855/E.py
+++ codeforces/855/E.py
@@ -3,33 +3,18 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 
 for i in range(1, len(lines), 3):
     n, k = map(int, lines[i].split())
-    a = lines[i+1]
-    b = lines[i+2]
+    a = list(lines[i+1])
+    b = list(lines[i+2])
     if a == b:
         print("YES")
         continue
     if (list(sorted(a)) != list(sorted(b))):
         print("NO")
         continue
-    a = list(a)
-    b = list(b)
-    if len(a) == 1 and a != b:
-        print("NO")
-        continue
-    if len(a) == 2 and a != b:
-        print("NO")
-        continue
-    if len(a) == 3 and a != b:
-        print("NO")
-        continue
-    if len(a) == 4 and a != b:
-        if a[0] == b[-1] and a[-1] == b[0] and a[1] == b[1] and a[2] == b[2]:
-            print("YES")
-            continue
-        else:
+    for j in range(len(a)):
+        if j - k < 0 and j + k >= len(a) and a[j] != b[j]:
             print("NO")
-            continue
-    if len(a) == 5 and a[2] != b[2]:
-        print("NO")
-        continue
-    print("YES")
+            break
+    else:
+        print("YES")
+    
