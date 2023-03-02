1623249c945bf6a61ff007cabf369a8ee425fe3d
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-02 03:52:58 +0100

    solve E1

--- codeforces/855/E.py
+++ codeforces/855/E.py
@@ -5,11 +5,20 @@ for i in range(1, len(lines), 3):
     n, k = map(int, lines[i].split())
     a = lines[i+1]
     b = lines[i+2]
+    if a == b:
+        print("YES")
+        continue
     if (list(sorted(a)) != list(sorted(b))):
         print("NO")
         continue
     a = list(a)
     b = list(b)
+    if len(a) == 1 and a != b:
+        print("NO")
+        continue
+    if len(a) == 2 and a != b:
+        print("NO")
+        continue
     if len(a) == 3 and a != b:
         print("NO")
         continue
