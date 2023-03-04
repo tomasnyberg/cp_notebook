a8016569749bb3edc1e7c69dec07d8fe6a7b5eb5
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-04 18:41:44 +0100

    solve A

--- codeforces/856/A.py
+++ codeforces/856/A.py
@@ -1,2 +1,12 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    words = line.split()
+    words.sort(key=lambda x: len(x))
+    longest = max(map(len, words)) + 1
+    midpoint = list(filter(lambda x: len(x) == longest // 2, words))
+    if midpoint[0] == midpoint[1][::-1]:
+        print("YES")
+    else:
+        print("NO")
