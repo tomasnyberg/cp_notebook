e150476cf3f10008a5e013507fc64687189e0290
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-04 18:47:24 +0100

    solve B

--- codeforces/856/B.py
+++ codeforces/856/B.py
@@ -1,2 +1,12 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split()))
+    # replace all 1s with 2s
+    nums = list(map(lambda x: 2 if x == 1 else x, nums))
+    for i in range(1, len(nums)):
+        if nums[i] % nums[i-1] == 0:
+            nums[i] += 1
+    print(*nums)
+        
