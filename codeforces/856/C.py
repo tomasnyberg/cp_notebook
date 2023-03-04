932687f650f696333c99167f00a7d9613d4dea30
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-04 19:04:13 +0100

    solve C

--- codeforces/856/C.py
+++ codeforces/856/C.py
@@ -1,2 +1,14 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+from heapq import heappop, heappush
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split()))
+    left = 0
+    right = 0
+    while right < len(nums):
+        right += 1
+        while nums[left] < (right - left):
+            left += 1
+        print(right - left, end=' ')
+    print()
