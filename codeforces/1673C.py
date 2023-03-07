80d13de0de38614173586b0c90ad0ddd893c62e3
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-07 14:58:50 +0100

    resolve 1673C

--- codeforces/1673C.py
+++ codeforces/1673C.py
@@ -8,21 +8,21 @@ all_pals = [0]
 for i in range(1, 2*N):
     if str(i) == str(i)[::-1]:
         all_pals.append(i)
-        
- 
-dp = [[0 for _ in range(M)] for x in range(N)]
+
+dp = [[0]*(M) for _ in range(N)]
 for i in range(1, M):
     dp[0][i] = 1
 for i in range(1, N):
     dp[i][0] = 0
- 
+
 for i in range(1, N):
     for j in range(1, M):
-        if(all_pals[j] <= i):
-            dp[i][j]=(dp[i][j-1]+dp[i-all_pals[j]][j])%mod
-        else:
-            dp[i][j] = dp[i][j-1]
- 
+        dp[i][j] = dp[i][j-1]
+        if all_pals[j] <= i:
+            dp[i][j] += dp[i-all_pals[j]][j]
+            dp[i][j] %= mod
+
 for line in lines[1:]:
     n = int(line)
     print(dp[n][M-1])
+ 
