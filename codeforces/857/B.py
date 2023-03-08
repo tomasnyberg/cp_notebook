bf4bb2297cdfea1b2b4170130ba72ec3390e9847
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-08 17:30:38 +0100

    WA2 on B

--- codeforces/857/B.py
+++ codeforces/857/B.py
@@ -1,2 +1,27 @@
 import sys
+from math import ceil
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split()))
+    waiting = {'a': 0, 'b': 0}
+    unknown = 0
+    result = 0
+    for x in nums:
+        if x == 1:
+            unknown += 1
+            result = max(result, ceil(waiting['a']/2) + ceil(waiting['b']/2) + unknown)
+        elif x == 2:
+            waiting['a'] += (unknown // 2)
+            waiting['b'] += (unknown // 2)
+            if waiting['a'] % 2 == 0:
+                waiting['a'] += unknown % 2
+            else:
+                waiting['b'] += unknown % 2
+            unknown = 0
+    # print(unknown, result, waiting)
+    print(max(ceil(waiting['a']/2) + ceil(waiting['b']/2) + unknown, result))
+
+
+
+    # print(result + running)
