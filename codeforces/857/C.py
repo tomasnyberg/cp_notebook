c815024062aa05b59a3a446a16dc80f36ed5a743
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-13 02:30:14 +0100

    resolve C

--- codeforces/857/C.py
+++ codeforces/857/C.py
@@ -1,24 +1,6 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-
-a = [[9740, 1549, 9744, 1553, 9748],
- [1550, 1551, 1554, 1555, 1558],
- [10252, 2061, 10256, 2065, 10260],
- [2062, 2063, 2066, 2067, 2070]]
-
-b = [[3108, 3109, 3112, 3113],
- [3110, 3111, 3114, 3115],
- [3620, 3621, 3624, 3625],
- [3622, 3623, 3626, 3627]]
-
-c=  [[25800, 25801, 25804, 25805, 25808, 25809],
- [25802, 4294993099, 25806, 4294993103, 25810, 4294993107],
- [26312, 26313, 26316, 26317, 26320, 26321],
- [26314, 4294993611, 26318, 4294993615, 26322, 4294993619],
- [26824, 26825, 26828, 26829, 26832, 26833],
- [26826, 4294994123, 26830, 4294994127, 26834, 4294994131]]
-
 def check_submatrices(B):
     rows, cols = len(B), len(B[0])
     for i in range(rows-3):
@@ -34,15 +16,15 @@ def check_submatrices(B):
 for line in lines[1:]:
     n, m = map(int, line.split())
     matrix = [[0] * m for _ in range(n)]
+    for i in range(0, n, 2):
+        for j in range(0, m, 2):
+            for idx, (di, dj) in enumerate([(0, 0), (0, 1), (1, 0), (1, 1)]):
+                if i+di < n and j+dj < m:
+                    matrix[i+di][j+dj] = idx
     for i in range(n):
         for j in range(m):
-            matrix[i][j] = (i << 10) + j
+            matrix[i][j] |= (i << 3) | (j << 20) 
     print(n*m)
-    for row in matrix:
-        print(*row)
+    for xs in matrix:
+        print(*xs)
     # print(check_submatrices(matrix))
-
-# print(check_submatrices(a))
-# print(check_submatrices(b))
-# print(check_submatrices(c))
-
