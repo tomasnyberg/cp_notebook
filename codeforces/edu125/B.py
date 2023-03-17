5b71b1a3d0d52c1c6eadd2769b16013ada0f53f2
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-17 09:30:06 +0100

    solve B

--- codeforces/edu125/B.py
+++ codeforces/edu125/B.py
@@ -1,2 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    n, B, x, y = list(map(int, line.split()))
+    prev = 0
+    result = 0
+    for i in range(n):
+        if prev + x <= B:
+            result += prev + x
+            prev = prev + x
+        else:
+            result += prev - y
+            prev = prev - y
+    print(result)
