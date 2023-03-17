7c0404229b1c14c3d39b4d5ed3d13b22d41600f7
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-17 09:26:49 +0100

    solve A

--- codeforces/edu125/A.py
+++ codeforces/edu125/A.py
@@ -1,2 +1,11 @@
-import sys
+import sys, math
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    a, b = map(int, line.split())
+    if a == 0 and b == 0:
+        print(0)
+    elif int(math.sqrt(a**2 + b**2))**2 == a**2 + b**2:
+        print(1)
+    else:
+        print(2)
