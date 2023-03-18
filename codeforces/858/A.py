5e84628005c5f6b9a7709c4daa7ded8e026ac541
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-18 13:10:38 +0100

    solve A

--- codeforces/858/A.py
+++ codeforces/858/A.py
@@ -1,2 +1,19 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    a, b, c, d = map(int, line.split())
+    if b > d:
+        print(-1)
+        continue
+    result = 0
+    result += abs(d - b)
+    a += result
+    b += result
+    if a < c:
+        print(-1)
+        continue
+    result += abs(c - a)
+    print(result)
+    
+    
