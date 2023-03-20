2f3c92923bada403c87c3a766f6ea4c76412c00a
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-20 18:42:55 +0100

    solve A

--- codeforces/859/A.py
+++ codeforces/859/A.py
@@ -1,2 +1,9 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    a, b, c = map(int, line.split())
+    if a + b == c:
+        print("+")
+    else:
+        print("-")
