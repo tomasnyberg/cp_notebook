db63e5a1017953c94c0e0eb0ab45529da11c24c9
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-20 19:14:32 +0100

    solve E

--- codeforces/859/E.py
+++ codeforces/859/E.py
@@ -1,2 +1,34 @@
 import sys
-lines = list(map(str.strip, sys.stdin.readlines()))
+
+def cum_sum(nums):
+    curr = 0
+    result = [0]*len(nums)
+    for idx, num in enumerate(nums):
+        curr += num
+        result[idx] = curr
+    return result
+
+def solve():
+    n = int(sys.stdin.readline())
+    nums = list(map(int, sys.stdin.readline().split()))
+    CS = cum_sum(nums)
+    low = 1
+    high = n
+    # print(CS)
+    while low < high:
+        mid = (low+high)//2
+        print("?", mid, *[i for i in range(1, mid+1)], flush=True)
+        sys.stdout.flush()
+        sum1 = int(sys.stdin.readline())
+        if sum1 <= CS[mid-1]:
+            low = mid+1
+        else:
+            high = mid
+    print("!", low, flush=True)
+
+t = int(sys.stdin.readline())
+for _ in range(t):
+    solve()
+# 2
+# 5
+# 1 2 3 4 5
