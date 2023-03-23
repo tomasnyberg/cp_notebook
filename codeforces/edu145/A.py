055b40edc9ae7afb471bdf02cc84efeac380fd46
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-23 16:17:04 +0100

    FINALLY solve A

--- codeforces/edu145/A.py
+++ codeforces/edu145/A.py
@@ -1,2 +1,18 @@
 import sys
+import itertools
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    counts = {}
+    for c in line:
+        counts[c] = counts.get(c, 0) + 1
+    if len(counts) == 1:
+        print(-1)
+    elif len(counts) == 2:
+        if counts[line[0]] == 2:
+            print(4)
+        else:
+            print(6)
+    else:
+        print(4)
+
