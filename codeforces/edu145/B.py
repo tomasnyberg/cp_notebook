f89130a03de4b3a8c8dd1b39263e91b7a5f98545
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-23 16:20:09 +0100

    solve B

--- codeforces/edu145/B.py
+++ codeforces/edu145/B.py
@@ -1,2 +1,18 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+def check(l, target):
+    return (l+1)**2 >= target
+
+
+for line in lines[1:]:
+    n = int(line)
+    low = 0
+    high = 10**25
+    while low < high:
+        mid = (low + high) >> 1
+        if check(mid, n):
+            high = mid
+        else:
+            low = mid + 1
+    print(low)
