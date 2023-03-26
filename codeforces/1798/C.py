141baf40ae1f9c3d4e511ea12647144c403ec906
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-26 17:32:05 +0200

    working on C

--- codeforces/1798/C.py
+++ codeforces/1798/C.py
@@ -1,2 +1,42 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+from collections import deque
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+i = 1
+while i < len(lines):
+    n = int(lines[i])
+    i+=1
+    a = []
+    b = []
+    for _ in range(n):
+        c, d = map(int, lines[i].split())
+        a.append(c)
+        b.append(d)
+        i+=1
+    a = deque(a)
+    b = deque(b)
+    result = 0
+    while a and b:
+        curr_a = a.popleft()
+        curr_target = b.popleft()
+        while b:
+            target = lcm(curr_target, b[0])
+            print("Target", target, a[0], b[0])
+            if a[0] % (target//b[0]) == 0:
+                a.popleft()
+                b.popleft()
+                curr_target = target
+            else:
+                break
+        result += 1
+    print(result)
+
+    
