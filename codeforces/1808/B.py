0e3f1b423f7b9c928b856841e5c993cfeced74e8
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-29 11:57:07 +0200

    close on B

--- codeforces/1808/B.py
+++ codeforces/1808/B.py
@@ -1,2 +1,31 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+i = 1
+while i < len(lines):
+    n, m = map(int, lines[i].split())
+    cards = []
+    for _ in range(n):
+        cards.append(list(map(int, lines[i+1].split())))
+        i += 1
+    i += 1
+    transposed = [[0 for _ in range(n)] for _ in range(m)]
+    # Transpose the cards matrix
+    for j in range(n):
+        for k in range(m):
+            transposed[k][j] = cards[j][k]
+    result = 0
+    for xs in transposed:
+        xs.sort()
+        # print(xs)
+        running = 0
+        for j in range(len(xs)):
+            contribute = int((xs[j] - (running / (j))) * j) if j > 0 else 0
+            result += contribute 
+            print(xs[j], "contributes with", contribute, "to result")
+            running += xs[j]
+    print(result)
+
+
+
+    
