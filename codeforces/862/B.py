3712fbb3ad5d347c6520183afaed83f50ec892a9
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-02 16:41:04 +0200

    solve B

--- codeforces/862/B.py
+++ codeforces/862/B.py
@@ -1,2 +1,9 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    line = list(line)
+    smallest = min(line)
+    occurrences = [i for i in range(len(line)) if line[i] == smallest]
+    line.pop(occurrences[-1])
+    print(smallest + ''.join(line))
