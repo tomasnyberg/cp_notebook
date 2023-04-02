026fd1b3816c683d67122103851c8b1e3c667763
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-02 17:47:35 +0200

    got further on C

--- codeforces/862/C.py
+++ codeforces/862/C.py
@@ -1,11 +1,28 @@
 import sys
+from decimal import Decimal, getcontext
+
+getcontext().prec = 50  # Set the precision of the decimal calculations
+
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 def intersects_parabola(a, b, c, k):
-    x_vertex = -b / (2*a)
-    y_vertex = a*x_vertex**2 + b*x_vertex + c
-    y_line = k*x_vertex
-    return y_line >= y_vertex
+        # Ensure the parabola is upwards open
+    if a <= 0:
+        raise ValueError("The parabola must be upwards open (a > 0).")
+
+    # Find the discriminant (D) for the quadratic equation (ax^2 + bx + c - kx = 0)
+    D = (b - k)**2 - 4 * a * (c)
+
+    # Check the conditions for intersection or touching
+    if D > 0:
+        # The parabola and the line intersect at two distinct points
+        return True
+    elif D == 0:
+        # The parabola and the line touch each other (tangent)
+        return True
+    else:
+        # The parabola and the line do not intersect or touch
+        return False
 
 ii = 1
 while ii< len(lines):
