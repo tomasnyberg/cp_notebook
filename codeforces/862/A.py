e93bd2058bbfdbc604de6cf31b4a720de87eb0d6
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-02 16:37:44 +0200

    solve A

--- codeforces/862/A.py
+++ codeforces/862/A.py
@@ -1,2 +1,21 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+def check(xs, x):
+    bs = []
+    for a in xs:
+        bs.append(a ^ x)
+    res = 0
+    for b in bs:
+        res ^= b
+    return res == 0
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split()))
+    for i in range(2**8):
+        if check(nums, i):
+            print(i)
+            break
+    else:
+        print(-1)
+        
