f8dfee73b872f7241700f0f8ad7a7f94d293551b
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-04 20:00:50 +0200

    solve C

--- codeforces/863/C.py
+++ codeforces/863/C.py
@@ -1,32 +1,24 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-def layer(x, y, n):
-    return max(abs(x - (n + 1)), abs(y - (n + 1)))
-
 for line in lines[2::2]:
     nums = list(map(int, line.split()))
-    a = []
-    added = False
-    for x in nums:
-        if not a:
-            a.append(x)
-        else:
-            if a[-1] > x:
-                if len(a) >= 3 and a[-2] >= a[-1]:
-                    a[-1] = x
-                else:
-                    a.append(x)
-                    added = True
-                a.append(x)
-            else:
-                a.append(x)
-    if not added:
-        if len(a) >= 2 and a[0] < a[1]:
-            a.insert(0, a[0])
-        else:
-            a.append(a[-1])
-    print(*a)
-        
+    original = nums[:]
+    firstsmaller = -1
+    for i in range(len(nums)-1):
+        if nums[i] < nums[i+1]:
+            firstsmaller = i
+            nums.insert(i+1, 0)
+            break
+    else:
+        nums.append(nums[-1])
+        print(*nums)
+        continue
+    for i in range(len(original)):
+        this = max(nums[i], nums[i+1])
+        if this != original[i]:
+            nums[i] = nums[i+1]
+    print(*nums)
+    
 
-
+    
