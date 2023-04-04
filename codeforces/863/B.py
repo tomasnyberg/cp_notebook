5ddb6f71b4d40c9079d8d5e749f1fd77829d7b46
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-04 17:45:32 +0200

    solve B

--- codeforces/863/B.py
+++ codeforces/863/B.py
@@ -1,2 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+def layer(x, y, n):
+    middles = [n//2, n//2 + 1]
+    xtomiddle = min(abs(x - middles[0]), abs(x - middles[1]))
+    ytomiddle = min(abs(y - middles[0]), abs(y - middles[1]))    
+    return max(xtomiddle, ytomiddle)
+
+for line in lines[1:]:
+    n, x1, y1, x2,y2 = map(int, line.split())
+    # print(x1, y1, x2, y2)
+    # print(layer(x1, y1, n))
+    # print(layer(x2, y2, n))
+    print(abs(layer(x1, y1, n) - layer(x2, y2, n)))
