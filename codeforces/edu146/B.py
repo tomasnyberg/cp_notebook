2d54f92a60d2b878267b62f096a11168fd694f33
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-05 03:36:01 +0200

    solve B

--- codeforces/edu146/B.py
+++ codeforces/edu146/B.py
@@ -5,12 +5,13 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 def solve(a, b):
     a, b = min(a, b), max(a, b)
     result = 10**25
-    for length in range(1, 10000):
+    for length in range(1, min(b, 10**5) + 1):
         lencost = length - 1
-        bcost = 1 + b // length
-        acost = 1 if a <= length else 1 + a // length
+        bcost = 1 + b // length if b % length != 0 else b // length
+        acost = 1 if a <= length else (1 + a // length if a % length != 0 else a // length)
         result = min(result, lencost + bcost + acost)
-    return result - 1
+        # print(length, acost + bcost + lencost)
+    return result
 
 for line in lines[1:]:
     a, b = map(int, line.split())
