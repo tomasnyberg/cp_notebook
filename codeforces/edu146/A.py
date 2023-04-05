e70d85139b53eb699e0422cf11889fdc63b11a52
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-05 02:13:10 +0200

    solve A

--- codeforces/edu146/A.py
+++ codeforces/edu146/A.py
@@ -1,2 +1,24 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    n, k = map(int, line.split())
+    if n == 1 and k != 1:
+        print("NO")
+        continue
+    if n % 2 == 0:
+        print("YES")
+        continue
+    if k == 1 or k == n:
+        print("YES")
+        continue
+    if (n % k) % 2 == 0:
+        print("YES")
+        continue
+    if k > n:
+        print("NO")
+        continue
+    if n - k > 1 and (n - k) % 2 == 0:
+        print("YES")
+        continue
+    print("NO")
