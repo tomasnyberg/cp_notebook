a281920bade6f54c053ed80d0d264d13f92c4d30
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-07 13:30:51 +0200

    solve A

--- codeforces/edu122/A.py
+++ codeforces/edu122/A.py
@@ -1,2 +1,16 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    n = int(line)
+    line = list(line)
+    if n % 7 == 0:
+        print(n)
+        continue
+    for i in range(0, 10):
+        line[-1] = str(i)
+        joined = int(''.join(line))
+        if joined % 7 == 0 and joined != 0:
+            print(joined)
+            break
+
