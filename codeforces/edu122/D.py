d65cd266022b80b41ac479b9e1ba98f7b9df97e9
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-07 14:50:22 +0200

    cleanup, still bad

--- codeforces/edu122/D.py
+++ codeforces/edu122/D.py
@@ -1,18 +1,5 @@
 import sys
-import math
-from functools import lru_cache
 lines = list(map(str.strip, sys.stdin.readlines()))
-
-@lru_cache(None)
-def minimal_amount(target, curr):
-    if target < curr:
-        return 10**9
-    if target == curr:
-        return 0
-    result = 10**9
-    for i in range(1, curr + 1):
-        result = min(result, 1 + minimal_amount(target, curr + curr // i))
-    return result
 
 x = 2*10**3 
 dp = [10**9] * (x + 1)
@@ -24,17 +11,6 @@ for i in range(1, x + 1):
         if i + i//j >= len(dp): break
         dp[i + (i//j)] = min(dp[i] + 1, dp[i + (i//j)])
 
-# print(dp)
-
-def count_bits(n):
-    return dp[n]
-
-# for i in range(1, 1000):
-#     if i % 100 == 0:
-#         print(i)
-#     if count_bits(i) != minimal_amount(i, 1):
-#         print(i, bin(i), count_bits(i), minimal_amount(i, 1))
-
 def knapsack(values, weights, capacity):
     dp = [[0 for _ in range(capacity + 1)] for _ in range(len(values) + 1)]
     for i in range(1, len(values) + 1):
@@ -42,7 +18,7 @@ def knapsack(values, weights, capacity):
             dp[i][j] = dp[i - 1][j]
             if weights[i - 1] <= j:
                 dp[i][j] = max(dp[i][j], dp[i - 1][j - weights[i - 1]] + values[i - 1])
-    return (dp[-1][-1], dp)
+    return dp[-1][-1]
 
 for i in range(1, len(lines), 3):
     n, k = map(int, lines[i].split())
@@ -57,5 +33,5 @@ for i in range(1, len(lines), 3):
             removed.add(j)
     b = [b[j] for j in range(len(b)) if j not in removed]
     coins = [coins[j] for j in range(len(coins)) if j not in removed]
-    ans, _ = knapsack(coins, b, k)
+    ans = knapsack(coins, b, k)
     print(ans + result)
