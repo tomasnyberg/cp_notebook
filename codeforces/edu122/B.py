d12982d69cf205b30083527fa570dc1975bfbf0b
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-07 13:36:30 +0200

    solve B

--- codeforces/edu122/B.py
+++ codeforces/edu122/B.py
@@ -1,2 +1,9 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    ones = line.count('1')
+    zeroes = line.count('0')
+    # print(line)
+    # print(ones, zeroes)
+    print(min(ones, zeroes) if ones != zeroes else min(ones, zeroes) - 1)
