300552399848f8e51493fbc5588bd4daa1791314
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-08 17:47:55 +0200

    working on C, idlesness limit

--- codeforces/864/C.py
+++ codeforces/864/C.py
@@ -1,2 +1,31 @@
 import sys
-lines = list(map(str.strip, sys.stdin.readlines()))
+from decimal import Decimal, getcontext
+
+def query(x, y):
+    print(x, y)
+    sys.stdout.flush()
+    return int(input())
+
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    first = query(1, 1)
+    second_query = (min(n, 1 + first), min(m, 1 + first))
+    second = query(*second_query)
+    # The node second positions up from the second_query
+    up = (second_query[0] - second, second_query[1])
+    if up[0] < 1:
+        up = (1, up[1])
+    # The node second positions left from the second_query
+    left = (second_query[0], second_query[1] - second)
+    if left[1] < 1:
+        left = (left[0], 1)
+    third = query(*up)
+    if third == 0:
+        print("!", *up)
+    else:
+        print("!", *left)
+    sys.stdout.flush()
+# Choose three non-collinear points A, B, and C based on the dimensions
+    
+
