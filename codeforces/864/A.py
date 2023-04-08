325961853653ad22044949bdb3d6a418e8576f8f
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-08 16:10:23 +0200

    solve A

--- codeforces/864/A.py
+++ codeforces/864/A.py
@@ -1,2 +1,17 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+def block_off(x, y,n,m):
+    result = 4
+    if x == 1 or x == n:
+        result -= 1
+    if y == 1 or y == m:
+        result -= 1
+    return result
+
+
+for i in range(1, len(lines),2):
+    n, m = map(int, lines[i].split())
+    x1, y1, x2, y2 = map(int, lines[i+1].split())
+    print(min(block_off(x1, y1,n,m), block_off(x2, y2,n,m)))
+    
