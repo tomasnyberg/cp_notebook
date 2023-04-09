2d0f11d1cfd1ace9eb00032b1b4e4ff25b85c6d7
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-09 14:27:13 +0200

    attempt C

--- codeforces/865/C.py
+++ codeforces/865/C.py
@@ -1,2 +1,14 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split()))
+    if len(nums) == 2:
+        print("YES" if nums[0] <= nums[1] else "NO")
+        continue
+    for i in range(1, len(nums) - 1):
+        if nums[i-1] > nums[i] < nums[i+1]:
+            print("NO")
+            break
+    else:
+        print("YES")
