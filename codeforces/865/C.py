0f548c2a0ea6b4b2e344a6fb1a53ef042f8ba5f0
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-09 15:00:06 +0200

    STILL wa2

--- codeforces/865/C.py
+++ codeforces/865/C.py
@@ -4,10 +4,11 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 def check(xs):
     if len(xs) == 2:
         return xs[0] <= xs[1]
-    for i in range(1, len(xs)-1):
-        if xs[i-1] > xs[i] < xs[i+1]:
-            return False
-    return True
+    count = 0
+    for i in range(len(xs)):
+        if (xs[i-1] if i-1 >= 0 else 10**15) > xs[i] < (xs[i+1] if i+1 < len(xs) else 10**15):
+            count += 1
+    return count <= 1
 
 for line in lines[2::2]:
     nums = list(map(int, line.split()))
