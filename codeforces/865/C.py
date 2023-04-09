44c3f428eec882bdd57e9a8ac33364683710709b
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-09 14:55:48 +0200

    wa2 on C again

--- codeforces/865/C.py
+++ codeforces/865/C.py
@@ -1,14 +1,20 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+def check(xs):
+    if len(xs) == 2:
+        return xs[0] <= xs[1]
+    for i in range(1, len(xs)-1):
+        if xs[i-1] > xs[i] < xs[i+1]:
+            return False
+    return True
+
 for line in lines[2::2]:
     nums = list(map(int, line.split()))
-    if len(nums) == 2:
-        print("YES" if nums[0] <= nums[1] else "NO")
-        continue
-    for i in range(1, len(nums) - 1):
-        if nums[i-1] > nums[i] < nums[i+1]:
-            print("NO")
-            break
+    if len(nums) % 2 == 1:
+        a = check(nums[:-1])
+        b = check(nums[1:])
+        print("YES" if a or b else "NO")
     else:
-        print("YES")
+        print("YES" if check(nums) else "NO")
+    
