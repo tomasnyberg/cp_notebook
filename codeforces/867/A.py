f3f7de6e58250015303840d39631a3fe796a5259
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-28 18:22:11 +0200

    solve A

--- codeforces/867/A.py
+++ codeforces/867/A.py
@@ -1,2 +1,14 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for i in range(1, len(lines), 3):
+    n, t = map(int, lines[i].split())
+    a = list(map(int, lines[i+1].split()))
+    b = list(map(int, lines[i+2].split()))
+    best = (-1, -1)
+    for j in range(len(a)):
+        if a[j] <= t:
+            if b[j] >= best[1]:
+                best = (j, b[j])
+        t -= 1
+    print(best[0] + 1 if best[0] != -1 else -1)
