d8af65141e28bd1fd27b39127aa22436a672443e
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-28 20:29:59 +0200

    upsolve E

--- codeforces/867/E.py
+++ codeforces/867/E.py
@@ -9,6 +9,7 @@ for line in lines[2::2]:
     counts = {}
     for c in s:
         counts[c] = counts.get(c, 0) + 1
+    # print(counts)
     if any(val > len(s) // 2 for val in counts.values()):
         print(-1)
         continue
@@ -16,7 +17,12 @@ for line in lines[2::2]:
     for i in range(len(s) // 2):
         if s[i] == s[len(s) - i - 1]:
             conflicts[s[i]] = conflicts.get(s[i], 0) + 1
+    # print(conflicts)
     conflicts = [v for v in conflicts.values()]
+    m = max(conflicts) if conflicts else 0
+    k = sum(conflicts)
+    print(max(m, (k+1)//2))
+    continue
     result = 0
     curr = 0
     while conflicts:
@@ -26,6 +32,7 @@ for line in lines[2::2]:
             if curr <= conflicts[-1]:
                 result += curr
                 conflicts[-1] -= curr
+                curr = 0
                 break
             else:
                 take = conflicts.pop()
