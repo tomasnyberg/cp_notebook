c115b4991dc6b2360f5e78a24b6c6e9b63278e3a
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-05 18:46:42 +0200

    solve B

--- codeforces/871/B.py
+++ codeforces/871/B.py
@@ -1,2 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split(" ")))
+    result = 0
+    streak = 0
+    for x in nums:
+        if x == 0:
+            streak += 1
+            result = max(result, streak)
+        else:
+            streak = 0
+    result = max(result, streak)
+    print(result)
