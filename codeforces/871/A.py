1d5304201ac9f906833d0aec03f0b3b5652671b4
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-05 18:44:23 +0200

    solve A

--- codeforces/871/A.py
+++ codeforces/871/A.py
@@ -1,2 +1,10 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+s = "codeforces"
+for line in lines[1:]:
+    res = 0
+    for i in range(len(line)):
+        if line[i] != s[i]:
+            res += 1
+    print(res)
