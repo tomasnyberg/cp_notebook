19a162b989c17dd33cf5a614159c273135593a04
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-05 18:52:27 +0200

    solve C

--- codeforces/871/C.py
+++ codeforces/871/C.py
@@ -1,2 +1,27 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+i = 1
+while i < len(lines):
+    n = int(lines[i])
+    i+=1
+    mbs = []
+    result = 10**9
+    onesmallest = 10**9
+    twosmallest = 10**9
+    for _ in range(n):
+        m, b = lines[i].split(" ")
+        m = int(m)
+        b = int(b, 2)
+        if b == 3:
+            result = min(result, m)
+        elif b == 2:
+            twosmallest = min(twosmallest, m)
+        elif b == 1:
+            onesmallest = min(onesmallest, m)
+        else:
+            x = 1
+        i+=1
+    result = min(result, onesmallest + twosmallest) 
+    print(result if result < 10**9 else -1)
+    
