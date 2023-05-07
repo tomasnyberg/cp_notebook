e687719b4a740843ce6305bf1cb4c4472c9ff87f
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-07 14:44:20 +0200

    other solution

--- codeforces/1500/1542B.py
+++ codeforces/1500/1542B.py
@@ -1,21 +1,16 @@
 import sys
-import math
 lines = list(map(str.strip, sys.stdin.readlines()))
-
+ 
 for line in lines[1:]:
     n, a, b = map(int, line.split(" "))
-    if b == 1:
-        print("YES")
-        continue
     if a == 1:
         print("YES" if (n-1) % b == 0 else "NO")
         continue
-    i = 1
-    while i <= n:
-        if i % b == n % b:
+    for power in range(40):
+        diff = n - pow(a, power)
+        if diff % b == 0 and diff >= 0:
             print("YES")
             break
-        i *= a
     else:
         print("NO")
-
+ 
