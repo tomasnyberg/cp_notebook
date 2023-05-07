c52f142b06b4d75b398babef657e9d8924933194
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-07 15:10:35 +0200

    better solution for 1537C

--- codeforces/1500/1537C.py
+++ codeforces/1500/1537C.py
@@ -5,16 +5,11 @@ for line in lines[2::2]:
     nums = list(map(int, line.split(" ")))
     nums.sort()
     candidates = []
-    smallestdiff = 10**10
+    best = (10**10, -1)
     for i in range(len(nums) - 1):
-        smallestdiff = min(smallestdiff, nums[i+1] - nums[i])
-    best = (0, -1)
-    for i in range(len(nums) - 1):
-        if nums[i + 1] - nums[i] != smallestdiff: continue
-        right = len(nums) - (i+1)
-        left = i
-        if right + left > best[0]:\
-            best = (right + left, i)
+        d = nums[i+1] - nums[i]
+        if d < best[0]:
+            best = (d, i)
     _, start = best
     print(*([nums[start]] + nums[start + 2:] + nums[:start] + [nums[start + 1]]))
     
