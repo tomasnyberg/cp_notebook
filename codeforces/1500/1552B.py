31f2fdf10cb3f81bfffca254df243d691c1c73ac
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-09 20:06:32 +0200

    solve 1552B

--- codeforces/1500/1552B.py
+++ codeforces/1500/1552B.py
@@ -1,4 +1,5 @@
 import sys
+import random
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 i = 1
@@ -11,18 +12,18 @@ while i < len(lines):
         i+=1
     def beats(a, b):
         return sum([1 if x < y else 0 for x, y in zip(a, b)]) >= 3
-    candidates = set([i for i in range(len(athletes))])
+    candidates = [i for i in range(len(athletes))]
     result = -1
     while candidates:
-        candidate = candidates.pop()
-        to_remove = set()
+        candidate = random.choice(candidates)
+        to_remove = set([candidate])
         beatcount = 0
         for other in range(len(athletes)):
             if beats(athletes[candidate], athletes[other]):
                 beatcount += 1
                 to_remove.add(other)
-        candidates -= to_remove
         if beatcount == len(athletes) - 1:
             result = candidate + 1
             break
+        candidates = [x for x in candidates if x not in to_remove]
     print(result)
