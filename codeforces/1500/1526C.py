dda1d563a8846a7ae7bf26fb22970aa75580893c
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-09 14:47:58 +0200

    greedy solution 1526C

--- codeforces/1500/1526C.py
+++ codeforces/1500/1526C.py
@@ -1,34 +1,17 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+from heapq import heappop, heappush
 
 nums = list(map(int, lines[1].split(" ")))
-dp = [[-10**10] * (len(nums) + 1) for _ in range(len(nums) + 1)]
-dp[0][0] = 0
-
-for i in range(1, len(nums) + 1):
-    curr_potion = nums[i - 1]
-    for j in range(len(nums) + 1):
-        if dp[i-1][j-1] + curr_potion >= 0:
-            dp[i][j] = max(dp[i-1][j-1] + curr_potion, dp[i-1][j])
-        else:
-            dp[i][j] = dp[i-1][j]
-
-
-# for i, xs in enumerate(dp):
-#     if i > 0:
-#         print("Curr:", nums[i-1])
-#     for x in xs:
-#         print(x if x != -10**10 else "x", end=" ")
-#     print()
-            
-result = 0
-for i in range(len(dp[0])):
-    if dp[-1][i] >= 0:
-        result = i
-print(result)
-
-
-
-
-
-
+health = 0
+hq = []
+for x in nums:
+    if health + x >= 0:
+        heappush(hq, x)
+        health += x
+    elif hq:
+        if x > hq[0]:
+            prev = heappop(hq)
+            heappush(hq, x)
+            health += x - prev
+print(len(hq))
