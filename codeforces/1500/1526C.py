5aaaf1ebb663fad5ce49cb6ed758e064f45a10ab
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-09 14:16:11 +0200

    do dp solution of 1526C easy instead

--- codeforces/1500/1526C.py
+++ codeforces/1500/1526C.py
@@ -2,26 +2,33 @@ import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 nums = list(map(int, lines[1].split(" ")))
+dp = [[-10**10] * (len(nums) + 1) for _ in range(len(nums) + 1)]
+dp[0][0] = 0
 
-def check(xs):
-    running = 0
-    for x in xs:
-        running += x
-        if running < 0:
-            return False
-    return True
-xs = [x if x >= 0 else 0 for x in nums]
-negatives = [(x, i) for i, x in enumerate(nums) if x < 0]
-negatives.sort(key=lambda x: (-x[0], -x[1]))
-result = len([x for x in nums if x >= 0])
-for x, i in negatives:
-    xs[i] = x
-    if check(xs):
-        # print("Took", x, i)
-        result += 1
-    else:
-        xs[i] = 0
+for i in range(1, len(nums) + 1):
+    curr_potion = nums[i - 1]
+    for j in range(len(nums) + 1):
+        if dp[i-1][j-1] + curr_potion >= 0:
+            dp[i][j] = max(dp[i-1][j-1] + curr_potion, dp[i-1][j])
+        else:
+            dp[i][j] = dp[i-1][j]
+
+
+# for i, xs in enumerate(dp):
+#     if i > 0:
+#         print("Curr:", nums[i-1])
+#     for x in xs:
+#         print(x if x != -10**10 else "x", end=" ")
+#     print()
+            
+result = 0
+for i in range(len(dp[0])):
+    if dp[-1][i] >= 0:
+        result = i
 print(result)
+
+
+
 
 
 
