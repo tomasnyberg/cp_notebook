643a5b40ecccc9cf4e9b9b386fdf3f005bdadbfd
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-10 10:22:04 +0200

    other solution

--- codeforces/1500/1516B.py
+++ codeforces/1500/1516B.py
@@ -7,7 +7,10 @@ for line in lines[2::2]:
     prefix = [0] * 2005
     for i in range(1, n+1):
         prefix[i] = prefix[i-1] ^ nums[i - 1]
-    good = prefix[n] == 0
+    good = False
+    for i in range(1, n+1):
+        if prefix[i] == prefix[n]^prefix[i]:
+            good = True
     for i in range(1, n+1):
         for j in range(i+1, n):
             good |= prefix[i] == (prefix[j]^prefix[i]) == (prefix[n]^prefix[j])
