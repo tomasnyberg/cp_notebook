0a564fddd631e591570de4fd3e8abc038e80b241
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-10 09:52:40 +0200

    attempt 1516B, Wa18

---
+++ codeforces/1500/1516B.py
@@ -0,0 +1,23 @@
+import sys
+lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split(" ")))
+    running = 0
+    for x in nums:
+        running ^= x
+    if running == 0:
+        print("YES")
+        continue
+    good = True
+    count = 0
+    while nums:
+        curr = nums.pop()
+        while nums and curr != running:
+            curr ^= nums.pop()
+        if curr != running:
+            good = False
+            break
+        count += 1
+    print("YES" if good and count >= 2 else "NO")
+
