890b9c57511d1c271db9ad0b22a5e0dc9a69ba0b
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-12 12:04:36 +0200

    closer

--- codeforces/1600/1631C.py
+++ codeforces/1600/1631C.py
@@ -19,24 +19,39 @@ maxes = {}
 for i in range(2,17):
     maxes[2**i] = find_sum(2**i)
 
-print(maxes)
-
 def solve(n, k):
     nums = [i for i in range(n)]
     q = deque(nums)
     pairs = []
-    while k - q[-1] & q[-2] >= 0:
+    while q and k - (q[-1] & q[-2]) >= 0:
         a, b = q.pop(), q.pop()
+        pairs.append((a, b))
         k-= a & b
-    assert k == 0 or k < q[-1] + 1
+    print(k)
+    look_for = set()
+    for i in range(18):
+        if (1 << i) & k:
+            look_for.add((1 << i) + 1)
+    while q:
+        if q[-1] in look_for:
+            a, b = q.pop(), q.pop()
+            pairs.append((a, b))
+            k -= a & b
+        else:
+            a, b =  q.popleft(),  q.pop()
+            pairs.append((a, b))
+            k -= a & b
+    print(pairs)
+    
+
     # if k > 0:
     #     print(k, bin(k), q[-1], bin(q[-1]), len(bin(q[-1])[2:]), len(bin(k)[2:]))
     # print(k,q)
     # for x in q:
     #     print(bin(x)[2:].zfill(max([len(bin(x)[2:]) for x in q])))
 
-for i in range(1000000):
-    solve(2**8, random.randint(0, maxes[2**8]))
+# for i in range(1000000):
+#     solve(2**8, random.randint(0, maxes[2**8]))
 
 
 for line in lines[1:]:
@@ -44,4 +59,5 @@ for line in lines[1:]:
     if k > maxes[n]:
         print(-1)
         continue
+    solve(n, k)
 
