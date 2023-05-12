d86975af7045557f0b5cd6dfabaa2bfa2b8787e8
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-12 18:41:19 +0200

    solve 1631C

--- codeforces/1600/1631C.py
+++ codeforces/1600/1631C.py
@@ -1,63 +1,54 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 from collections import deque
-import random
+# import random
 
-def find_sum(n):
-    def total_and(nums):
-        q = deque(nums)
-        total = 0
-        while q:
-            a, b = q.pop(), q.pop()
-            total += a & b
-        return total
-    # n = 2**n
-    nums = [i for i in range(n)]
-    return total_and(nums)
+# def find_sum(n):
+#     def total_and(nums):
+#         q = deque(nums)
+#         total = 0
+#         while q:
+#             a, b = q.pop(), q.pop()
+#             total += a & b
+#         return total
+#     # n = 2**n
+#     nums = [i for i in range(n)]
+#     return total_and(nums)
 
-maxes = {}
-for i in range(2,17):
-    maxes[2**i] = find_sum(2**i)
+# maxes = {}
+# for i in range(2,17):
+#     maxes[2**i] = find_sum(2**i)
 
-def solve(n, k):
-    nums = [i for i in range(n)]
-    q = deque(nums)
-    pairs = []
-    while q and k - (q[-1] & q[-2]) >= 0:
-        a, b = q.pop(), q.pop()
-        pairs.append((a, b))
-        k-= a & b
-    print(k)
-    look_for = set()
-    for i in range(18):
-        if (1 << i) & k:
-            look_for.add((1 << i) + 1)
-    while q:
-        if q[-1] in look_for:
-            a, b = q.pop(), q.pop()
-            pairs.append((a, b))
-            k -= a & b
-        else:
-            a, b =  q.popleft(),  q.pop()
-            pairs.append((a, b))
-            k -= a & b
-    print(pairs)
-    
+# print(maxes)
 
-    # if k > 0:
-    #     print(k, bin(k), q[-1], bin(q[-1]), len(bin(q[-1])[2:]), len(bin(k)[2:]))
-    # print(k,q)
-    # for x in q:
-    #     print(bin(x)[2:].zfill(max([len(bin(x)[2:]) for x in q])))
+# print(find_sum(2**16))
 
-# for i in range(1000000):
-#     solve(2**8, random.randint(0, maxes[2**8]))
+# def solve(n, k):
+#     nums = i for i in range
 
 
 for line in lines[1:]:
     n, k = map(int, line.split(" "))
-    if k > maxes[n]:
-        print(-1)
-        continue
-    solve(n, k)
+    mappings = {}
+    for i in range(n // 2):
+        mappings[i] = n - i - 1
+    if k == n-1:
+        if n == 4:
+            print(-1)
+            continue
+        mappings[n-1] = n-2
+        mappings[1] = 3
+        mappings[0] = n-4
+        del mappings[3]
+    else:
+        mappings[k] = n-1
+        mappings[n-k-1] = 0
+        del mappings[0]
+
+    for x in mappings:
+        if mappings[x] not in mappings:
+            print(x, mappings[x])
+
+        k-=x & mappings[x]
+    assert k == 0
 
