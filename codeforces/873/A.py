5bda5275aa18cc9ef0e6d20f617be64fafdb6561
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-14 16:39:36 +0200

    solve A

--- codeforces/873/A.py
+++ codeforces/873/A.py
@@ -1,2 +1,10 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    n = int(line)
+    xs = []
+    for i in range(1, n+1):
+        xs.append(i if n % 2 == 1 else i*2)
+    # assert sum(xs) % n == 0
+    print(*xs)
