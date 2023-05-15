e5103b894718e347fdb6a7e8a910636be363af98
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-15 16:36:41 +0200

    working on 455A, RE because deep recursion

---
+++ codeforces/0400/455A.py
@@ -0,0 +1,42 @@
+import sys
+from functools import lru_cache
+lines = list(map(str.strip, sys.stdin.readlines()))
+
+def optimal(xs):
+    @lru_cache(None)
+    def recur(i, prevtaken):
+        if i == len(xs):
+            return 0
+        if prevtaken:
+            return recur(i+1, False)
+        else:
+            return max(xs[i] + recur(i+1, True), recur(i+1, False))
+    return recur(0, False)
+
+nums = list(map(int, lines[1].split(" ")))
+counts = {}
+for x in nums:
+    counts[x] = 1 if x not in counts else counts[x] + 1
+result = 0
+taken = set()
+for key in counts:
+    if key in taken: continue
+    taken.add(key)
+    curr_xs = [key*counts[key]]
+    ptr = key - 1
+    add_behind = []
+    while ptr in counts:
+        taken.add(ptr)
+        add_behind.append(ptr*counts[ptr])
+        ptr -= 1
+    curr_xs = add_behind[::-1] + curr_xs
+    ptr = key + 1
+    while ptr in counts:
+        taken.add(ptr)
+        curr_xs.append(ptr*counts[ptr])
+        ptr += 1
+    for x in curr_xs:
+        taken.add(x)
+    result += optimal(curr_xs)
+print(result)
+    
