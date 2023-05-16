8da1d60918bf8f54d3648aa9879ecf9eae012887
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-16 11:33:47 +0200

    remove bad functions

--- codeforces/1600/1614C.py
+++ codeforces/1600/1614C.py
@@ -1,27 +1,6 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-def generate_xor_sum(arr):
-    result = 0
-    n = len(arr)
-    subseq_count = 2**(n-1)
-
-    for num in arr:
-        result ^= num * subseq_count
-
-    return result
-
-def generate_xor_sum_bf(arr):
-    total = 0
-    for i in range(2**len(arr)):
-        result = 0
-        for j in range(len(arr)):
-            if i & (1 << j):
-                result ^= arr[j]
-        total += result
-        # print(result, end=" ")
-    return total
-
 def xorSum(arr, n):
     bits = 0
     for i in range(n):
