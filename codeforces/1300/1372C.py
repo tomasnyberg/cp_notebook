a4c9cc63c0517ab5ac6f7042eb74c75841ec6fc5
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-16 19:34:03 +0200

    solve 1372C

--- codeforces/1300/1372C.py
+++ codeforces/1300/1372C.py
@@ -3,11 +3,18 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 
 for line in lines[2::2]:
     nums = list(map(int, line.split(" ")))
-    if nums == list(sorted(nums)):
+    expected = [i for i in range(1, len(nums)+1)]
+    if nums == expected:
         print(0)
         continue
+    for _ in range(2):
+        while nums and nums[-1] == expected[-1]:
+            nums.pop()
+            expected.pop()
+        nums.reverse()
+        expected.reverse()
     for i in range(len(nums)):
-        if nums[i] == i+1:
+        if nums[i] == expected[i]:
             print(2)
             break
     else:
