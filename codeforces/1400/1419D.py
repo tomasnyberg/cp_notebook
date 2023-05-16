ac10646aec914bda583a651dffdeb882ab34aea8
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-16 19:57:51 +0200

    wa3 on 1419D

---
+++ codeforces/1400/1419D.py
@@ -0,0 +1,43 @@
+import sys
+lines = list(map(str.strip, sys.stdin.readlines()))
+
+n = int(lines[0])
+if n == 1 or n == 2:
+    print(0)
+    print(*list(range(1, n+1)))
+    exit()
+
+nums = list(map(int, lines[1].split(" ")))
+nums.sort()
+def check(x, nums, construct=False):
+    if x == 0:
+        if not construct:
+            return len(nums) >= 1
+        else:
+            return nums
+    nums = nums[:][::-1]
+    to_insert = []
+    for _ in range(x):
+        to_insert.append(nums.pop())
+    while nums and nums[-1] == to_insert[-1]:
+        nums.pop()
+    if not construct:
+        return len(nums) >= x + 1
+    else:
+        result = []
+        while to_insert:
+            result.append(nums.pop())
+            result.append(to_insert.pop())
+        result.append(nums.pop())
+        return result
+
+low = 0
+high = len(nums) - 1
+while low < high:
+    mid = (low + high) // 2
+    if check(mid, nums):
+        low = mid + 1
+    else:
+        high = mid
+print(low-1)
+print(*check(low-1, nums, True))
