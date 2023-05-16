fc98c352244c50166fcf99e62d5ec6a4ff9ab70b
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-16 10:38:31 +0200

    solve 1499C

--- codeforces/1400/1499C.py
+++ codeforces/1400/1499C.py
@@ -4,28 +4,28 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 
 # Assume we always start going right
 def solve(n, costs):
-    rights = ceil(n//2)
-    ups = n//2
-    smallest_right = 10**10
-    smallest_up = 10**10
-    rightsum = 0
-    upsum = 0
-    for i in range(0, n, 2):
-        smallest_right = min(costs[i], smallest_right)
-        rightsum += costs[i]
-    for i in range(1, n, 2):
-        smallest_up = min(costs[i], smallest_up)
-        upsum += costs[i]
-    rightsum -= smallest_right
-    upsum -= smallest_up
-    right_long = n - rights + 1
-    up_long = n - ups + 1
-    return rightsum + upsum + right_long*smallest_right + up_long*smallest_up
-
+    oddcnt = 0
+    evencnt = 0
+    oddsum = 0
+    evensum = 0
+    small_even = 10**20
+    small_odd = 10**20
+    result = 10**30
+    for i in range(n):
+        if i % 2 == 0:
+            small_even = min(small_even, costs[i])
+        else:
+            small_odd = min(small_odd, costs[i])
+        result = min(result, oddsum + (n-oddcnt)*small_odd + evensum + (n-evencnt)*small_even)
+        if i % 2 == 0:
+            evencnt += 1
+            evensum += costs[i]
+        else:
+            oddcnt += 1
+            oddsum += costs[i]
+    return result
+    
 for line in lines[2::2]:
     nums = list(map(int, line.split(" ")))
     n = len(nums)
     print(solve(n, nums))
-
-
-
