415b52b730e92b26217598aefcb41e1bdd3c71a1
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-17 04:46:43 +0200

    solve A

--- codeforces/edu148/A.py
+++ codeforces/edu148/A.py
@@ -1,2 +1,13 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    if len(line) <= 3:
+        print("NO")
+        continue
+    for i in range(len(line) // 2 - 1):
+        if line[i] != line[i+1]:
+            print("YES")
+            break
+    else:
+        print("NO")
