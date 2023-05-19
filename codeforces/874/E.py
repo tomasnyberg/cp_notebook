809fb660c81874785abf26c3c8a6daa88317492b
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-19 17:17:10 +0200

    one part of E

--- codeforces/874/E.py
+++ codeforces/874/E.py
@@ -1,2 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split(" ")))
+    parents = [i for i in range(len(nums))]
+    def find(x):
+        if parents[x] != x:
+            parents[x] = find(parents[x])
+        return parents[x]
+    def union(x, y):
+        parents[find(x)] = find(y)
+    for i in range(len(nums)):
+        union(i, nums[i]-1)
+    print(len(set(find(i) for i in range(len(nums)))))
