53d12b33e38860dbde66b9ce4e9f86b6319c449d
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-19 16:45:25 +0200

    solve C

--- codeforces/874/C.py
+++ codeforces/874/C.py
@@ -1,2 +1,16 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    a = list(map(int, line.split(" ")))
+    if all(x % 2 == a[0] % 2 for x in a):
+        print("YES")
+        continue
+    smallestodd = min(x for x in a if x % 2 == 1)
+    for x in a:
+        if x % 2 == 0:
+            if x < smallestodd:
+                print("NO")
+                break
+    else:
+        print("YES")
