09c3c1605385f5e4aa41c9e5876188504971392b
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-05-19 16:36:59 +0200

    solve A

--- codeforces/874/A.py
+++ codeforces/874/A.py
@@ -1,2 +1,8 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    seen = set()
+    for i in range(len(line) - 1):
+        seen.add(line[i:i+2])
+    print(len(seen))
