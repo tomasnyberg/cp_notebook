72cfe0affdc964212f8db572842f7ae5d2b1980d
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-02 11:45:31 +0200

    better solution for 1304C

--- codeforces/1300/1304C.py
+++ codeforces/1300/1304C.py
@@ -1,20 +1,6 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-def check_same_time_bad(customers):
-    i = 0
-    while i < len(customers):
-        arrive, low, high = customers[i]
-        i += 1
-        while i < len(customers) and customers[i][0] == arrive:
-            _, low_b, high_b = customers[i]
-            if low_b > high or high_b < low:
-                return True
-            low = max(low, low_b)
-            high = min(high, high_b)
-            i += 1
-    return False
-
 ii = 1
 while ii < len(lines):
     n, m = map(int, lines[ii].split())
@@ -24,9 +10,6 @@ while ii < len(lines):
         customers.append(list(map(int, lines[ii].split())))
         ii += 1
     customers.sort()
-    if check_same_time_bad(customers):
-        print("NO")
-        continue
     curr_temp = m
     curr_time = 0
     low = curr_temp
