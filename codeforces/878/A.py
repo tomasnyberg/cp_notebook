41ec3b3e1fbdf103d56aadc9c6eba43a429ceab7
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-07 10:04:48 +0200

    solve A

--- codeforces/878/A.py
+++ codeforces/878/A.py
@@ -1,2 +1,14 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    i = 0
+    result = ""
+    while i < len(line):
+        curr = line[i]
+        result += curr
+        i+=1
+        while i < len(line) and line[i] != curr:
+            i+=1
+        i+=1
+    print(result)
