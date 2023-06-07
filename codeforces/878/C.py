607988e12f582f025d22e1e9640517de43fa5a42
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-07 10:16:54 +0200

    solve C

--- codeforces/878/C.py
+++ codeforces/878/C.py
@@ -1,2 +1,22 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for ii in range(1, len(lines), 2):
+    n, k, q = map(int, lines[ii].split())
+    a = list(map(int, lines[ii+1].split()))
+    result = 0
+    subs = []
+    curr = []
+    for x in a:
+        if x > q:
+            if len(curr) >= k:
+                subs.append(curr)
+            curr = []
+        else:
+            curr.append(x)
+    if len(curr) >= k:
+        subs.append(curr)
+    for sub in subs:
+        for i in range(len(sub)-k+1):
+            result += len(sub)-i-k+1
+    print(result)
