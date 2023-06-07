b81b62abd2c3906648d04844c03de5ecf4f894a3
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-07 10:08:58 +0200

    solve B

--- codeforces/878/B.py
+++ codeforces/878/B.py
@@ -1,2 +1,9 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    n, k = map(int, line.split())
+    if k >= 33:
+        print(n+1)
+    else:
+        print(min(n, (1 << k) - 1) + 1)
