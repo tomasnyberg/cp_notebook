bdc7107b4462e67ee75ae76805df489905bc6cfe
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-07 10:43:29 +0200

    solve D

--- codeforces/878/D.py
+++ codeforces/878/D.py
@@ -1,2 +1,29 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+import bisect
+ 
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split()))
+    nums.sort()
+    low = 0
+    high = 10**9
+    while low < high:
+        mid = (low + high) // 2
+        first = nums[0] + mid
+        idx = bisect.bisect_right(nums, first + mid)
+        if idx == len(nums):
+            high = mid
+            continue
+        second = nums[idx] + mid
+        idx2 = bisect.bisect_right(nums, second + mid)
+        if idx2 == len(nums):
+            high = mid
+            continue
+        third = nums[idx2] + mid
+        if abs(nums[-1] - third) <= mid:
+            high = mid
+        else:
+            low = mid + 1
+    print(low)
+    
