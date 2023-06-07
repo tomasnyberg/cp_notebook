c7ae3ada714178a542113c02c6de1556bf23d5ac
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-07 18:07:03 +0200

    still working on G

--- codeforces/878/G.py
+++ codeforces/878/G.py
@@ -19,41 +19,28 @@ def query_counter_clockwise(x):
     sys.stdout.flush()
     return read_int()
 
-nums = [i for i in range(1, 10**6)]
-random.shuffle(nums)
-
-first_thousand = []
-first_thousand_set = set()
-at_least = read_int()
-at_most = 10**6
-first_thousand.append(at_least)
-first_thousand_set.add(at_least)
-count = 0
+# nums = [i for i in range(1, 202234)]
+# random.shuffle(nums)
+first = read_int()
+first_thousand = [first]
+positions = {first:1}
+count = 1
 while len(first_thousand) < 1000:
+    # res = nums[count]
+    res = query_clockwise(1) # UNCOMMENT
     count += 1
-    res = query_clockwise(1)
-    if res in first_thousand_set:
-        print("!", count)
+    if res in positions:
+        print("!", count - positions[res])
         sys.exit(0)
-    at_least = max(at_least, res)
     first_thousand.append(res)
-    first_thousand_set.add(res)
-query_counter_clockwise(500)
+    positions[res] = count
 
-queries = 1000
+pos = 1001
 while True:
-    # res = query_clockwise(at_least) # uncomment 
-    # query_counter_clockwise(at_least)
-    res = nums[(500 + at_least - 1) % len(nums)]
-    at_least = max(at_least, res)
-    queries += 2
-    if res in first_thousand_set:
-        print(res, at_least)
-        idx = first_thousand.index(res)
-        if idx < 500:
-            print_ans(at_least + 500 - idx)
-        else:
-            print_ans(at_least - (idx - 500))
-        print("queries:", queries)
-        break
-    at_least += 500
+    pos += 1000
+    # res = nums[pos % len(nums)]
+    res = query_clockwise(1000) #UNCOMMENT
+    if res in positions:
+        print("!", pos - positions[res])
+        sys.exit(0)
+    positions[res] = pos
