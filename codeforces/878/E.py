2808124d3e241c54550a68cf4d1f21bdb2284c7c
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-07 11:00:37 +0200

    solve E

--- codeforces/878/E.py
+++ codeforces/878/E.py
@@ -19,12 +19,12 @@ while ii < len(lines):
     for i in range(q):
         query = list(map(int, lines[ii+i].split()))
         time +=1
-        while blocked and blocked[0] <= time:
-            idx = blocked.popleft()
+        while blocked and blocked[0][0] <= time:
+            _, idx = blocked.popleft()
             if a[idx] != b[idx]:
                 notequal.add(idx)
         if query[0] == 1:
-            blocked.append(query[1]-1)
+            blocked.append((time + t, query[1]-1))
             notequal.discard(query[1]-1)
         elif query[0] == 2:
             first_idx, a_swap, second_idx, b_swap = query[1]-1, query[2]-1, query[3]-1, query[4]-1
@@ -36,4 +36,5 @@ while ii < len(lines):
                     notequal.discard(idx)
         else:
             print("YES" if len(notequal) == 0 else "NO")
+        # print(''.join(a), ''.join(b))
     ii += q
