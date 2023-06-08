2bf968eaaf1e435894a49dc5a7ab7564d732d243
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-08 15:38:57 +0200

    WA7 on 1307C

---
+++ codeforces/1300/1307C.py
@@ -0,0 +1,25 @@
+import sys
+lines = list(map(str.strip, sys.stdin.readlines()))
+from collections import Counter
+
+def combo_breaker(s, combo):
+    firstseen = 0
+    result = 0
+    for c in s:
+        if c == combo[0]:
+            firstseen += 1
+        elif c == combo[1]:
+            result += firstseen
+    return result
+
+for s in lines:
+    d = dict(Counter(s))
+    result = max(d.values())
+    for i in range(26):
+        for j in range(26):
+            first = chr(ord('a') + i)
+            second = chr(ord('a') + j)
+            if first == second: continue
+            result = max(result, combo_breaker(s, (first, second)))
+    print(result)
+            
