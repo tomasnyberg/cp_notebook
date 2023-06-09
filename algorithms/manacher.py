24a407dd80e7ef46e551521e9743067b1c5cf6e0
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-09 15:12:43 +0200

    add manacher algorithm

--- algorithms/manacher.py
+++ algorithms/manacher.py
@@ -14,10 +14,14 @@ def manacher(s):
         if R > i:
             i_mirror = 2*C - i  # i's mirror position in the previously calculated palindrome
             P[i] = min(R-i, P[i_mirror])  # Truncate P[i] at R if it overflows
+        # Attempt to expand
         while s[i + 1 + P[i]] == s[i - 1 - P[i]]:
             P[i] += 1
+        # Update C and R if i's palindrome expands past R
         if i + P[i] > R:
             C, R = i, i + P[i]
     max_length = max(P)
     center_index = P.index(max_length)
     return s[center_index - max_length:center_index + max_length].replace("#", "")
+
+print(manacher("abcdfdcecba"))
