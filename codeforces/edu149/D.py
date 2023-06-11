55bdeec89951cee745eedf1ae5566393868d5aa8
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-11 13:05:50 +0200

    still wa2 on D

--- codeforces/edu149/D.py
+++ codeforces/edu149/D.py
@@ -8,18 +8,33 @@ for line in lines[2::2]:
     st = []
     colors = [-1]*len(line)
     line = list(enumerate(line))
+    currcolor = 1
     for i,c in line:
         if c == '(':
             st.append((c,i))
         else:
             if st and st[-1][0] == '(':
                 _, idx = st.pop()
-                colors[idx] = 1
-                colors[i] = 1
+                colors[idx] = currcolor
+                colors[i] = currcolor
             else:
                 st.append((c,i))
-    for i in range(len(colors)):
-        if colors[i] == -1:
-            colors[i] = 2 if len(st) < len(line) else 1
+    if len(st) != len(line):
+        currcolor += 1
+    line = st
+    st = []
+    for c, i in line:
+        if c == ')':
+            st.append((c,i))
+        else:
+            if st and st[-1][0] == ')':
+                _, idx = st.pop()
+                colors[idx] = currcolor
+                colors[i] = currcolor
+            else:
+                st.append((c,i))
+    if st:
+        print(-1)
+        continue
     print(len(set(colors)))
     print(*colors)
