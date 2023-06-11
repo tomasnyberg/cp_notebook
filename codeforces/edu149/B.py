490a8737c0487380cbfc6d4259e64d55aa592344
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-11 12:22:02 +0200

    solve B

--- codeforces/edu149/B.py
+++ codeforces/edu149/B.py
@@ -1,2 +1,16 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    result = 0
+    curr = 0
+    currchar = '0'
+    for c in line:
+        if c == currchar:
+            curr += 1
+            result = max(result, curr)
+        else:
+            currchar = c
+            curr = 1
+    result = max(result, curr)
+    print(result + 1)
