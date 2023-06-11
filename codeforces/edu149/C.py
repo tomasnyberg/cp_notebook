a0c49d20e883ea481135ac189a156172ae1f1821
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-11 12:49:54 +0200

    solve C with a terrible solution

--- codeforces/edu149/C.py
+++ codeforces/edu149/C.py
@@ -2,30 +2,45 @@ import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 def try_replace(start, end, line):
-    biggest = [-1,-1]
-    for replacement in ['0', '1']:
-        for i in range(start, end):
-            line[i] = replacement
-        count = 0
-        left = start
-        right = start + 1
-        while left >= 0:
-            if line[left] != replacement:
-                break
-            count += 1
-            left -= 1
-        while right < len(line):
-            if line[right] != replacement:
-                break
-            count += 1
-            right += 1
-        biggest = max(biggest, [count, replacement])
+    total = 0
+    if start > 0:
+        total += line[start-1]
+    if end < len(line):
+        total += line[end]
+    replacer = '.'
+    if total >= 0:
+        replacer = 1
+    else:
+        replacer = -1
     for i in range(start, end):
-        line[i] = biggest[1]
-
+        line[i] = replacer
+        
 for line in lines[1:]:
     i = 0
     line = list(line)
+    curr = '.'
+    count = 0
+    for i in range(len(line)):
+        if line[i] == '?':
+            j = i - 1
+            for j in range(i-1, i - 1- count, -1):
+                line[j] = count if curr == '0' else -count
+            count = 0
+            curr = '.'
+            continue
+        elif line[i] == curr:
+            count += 1
+        else:
+            j = i - 1
+            for j in range(i-1, i - 1- count, -1):
+                line[j] = count if curr == '0' else -count
+            count = 1
+            curr = line[i]
+    i = len(line)
+    j = i - 1
+    for j in range(i-1, i - 1- count, -1):
+        line[j] = count if curr == '0' else -count
+    i = 0
     while i < len(line):
         if line[i] != '?':
             i += 1
@@ -34,5 +49,10 @@ for line in lines[1:]:
         while i < len(line) and line[i] == '?':
             i += 1
         try_replace(j, i, line)
+    for i in range(len(line)):
+        if line[i] < 0:
+            line[i] = '1'
+        else:
+            line[i] = '0'
     print(''.join(line))
         
