ee17d53b4e14e1a25495bdb28f7664765e5bbf25
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-11 12:18:16 +0200

    solve A

--- codeforces/edu149/A.py
+++ codeforces/edu149/A.py
@@ -1,2 +1,11 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    x, k = map(int, line.split())
+    if x % k == 0:
+        print(2)
+        print(1, x - 1)
+    else:
+        print(1)
+        print(x)
