a5c6d0198e7523ceaeea5fe4334884d295fdab78
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-11 13:16:17 +0200

    solve D

--- codeforces/edu149/D.py
+++ codeforces/edu149/D.py
@@ -1,19 +1,20 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-for line in lines[2::2]:
+def solve(line, first):
     if len(line) % 2:
-        print(-1)
-        continue
+        return (-1, -1)
     st = []
     colors = [-1]*len(line)
     line = list(enumerate(line))
     currcolor = 1
+    check = '(' if first else ')'
+    second_check = ')' if first else '('
     for i,c in line:
-        if c == '(':
+        if c == check:
             st.append((c,i))
         else:
-            if st and st[-1][0] == '(':
+            if st and st[-1][0] == check:
                 _, idx = st.pop()
                 colors[idx] = currcolor
                 colors[i] = currcolor
@@ -24,17 +25,36 @@ for line in lines[2::2]:
     line = st
     st = []
     for c, i in line:
-        if c == ')':
+        if c == second_check:
             st.append((c,i))
         else:
-            if st and st[-1][0] == ')':
+            if st and st[-1][0] == second_check:
                 _, idx = st.pop()
                 colors[idx] = currcolor
                 colors[i] = currcolor
             else:
                 st.append((c,i))
     if st:
+        return (-1,-1)
+    return (len(set(colors)), colors)
+
+for line in lines[2::2]:
+    a, c1 = solve(line, True)
+    b, c2 = solve(line, False)
+    if a == -1 and b == -1:
         print(-1)
         continue
-    print(len(set(colors)))
-    print(*colors)
+    if a == -1:
+        print(b)
+        print(' '.join(map(str, c2)))
+        continue
+    if b == -1:
+        print(a)
+        print(' '.join(map(str, c1)))
+        continue
+    if a < b:
+        print(a)
+        print(' '.join(map(str, c1)))
+    else:
+        print(b)
+        print(' '.join(map(str, c2)))
