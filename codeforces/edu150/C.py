0bb93293d8c754ebd12f34ab6f580c4c98b09179
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-12 17:03:15 +0200

    WA2 on C

--- codeforces/edu150/C.py
+++ codeforces/edu150/C.py
@@ -3,7 +3,7 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 
 values = {'A':1, 'B':10, 'C':100, 'D':1000, 'E':10000}
 
-def calculate_score(s):
+def calculate_score(line):
     biggest = ['A' for _ in range(len(line) + 1)]
     for i in range(len(line) - 1, -1, -1):
         biggest[i] = max(biggest[i + 1], line[i])
@@ -16,12 +16,25 @@ def calculate_score(s):
             result += values[line[i]]
             # print("+", values[line[i]])
     return result
-
+# s = "ABCEEEDCBA"
+# s = list(s)
+# for i in range(len(s)):
+#     temp = s[i]
+#     s[i] = 'E'
+#     print(calculate_score(s))
+#     s[i] = temp
+# print(calculate_score(s))
 
 for line in lines[1:]:
     line = list(line)
-    for i in range(len(line)):
-        if line[i] != 'E':
-            line[i] = 'E'
-            break
-    print(calculate_score(line))
+    result = 0
+    for k in values:
+        idx = -1
+        for i in range(len(line)):
+            if line[i] == k:
+                line[i] = 'E'
+                idx = i
+                break
+        result = max(result, calculate_score(line))
+        line[idx] = k
+    print(result)
