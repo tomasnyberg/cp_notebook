0dbd3ed60f8aff18ce349336b7d8459c68a39ccd
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-12 17:49:25 +0200

    wa5 on D

--- codeforces/edu150/D.py
+++ codeforces/edu150/D.py
@@ -1,2 +1,35 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+from collections import defaultdict
+from functools import lru_cache
+
+ii = 1
+while ii < len(lines):
+    n = int(lines[ii])
+    ii+=1
+    ranges = []
+    for _ in range(n):
+        l, r = list(map(int, lines[ii].split()))
+        ranges.append((l, r))
+        ii+=1
+    ranges.sort()
+    @lru_cache(None)
+    def recur(i):
+        if i == len(ranges):
+            return 0
+        result = 1 + recur(i+1)
+        removed = 0
+        for j in range(i+1, len(ranges)):
+            if ranges[j][0] > ranges[i][1]:
+                break
+            extra = 0
+            k = j + 1
+            while k < len(ranges) and ranges[k][0] <= ranges[j][1]:
+                extra += 1
+                k += 1
+            result = min(result, removed + extra + recur(k))
+            removed += 1
+        return result
+    print(recur(0))
+
+        
