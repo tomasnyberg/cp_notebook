979f2ac3f1a81d77c86d8741ee81e4dde4c17d46
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-12 16:57:58 +0200

    refactor

--- codeforces/edu150/C.py
+++ codeforces/edu150/C.py
@@ -3,16 +3,10 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 
 values = {'A':1, 'B':10, 'C':100, 'D':1000, 'E':10000}
 
-for line in lines[1:]:
-    line = list(line)
-    for i in range(len(line)):
-        if line[i] != 'E':
-            line[i] = 'E'
-            break
+def calculate_score(s):
     biggest = ['A' for _ in range(len(line) + 1)]
     for i in range(len(line) - 1, -1, -1):
         biggest[i] = max(biggest[i + 1], line[i])
-    print(''.join(line))
     result = 0
     for i in range(len(line)):
         if line[i] < biggest[i+1]:
@@ -21,4 +15,13 @@ for line in lines[1:]:
         else:
             result += values[line[i]]
             # print("+", values[line[i]])
-    print(result)
+    return result
+
+
+for line in lines[1:]:
+    line = list(line)
+    for i in range(len(line)):
+        if line[i] != 'E':
+            line[i] = 'E'
+            break
+    print(calculate_score(line))
