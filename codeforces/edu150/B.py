1d9f720b74e1498cb0fde19b715843551136e8e6
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-12 16:38:44 +0200

    WA2 on B

--- codeforces/edu150/B.py
+++ codeforces/edu150/B.py
@@ -1,2 +1,23 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    queries = list(map(int, line.split()))
+    nums = []
+    first = True
+    for x in queries:
+        if first:
+            if not nums or x >= nums[-1]:
+                nums.append(x)
+            else:
+                nums.append(x)
+                first = False
+            print("1", end="")
+        else:
+            if x >= nums[-1] and x <= nums[0]:
+                nums.append(x)
+                print("1", end="")
+            else:
+                print("0", end="")
+    print()
+
