510052c65baab132c9833d479fa5537e55272246
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-19 00:42:01 +0200

    solve B

--- codeforces/881/B.py
+++ codeforces/881/B.py
@@ -1,2 +1,18 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split()))
+    nums = list(filter(lambda x: x != 0, nums))
+    streak = 0
+    result = 0
+    result2 = 0
+    for num in nums:
+        if num <= 0:
+            streak = 1
+        else:
+            result += streak
+            streak = 0
+        result2 += abs(num)
+    print(result2, result + streak)
+
