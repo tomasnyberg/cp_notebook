2e195833ed7aae8f8bb48ae29a5c217a72c029a5
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-19 00:44:28 +0200

    solve C

--- codeforces/881/C.py
+++ codeforces/881/C.py
@@ -1,2 +1,11 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[1:]:
+    running = ""
+    result = 0
+    for x in bin(int(line))[2:]:
+        running += x
+        result += int(running, 2)
+    print(result)
+        
