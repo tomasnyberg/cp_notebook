dcfbc125e06e4de8b46761e011158cc0b1b0ec9c
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-18 15:42:11 +0200

    upsolve 1622C

--- codeforces/1600/1622C.py
+++ codeforces/1600/1622C.py
@@ -1,51 +1,35 @@
+import math
 from collections import deque
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
 
 def cum_sum(nums):
-    curr = 0
-    result = [0]*len(nums)
-    for idx, num in enumerate(nums):
-        curr += num
-        result[idx] = curr
+    total = 0
+    result = []
+    for num in nums:
+        total += num
+        result.append(total)
     return result
 
-def check_swap_immediately(nums, k, first):
-    total = sum(nums[1:]) + first
-    if total <= k:
-        return nums[0] - first
-    count = 0
-    for i in range(len(nums) - 1,0,-1):
-        count += 1
-        total -= nums[i] - first
-        if total <= k:
-            return count + (nums[0] - first)
-    return -1
-
+# lines = """1
+# 5 7
+# 1 4 1 1 1""".split("\n")
 
 for ii in range(1, len(lines), 2):
     n, k = map(int, lines[ii].split())
     nums = list(map(int, lines[ii+1].split()))
     total = sum(nums)
-    nums.sort()
-    test = check_swap_immediately(nums, k, nums[0])
-    if test != -1:
-        print(test)
+    if total <= k:
+        print(0)
         continue
-    low = 0
-    high = 10**35
-    result = 10**35
-    while low < high:
-        mid = (low + high) // 2
-        test = check_swap_immediately(nums, k, nums[0] - mid)
-        if test != -1:
-            high = mid
-        else:
-            low = mid + 1
-    for i in range(low, low + 15):
-        result = min(result, check_swap_immediately(nums, k, nums[0] - i))
+    nums.sort()
+    CS = cum_sum(nums)
+    result = 10**25
+    for y in range(n):
+        # Maybe ceil
+        x = math.floor((k - CS[n-1-y] + CS[0])/(y + 1))
+        x = min(x, nums[0])
+        diff = nums[0] - x
+        result = min(y + diff, result)
     print(result)
-
-
-
