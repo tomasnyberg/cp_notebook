74f98ad6f7f26777a696c89d8d3e58222b786da9
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-18 16:12:35 +0200

    solve A

--- codeforces/880/A.py
+++ codeforces/880/A.py
@@ -1,2 +1,24 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split()))
+    robot_lines = []
+    robot_lines.sort()
+    bad = False
+    for x in nums:
+        if x == 0:
+            robot_lines.append([0])
+            continue
+        for xs in robot_lines:
+            if xs[-1] == x - 1:
+                xs.append(x)
+                break
+        else:
+            bad = True
+            break
+    if bad:
+        print("NO")
+    else:
+        print("YES")
+            
