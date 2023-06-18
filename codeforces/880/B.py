c8f9bbb9aca229ca7f232456040370d433fcb9e3
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-18 17:02:30 +0200

    close but not quite on B

--- codeforces/880/B.py
+++ codeforces/880/B.py
@@ -2,6 +2,13 @@ import sys
 import math
 lines = list(map(str.strip, sys.stdin.readlines()))
 
+# lines="""5
+# 3 3 100
+# 2 1 14
+# 91 2 13
+# 36 16 6
+# 73 8 22""".split("\n")
+
 for line in lines[1:]:
     n, k, g = map(int, line.split())
     # n to split between
@@ -14,15 +21,24 @@ for line in lines[1:]:
         give = g // 2 - 1
     else:
         give = g // 2
-    result = 0
-    for i in range(n + 1):
-        one_and_a_half = i * (give + g)
-        remaining = silver - one_and_a_half
-        if i < n and math.ceil(remaining / (n - i)) >= give:
-            continue
+    def check(x): # Can we give x employees give money?
+        given = x* give
+        remaining = silver - given
+        remaining_employees = n - x
+        if remaining_employees*(give + g) >= remaining:
+            return True
         else:
-            result = max(result, silver - i*g)
-    print(result)
-
+            return False
+    low = 0
+    high = n
+    while low < high:
+        mid = (low + high) // 2
+        if check(mid):
+            low = mid + 1
+        else:
+            high = mid
+    if not check(low):
+        low -= 1
+    print(silver - ((n - (low))*g))
         
 
