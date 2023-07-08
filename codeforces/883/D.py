1f63cc39b913497a7c5639c528bb1d5a40203a92
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-07-08 11:11:14 +0200

    solve D

--- codeforces/883/D.py
+++ codeforces/883/D.py
@@ -1,2 +1,19 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+def area(height, width):
+    return (height * width) / 2
+
+for ii in range(1, len(lines), 2):
+    n, d, h = map(int, lines[ii].split())
+    nums = list(map(int, lines[ii+1].split()))
+    result = 0
+    for x in nums:
+        result += area(h, d)
+    proportion = d / h
+    for i in range(len(nums) - 1):
+        overlap = nums[i] + h - nums[i+1]
+        # print("overlap between triangle at", nums[i], "and", nums[i+1], "is", overlap)
+        if overlap > 0:
+            result -= area(overlap, d - (h-overlap)*proportion)
+    print(result)
