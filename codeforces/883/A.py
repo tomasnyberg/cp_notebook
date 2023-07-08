1442b9a74b586c332087dfeec45955a16ccfa878
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-07-08 10:31:04 +0200

    solve A

--- codeforces/883/A.py
+++ codeforces/883/A.py
@@ -1,2 +1,14 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+ii = 1
+while ii < len(lines):
+    n = int(lines[ii])
+    ii+=1
+    result = 0
+    for _ in range(n):
+        a, b = map(int, lines[ii].split())
+        if b < a:
+            result += 1
+        ii+=1
+    print(result)
