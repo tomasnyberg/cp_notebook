5a9c1cf8c7383066b0d61369957f62dbd3429c37
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-07-09 17:41:31 +0200

    trying to solve E2, not quite there

--- codeforces/883/E.py
+++ codeforces/883/E.py
@@ -1,26 +1,52 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-seen = set()
-def construct(k):
-    total = k+1
-    leaves = k
-    total += leaves * k
-    leaves *= k
-    if total > 10**18:
-        return
-    seen.add(total)
-    for _ in range(10000):
-        total += leaves * k
-        leaves *= k
-        if total > 10**18:
-            break
-        seen.add(total)
+def calculate(x, iterations):
+    result = 1
+    orig_x = x
+    for i in range(iterations - 1):
+        result += x
+        x *= orig_x
+        if result > 10**18:
+            return 10**18 + 500
+    return result
 
-for k in range(2, 10**7):
-    construct(k)
-print(len(seen))
+# TODO we might want to first look at the small numbers since they grow super quickly
+
+# print(calculate(2, 4))
+# seen = set()
+# def construct(k):
+#     total = k+1
+#     leaves = k
+#     total += leaves * k
+#     leaves *= k
+#     if total > 10**18:
+#         return
+#     seen.add(total)
+#     for _ in range(10000):
+#         total += leaves * k
+#         leaves *= k
+#         if total > 10**18:
+#             break
+#         seen.add(total)
+
+# for k in range(2, 10**4):
+#     construct(k)
+# print(len(seen))
 
 for line in lines[1:]:
     n = int(line)
-    print("YES" if n in seen else "NO")
+    for iterations in range(3, 30):
+        low = 2
+        high = 10**18
+        while low < high:
+            mid = (low + high) // 2
+            if calculate(mid, iterations) < n:
+                low = mid + 1
+            else:
+                high = mid
+        if calculate(low, iterations) == n:
+            print("YES")
+            break
+    else:
+        print("NO")
