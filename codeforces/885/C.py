7d6a3c5b41e98279a8f54e3e45f7236e7b3d089e
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-07-16 17:22:57 +0200

    progress on C

--- codeforces/885/C.py
+++ codeforces/885/C.py
@@ -1,2 +1,18 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+def operation(a, b):
+    c = []
+    for x, y in zip(a,b):
+        c.append(abs(x-y))
+    return b, c
+
+for ii in range(1, len(lines), 3):
+    a = list(map(int, lines[ii+1].split()))
+    b = list(map(int, lines[ii+2].split()))
+    for i in range(50):
+        print("iteration", i)
+        print(a)
+        print(b)
+        a, b = operation(a, b)
+    # print("\n\n\n")
