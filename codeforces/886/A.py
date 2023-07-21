d86d0a1ae25c2db969982198f8c494bb7eece0c8
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-07-21 20:20:27 +0200

    solve A

--- codeforces/886/A.py
+++ codeforces/886/A.py
@@ -1,3 +1,7 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
-o
+
+for line in lines[1:]:
+    nums = list(map(int, line.split()))
+    nums.sort()
+    print("YES" if nums[-1] + nums[-2] >= 10 else "NO")
