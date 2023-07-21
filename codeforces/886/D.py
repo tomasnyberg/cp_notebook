e52416dccda37825ea27442793f02c23b3d98aca
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-07-21 20:43:05 +0200

    solve D

--- codeforces/886/D.py
+++ codeforces/886/D.py
@@ -1,2 +1,18 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for ii in range(1, len(lines), 2):
+    n, k = map(int, lines[ii].split())
+    a = list(map(int, lines[ii+1].split()))
+    a.sort()
+    prev = a[0]
+    streak = 0
+    result = 0
+    for x in a:
+        if abs(x-prev) <= k:
+            streak += 1
+            result = max(result, streak)
+        else:
+            streak = 1
+        prev = x
+    print(n - result)
