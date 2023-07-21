fef190fa62ec1e88c48c13a548a18f88807c44b1
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-07-21 20:34:52 +0200

    solve E

--- codeforces/886/E.py
+++ codeforces/886/E.py
@@ -1,2 +1,23 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+for ii in range(1, len(lines), 2):
+    n, c = map(int, lines[ii].split())
+    a = list(map(int, lines[ii+1].split()))
+    def check(mid):
+        total = 0
+        for x in a:
+            total += (x + mid)**2
+            if total > c:
+                return False
+        return total <= c
+    low = 0
+    high = 10**18
+    while low < high:
+        mid = (low + high) // 2
+        if check(mid):
+            low = mid + 1
+        else:
+            high = mid
+    print((low - 1) // 2)
+
