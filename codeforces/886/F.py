d978011ce01d9cc3bf6105c22184cf70eb8746bb
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-07-21 22:05:32 +0200

    solve G

--- codeforces/886/F.py
+++ codeforces/886/F.py
@@ -1,69 +1,33 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+from bisect import bisect_left
+from functools import lru_cache
+
+sys.setrecursionlimit(10**6)
 
 import math
 N = 10**5 + 100
 # stores smallest prime factor for every number
-spf = [0 for i in range(N)]
-def sieve():
-    spf[1] = 1
-    for i in range(2, N):
-        spf[i] = i
-    for i in range(4, N, 2):
-        spf[i] = 2
-    for i in range(3, math.ceil(math.sqrt(N))):
-        # Is i prime=
-        if (spf[i] == i):
-            # in that case, mark the spf of all numbers divisible by i as i
-            # (But only mark them if they have not previously been marked by a smaller number)
-            for j in range(i * i, N, i):
-                spf[j] = i if spf[j] == j else spf[j]
-sieve()
 
-# Very quickly finds the prime factors, since we always know what the
-# next one is.
-def prime_factors(x):
-    ret = list()
-    while (x != 1):
-        ret.append(spf[x])
-        x //= spf[x]
-    return ret
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
 
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.wordcount = 0
-        self.id = 0
-
-def insert(word: list[int], root: TrieNode):
-    curr = root
-    for c in word:
-        if c not in curr.children:
-            curr.children[c] = TrieNode()
-        curr = curr.children[c]
-    curr.wordcount += 1
-
-def amount_in_trie(word: list[int], root: TrieNode) -> bool:
-    def dfs(i, curr):
-        total = curr.wordcount
-        for c in curr.children:
-            for j in range(i, len(word)):
-                if word[j] == c:
-                    total += dfs(j + 1, curr.children[c])
-                    break
-        return total
-    return dfs(0, root)
+def lcm(a, b):
+    return a * b // gcd(a, b)
 
 for line in lines[2::2]:
     nums = list(map(int, line.split()))
     n = len(nums)
-    nums = [x for x in nums if x <= n]
-    trie_root = TrieNode()
-    for x in nums:
-        insert(prime_factors(x), trie_root)
-    result = 0
-    for i in range(1, n + 1):
-        result = max(result, amount_in_trie(prime_factors(i), trie_root))
-    print(result)
+
+    @lru_cache(maxsize=None)
+    def recur(i, cl):
+        if i == n:
+            return 0
+        a = 1 + recur(i+1, lcm(cl, nums[i])) if lcm(cl, nums[i]) <= n else 0
+        b = recur(i+1, cl)
+        return max(a, b)
+    print(recur(0, 1))
     
         
--- codeforces/886/G.py
+++ codeforces/886/G.py
@@ -1,70 +1,32 @@
 import sys
 import math
+from collections import defaultdict
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-def nPr(n, r):
-    return math.factorial(n) // math.factorial(n-r)
+def count_pairs_from_dict(d):
+    return sum(val * (val - 1) // 2 for val in d.values())
 
-def nCr(n,r):
-    return math.factorial(n) // (math.factorial(r)*(math.factorial(n-r)))
-
-def is_slope_1(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-    if x2 - x1 == 0:  # Avoid division by zero
-        return False
-    slope = (y2 - y1) / (x2 - x1)
-    return slope == 1
-
-def is_slope_minus1(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-    if x2 - x1 == 0:  # Avoid division by zero
-        return False
-    slope = (y2 - y1) / (x2 - x1)
-    return slope == -1
-
-ii = 1
-while ii < len(lines):
-    points = []
-    n = int(lines[ii])
-    ii+=1
-    x_coords = {}
-    y_coords = {}
-    for _ in range(n):
-        x, y = map(int, lines[ii].split())
-        points.append((x,y))
-        x_coords[x] = x_coords.get(x, 0) + 1
-        y_coords[y] = y_coords.get(y, 0) + 1
-        ii+=1
+def solve_morning_star(points):
+    x_coords = defaultdict(int)
+    y_coords = defaultdict(int)
+    slope1 = defaultdict(int)
+    slope_minus1 = defaultdict(int)
+    
+    for x, y in points:
+        x_coords[x] += 1
+        y_coords[y] += 1
+        slope1[x - y] += 1
+        slope_minus1[x + y] += 1
     result = 0
-    for d in [x_coords, y_coords]:
-        for val in d.values():
-            if val < 2:
-                continue
-            result += nCr(val, 2)
-    points.sort(key=lambda x: x[1])
-    i = 0
-    while i < len(points):
-        j = i + 1
-        while j < len(points):
-            # Check if the slope between points[i] and points[j] is 1
-            if not is_slope_1(points[i], points[j]):
-                break
-            j+=1
-        if j - i >= 2:
-            result += nCr(j-i, 2)
-        i = j
-    points.sort(key=lambda x: -x[1])
-    i = 0
-    while i < len(points):
-        j = i + 1
-        while j < len(points):
-            # Check if the slope between points[i] and points[j] is 1
-            if not is_slope_minus1(points[i], points[j]):
-                break
-            j+=1
-        if j - i >= 2:
-            result += nCr(j-i, 2)
-        i = j
-    print(result * 2)
+    for d in [x_coords, y_coords, slope1, slope_minus1]:
+        result += count_pairs_from_dict(d)
+    
+    return result
+ii = 1
+t = int(lines[0])
+for _ in range(t):
+    n = int(lines[ii])
+    ii += 1
+    points = [tuple(map(int, lines[ii + i].split())) for i in range(n)]
+    ii += n
+    print(solve_morning_star(points) * 2)
