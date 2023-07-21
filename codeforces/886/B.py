ad0103ecc54f0868892601e5b1f008af07dba758
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-07-21 20:24:03 +0200

    solve B

--- codeforces/886/B.py
+++ codeforces/886/B.py
@@ -1,2 +1,14 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+ii = 1
+while ii < len(lines):
+    n = int(lines[ii])
+    ii += 1
+    best = [-1, -1]
+    for i in range(n):
+        a, b = map(int, lines[ii].split())
+        ii+=1
+        if a > 10: continue
+        best = max(best, [b, i])
+    print(best[1] + 1)
