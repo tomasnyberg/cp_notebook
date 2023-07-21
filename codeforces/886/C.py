5584e32cc7e0af676199068c6d61ece57921d427
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-07-21 20:26:23 +0200

    solve C

--- codeforces/886/C.py
+++ codeforces/886/C.py
@@ -1,2 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+
+ii = 1
+while ii < len(lines):
+    grid = []
+    for _ in range(8):
+        grid.append(lines[ii])
+        ii += 1
+    word = ""
+    for r in range(8):
+        for c in range(8):
+            if grid[r][c] != ".":
+                word += grid[r][c]
+    print(word)
