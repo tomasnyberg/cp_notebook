04cc0a790b65dd660286e5ccbab9d4be29b70b17
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-07-23 15:54:51 +0200

    solve B

--- codeforces/887/B.py
+++ codeforces/887/B.py
@@ -1,3 +1,21 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[1:]:
+    n, k = map(int, line.split())
+    if k > 46:
+        print(0)
+        continue
+    count = 0
+    for x in range((n // 2) + 1):
+        y = n - x
+        valid = True
+        for _ in range(k - 2):
+            x, y = y - x, x
+            if x < 0:
+                valid = False
+                break
+        if valid:
+            count += 1
+    print(count)
