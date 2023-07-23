4f42bde9a48879228b6f6b7e057473361ba00851
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-07-23 11:33:52 +0200

    add anti hash table hack

--- codeforces/886/F.py
+++ codeforces/886/F.py
@@ -4,10 +4,20 @@ from bisect import bisect_left
 from functools import lru_cache
 from collections import Counter
 
+from random import getrandbits
+
+RANDOM = getrandbits(32)
+
+class Wrapper(int):
+    def __init__(self, x):
+        int.__init__(x)
+    def __hash__(self):
+        return super(Wrapper, self).__hash__() ^ RANDOM
+
 for line in lines[2::2]:
     nums = list(map(int, line.split()))
     n = len(nums)
-    nums = [x for x in nums if x <= len(nums)]
+    nums = [Wrapper(x) for x in nums if x <= len(nums)]
     counts = dict(Counter(nums))
     scores = [0 for _ in range(n + 1)]
     for k, v in counts.items():
