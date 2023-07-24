ff5a746dc8a163a8a71532962095162f87bcfb4a
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-07-24 23:56:05 +0200

    very close on E

--- codeforces/888/E.py
+++ codeforces/888/E.py
@@ -2,6 +2,32 @@ import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 import heapq
 # TODO Remember to add int wrapping if using dict
+# lines = """3
+# 6 3
+# 5 5 4 5 2 2
+# 3 4 5
+# 2 2 5
+# 1 5
+# 3 4 1 6
+# 4 2 6 1 5
+# 0 
+# 0 
+# 6 2
+# 1 4 4 1 5 2
+# 3 6
+# 4 6 3 4 5
+# 4 6 5 3 4
+# 0 
+# 1 5
+# 1 6
+# 0 
+# 2 1
+# 4 3
+# 1
+# 0 
+# 1 1
+# """.splitlines()
+
 
 results = []
 idx = 0
@@ -19,12 +45,24 @@ for _ in range(t):
         mix_recipes.append(list(map(int, lines[idx].split())))
         idx += 1
     mixings = {i: mix_recipes[i][1:] for i in range(n) if mix_recipes[i][0] > 0}
+    for k, v in mixings.items():
+        for i in range(len(v)):
+            v[i] -=1
     min_costs = costs[:]
     for potion in supplies:
         min_costs[potion-1] = 0
-    queue = [(cost, idx) for idx, cost in enumerate(min_costs)]
+    queue = []
+    for i in range(len(min_costs)):
+        if min_costs[i] == 0:
+            continue
+        mix = mixings.get(i, [])
+        if mix:
+            min_costs[i] = min(min_costs[i], sum(min_costs[m] for m in mix))
+        queue.append((min_costs[i], i))
     heapq.heapify(queue)
-    visited = set()
+    visited = set(i for i in range(n) if min_costs[i] == 0)
+    # print("costs", min_costs)
+    # print("mixings", mixings)
     while queue:
         cost, potion = heapq.heappop(queue)
         if potion in visited:
@@ -32,8 +70,7 @@ for _ in range(t):
         visited.add(potion)
         mix = mixings.get(potion, [])
         if mix:
-            mix_cost = sum(min_costs[m-1] for m in mix)
+            mix_cost = sum(min_costs[m] for m in mix)
             if mix_cost < min_costs[potion]:
                 min_costs[potion] = mix_cost
-                heapq.heappush(queue, (mix_cost, potion))
     print(*min_costs)
