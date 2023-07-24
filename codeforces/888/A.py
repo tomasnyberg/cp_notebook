5baad855695a39c1a0e176e7d855d2664a931d9c
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-07-24 22:30:31 +0200

    solve A

--- codeforces/888/A.py
+++ codeforces/888/A.py
@@ -1,3 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for ii in range(1, len(lines), 2):
+    n, m, k, H = map(int, lines[ii].split())
+    a = list(map(int, lines[ii+1].split()))
+    result = 0
+    for p in a:
+        small, big = min(p, H), max(p, H)
+        for i in range(1, m):
+            if i*k + small == big:
+                result += 1
+                break
+    print(result)
