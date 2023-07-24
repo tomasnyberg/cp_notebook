a5ebbc4b1e200f2e962dc477f1cac9b5e5a36387
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-07-24 23:53:41 +0200

    protection in D

--- codeforces/888/D.py
+++ codeforces/888/D.py
@@ -3,6 +3,16 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 from collections import Counter
 # TODO Remember to add int wrapping if using dict
 
+from random import getrandbits
+
+RANDOM = getrandbits(32)
+
+class Wrapper(int):
+    def __init__(self, x):
+        int.__init__(x)
+    def __hash__(self):
+        return super(Wrapper, self).__hash__() ^ RANDOM
+
 def prefix_sum_to_array(prefix_sum):
     if not prefix_sum:
         return []
@@ -29,18 +39,28 @@ for line in lines[2::2]:
         arr = prefix_sum_to_array(nums)
         print("YES" if is_permutation_of_length(len(nums), arr) else "NO")
         continue
-    permset = set(list(range(1, n+1)))
+    permset = set([Wrapper(i) for i in list(range(1, n+1))])
     arr = prefix_sum_to_array(nums)
-    missing = list(permset - set(arr))
+    missing = list(permset - set([Wrapper(i) for i in arr]))
     notexpected = []
     seen = set()
     for x in arr:
-        if x in seen or x not in permset:
+        if Wrapper(x) in seen or Wrapper(x) not in permset:
             notexpected.append(x)
-        seen.add(x)
-    # print("original", nums)
-    # print("array", arr)
-    # print("missing", missing)
-    # print("unexpected", notexpected)
+        seen.add(Wrapper(x))
     print("YES" if len(missing) == 2 and len(notexpected) == 1 and missing[0] + missing[1] == notexpected[0] else "NO")
+    # permset = set(list(range(1, n+1)))
+    # arr = prefix_sum_to_array(nums)
+    # missing = list(permset - set(arr))
+    # notexpected = []
+    # seen = set()
+    # for x in arr:
+    #     if x in seen or x not in permset:
+    #         notexpected.append(x)
+    #     seen.add(x)
+    # # print("original", nums)
+    # # print("array", arr)
+    # # print("missing", missing)
+    # # print("unexpected", notexpected)
+    # print("YES" if len(missing) == 2 and len(notexpected) == 1 and missing[0] + missing[1] == notexpected[0] else "NO")
 
