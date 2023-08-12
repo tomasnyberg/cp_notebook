2bd796e4d44ebada29c78381f12d5af8afa5c62c
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-08-12 15:03:42 +0200

    solve C

--- codeforces/891/C.py
+++ codeforces/891/C.py
@@ -1,3 +1,18 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+from collections import Counter
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split()))
+    d = dict(Counter(nums))
+    items = list(d.items())
+    items.sort(key=lambda x: x[0], reverse=True)
+    # print(items)
+    result = [10**9]
+    for i in range(len(items)):
+        elem, count = items[i]
+        while count > 0:
+            count -= len(result)
+            result.append(elem)
+    print(*result)
