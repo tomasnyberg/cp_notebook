37acb23cee2c60d76436838e11d22d87ca5adb88
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-08-12 14:49:58 +0200

    solve A

--- codeforces/891/A.py
+++ codeforces/891/A.py
@@ -1,3 +1,11 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[2::2]:
+    xs = list(map(int, line.split()))
+    total = sum(xs)
+    if total % 2 == 1:
+        print("NO")
+    else:
+        print("YES")
