a1918bb669c1041024576d15e512f0a7076e3270
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-08-30 13:26:01 -0400

    solve B

--- codeforces/894/B.py
+++ codeforces/894/B.py
@@ -1,3 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split()))
+    result = [nums[0]]
+    for i in range(1, len(nums)):
+        if nums[i] >= nums[i-1]:
+            result.append(nums[i])
+        else:
+            result.append(1)
+            result.append(nums[i])
+    print(len(result))
+    print(*result)
