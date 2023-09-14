908f43fa9973c2581e603a6b422332f68524c0f8
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-09-13 20:50:58 -0400

    solve C

--- codeforces/895/C.py
+++ codeforces/895/C.py
@@ -1,3 +1,28 @@
 import sys
+import math
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+
+for line in lines[1:]:
+    a, b = map(int, line.split())
+    if b < 4:
+        print(-1)
+        continue
+    if a < 4:
+        a = 4
+    if b - a >= 1:
+        if a % 2 == 1:
+            a += 1
+        print(a-2, 2)
+        continue
+    found = False
+    for i in range(2, int(math.sqrt(a)) + 5):
+        if i >= b: break
+        if b % i == 0:
+            found = True
+            print(i, b - i)
+            break
+    if not found:
+        print(-1)
+
