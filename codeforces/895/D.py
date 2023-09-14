7c4ba13b8e605765c0239cdc8d1a7418177369b2
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-09-14 08:51:47 -0400

    working on d

--- codeforces/895/D.py
+++ codeforces/895/D.py
@@ -1,3 +1,12 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+def f(x):
+    return x*(x+1)//2
+for line in lines[1:]:
+    n, x, y = map(int, line.split())
+    
+    
+    
+    
