e1a7dd9cb1e0c2e2d0ed3a9c20aaa831c547b6d4
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-09-13 20:20:53 -0400

    solve B

--- codeforces/895/B.py
+++ codeforces/895/B.py
@@ -1,3 +1,28 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+ii = 1
+while ii < len(lines):
+    n = int(lines[ii])
+    traps = []
+    ii += 1
+    for _ in range(n):
+        traps.append(list(map(int, lines[ii].split())))
+        ii += 1
+    earliests = {}
+    for d, s in traps:
+        earliests[d] = min(earliests.get(d, 100000), s+(d-1))
+    def check(k):
+        time = k
+        for spot in range(k+1, 0, -1):
+            # print(spot, time)
+            if earliests.get(spot, 10**9) <= time:
+                return False
+            time += 1
+        return True
+    for k in range(1, 1000):
+        # print(k, check(k))
+        if not check(k):
+            print(k)
+            break
