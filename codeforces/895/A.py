20dd2b74af7c3b6fd99ac502e9a2397ac6814b74
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-09-13 20:04:55 -0400

    solve A

--- codeforces/895/A.py
+++ codeforces/895/A.py
@@ -1,3 +1,14 @@
 import sys
+import math
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[1:]:
+    a, b, c = map(int, line.split())
+    a, b = max(a, b), min(a,b)
+    result = 0
+    while a > b:
+        a -= c
+        b += c
+        result +=1
+    print(result)
