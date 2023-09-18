a902c5e196fbcda0589941d5559f9506b05a77e5
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-09-17 21:26:26 -0400

    working on 895D

--- codeforces/895/D.py
+++ codeforces/895/D.py
@@ -2,11 +2,13 @@ import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
 
+
 def f(x):
     return x*(x+1)//2
+
+
 for line in lines[1:]:
     n, x, y = map(int, line.split())
-    
-    
-    
-    
+    common = n // (x*y)
+    xs = n // x - common
+    ys = n // y - common
