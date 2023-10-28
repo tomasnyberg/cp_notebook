182e2f3b10037d0c51fd1e04376fc4e5684a100f
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-10-28 15:50:45 -0400

    solve D

--- codeforces/898/D.py
+++ codeforces/898/D.py
@@ -1,3 +1,19 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for ii in range(1, len(lines), 2):
+    n, k = map(int, lines[ii].split())
+    s = lines[ii+1]
+    result = 0
+    good_for = 0
+    for i, c in enumerate(s):
+        if good_for > 0:
+            good_for -= 1
+            continue
+        if c == 'B':
+            result += 1
+            good_for = k-1
+    print(result)
+
+    
