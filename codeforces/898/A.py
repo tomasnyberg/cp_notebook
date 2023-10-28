7a0583cd3c6c53d7f64f3c2b55e1fd0da659c772
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-10-28 15:43:23 -0400

    solve A

--- codeforces/898/A.py
+++ codeforces/898/A.py
@@ -1,3 +1,11 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+answer = "abc"
+for line in lines[1:]:
+    count = 0
+    for x, y in zip(line, answer):
+        if x != y:
+            count += 1
+    print("YES") if count < 3 else print("NO")
