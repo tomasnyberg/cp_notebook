6d25d08f1157145f6d610343f99d88b35cb45c90
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-10-28 17:16:27 -0400

    solve G

--- codeforces/898/G.py
+++ codeforces/898/G.py
@@ -11,17 +11,26 @@ for line in lines[1:]:
     result = 0
     # print(''.join(s))
     indices = [i for i in range(len(s)) if s[i] == 'B']
-    @lru_cache(None)
-    def dp(i, prev_taken):
-        if i == len(indices):
-            return 0
-        # Take to the right
-        if prev_taken:
-            if i == len(indices) - 1:
-                return len(s) - indices[i] - 1
-            return indices[i+1] - indices[i] - 1 + dp(i+1, True)
-        previous = indices[i-1] if i > 0 else -1
-        take_previous = indices[i] - previous - 1 + dp(i+1, False)
-        take_next = (len(s) - indices[i] if i == len(indices) - 1 else indices[i+1] - indices[i]) - 1 + dp(i+1, True)
-        return max(take_previous, take_next)
-    print(dp(0, False))
+    def iterative_dp(indices, s):
+        n = len(indices)
+        # dp[i][j] represents the maximum value when considering the first i elements of indices
+        # and j indicates whether the previous element was taken (j=0 means not taken, j=1 means taken)
+        dp = [[0 for _ in range(2)] for _ in range(n + 1)]
+
+        for i in range(n - 1, -1, -1):
+            for prev_taken in [True, False]:
+                j = 1 if prev_taken else 0
+                if prev_taken:
+                    if i == n - 1:
+                        dp[i][j] = len(s) - indices[i] - 1
+                    else:
+                        dp[i][j] = indices[i + 1] - indices[i] - 1 + dp[i + 1][1]
+                else:
+                    previous = indices[i - 1] if i > 0 else -1
+                    take_previous = indices[i] - previous - 1 + dp[i + 1][0]
+                    take_next = (len(s) - indices[i] if i == n - 1 else indices[i + 1] - indices[i]) - 1 + dp[i + 1][1]
+                    dp[i][j] = max(take_previous, take_next)
+
+        return dp[0][0]
+
+    print(iterative_dp(indices, s))
