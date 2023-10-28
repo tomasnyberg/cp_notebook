31f4e7abb41d94e8bb493f0876886b12cdcb3977
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-10-28 15:45:15 -0400

    solve B

--- codeforces/898/B.py
+++ codeforces/898/B.py
@@ -1,3 +1,12 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split()))
+    nums.sort()
+    nums[0] += 1
+    result = 1
+    for x in nums:
+        result *= x
+    print(result)
