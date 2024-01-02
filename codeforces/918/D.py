de7e4b6b91a1280733ffca9343416403285a7825
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-01-02 16:03:32 +0100

    solve D

--- codeforces/918/D.py
+++ codeforces/918/D.py
@@ -1,3 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[2::2]:
+    s = list(line)
+    V = ['a', 'e']
+    C = ['b', 'c', 'd']
+    result = []
+    while s:
+        rng = 3 if s[-1] in C else 2
+        for _ in range(rng):
+            result.append(s.pop())
+        result.append('.')
+    print(''.join(result[::-1][1:]))
