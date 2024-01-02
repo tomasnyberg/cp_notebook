5e5892cbefefe43a392cd7b08f589252a886e366
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-01-02 15:53:38 +0100

    solve B

--- codeforces/918/B.py
+++ codeforces/918/B.py
@@ -1,3 +1,11 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for ii in range(1, len(lines), 3):
+    matrix = [lines[ii], lines[ii+1], lines[ii+2]]
+    for line in matrix:
+        if line.count('?') == 1:
+            expected = set(['A', 'B', 'C'])
+            print((expected - set(line)).pop())
+            
