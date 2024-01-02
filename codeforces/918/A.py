d162daad0e86d13573afab4c97ed73ac83b34907
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-01-02 15:51:33 +0100

    solve A

--- codeforces/918/A.py
+++ codeforces/918/A.py
@@ -1,3 +1,12 @@
 import sys
+from collections import Counter
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[1:]:
+    nums = list(map(int, line.split()))
+    counts = Counter(nums)
+    for k, v in counts.items():
+        if v == 1:
+            print(k)
+            break
