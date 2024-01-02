f10e5fb6f9b656b5137a37bba1d6e96074153acc
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-01-02 15:55:28 +0100

    solve C

--- codeforces/918/C.py
+++ codeforces/918/C.py
@@ -1,3 +1,9 @@
 import sys
+import math
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split()))
+    total = sum(nums)
+    print("YES" if int(math.sqrt(total)) == math.sqrt(total) else "NO")
