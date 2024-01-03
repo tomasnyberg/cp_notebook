b27e319e65eb0df50b81227a685a260b53953423
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-01-03 13:16:27 +0100

    solve B

--- codeforces/916/B.py
+++ codeforces/916/B.py
@@ -1,3 +1,11 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[1:]:
+    n, k = map(int, line.split())
+    for i in range(1, k+1):
+        print(i, end=' ')
+    for i in range(n, k, -1):
+        print(i, end=' ')
+    print()
