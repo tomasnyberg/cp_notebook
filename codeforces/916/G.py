9562c84767c409f961dfbc9258fdc4ad87b3546d
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-01-03 17:30:42 +0100

    shorten down G code a bit

--- codeforces/916/G.py
+++ codeforces/916/G.py
@@ -20,14 +20,15 @@ for ii in range(1, len(lines), 2):
             allways[nums[i]].add(x)
     visited = set()
     curr = []
-    def dfs(node):
+    def dfs(node, append=True):
         if node in visited:
             return
         visited.add(node)
-        curr.append(node)
-        for neighbor in allways[node]:
+        if append:
+            curr.append(node)
+        for neighbor in (allways[node] if append else inbetween[node]):
             if neighbor not in visited:
-                dfs(neighbor)
+                dfs(neighbor, append)
     counts = 0
     groups = []
     for i in range(1, n+1):
@@ -39,17 +40,10 @@ for ii in range(1, len(lines), 2):
     result = 1
     MOD = 998244353
     visited = set()
-    def dfs2(node):
-        if node in visited:
-            return
-        visited.add(node)
-        for neighbor in inbetween[node]:
-            if neighbor not in visited:
-                dfs2(neighbor)
     for group in groups:
         count = 0
         for node in group:
-            dfs2(node)
+            dfs(node, False)
             if len(visited) == len(group):
                 count += 1
             visited = set()
