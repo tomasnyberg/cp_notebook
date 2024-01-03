e46a5bf45d0cd0056b1a89f9e31be22e3c576fc4
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-01-03 13:26:08 +0100

    solve D

--- codeforces/916/D.py
+++ codeforces/916/D.py
@@ -1,3 +1,21 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for ii in range(1, len(lines), 4):
+    a = list(enumerate(map(int, lines[ii+1].split())))
+    b = list(enumerate(map(int, lines[ii+2].split())))
+    c = list(enumerate(map(int, lines[ii+3].split())))
+    a.sort(key=lambda x: x[1])
+    b.sort(key=lambda x: x[1])
+    c.sort(key=lambda x: x[1])
+    a = a[-3:]
+    b = b[-3:]
+    c = c[-3:]
+    result = 0
+    for i, x in a:
+        for j, y in b:
+            for k, z in c:
+                if i != j and j != k and i != k:
+                    result = max(result, x+y+z)
+    print(result)
