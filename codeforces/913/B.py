0b5827d4f37cebbd09ab4d930b76cf47dc4d6887
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-01-05 19:48:15 +0100

    solve B

--- codeforces/913/B.py
+++ codeforces/913/B.py
@@ -1,3 +1,26 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[1:]:
+    removed = set()
+    small = []
+    big = []
+    result = []
+    j = 0
+    for i, c in enumerate(line):
+        if c == 'b':
+            if small:
+                removed.add(small.pop())
+        elif c == 'B':
+            if big:
+                removed.add(big.pop())
+        else:
+            if c.isupper():
+                big.append(j)
+            else:
+                small.append(j)
+            result.append(c)
+            j+=1
+    print(''.join([c for i, c in enumerate(result) if i not in removed]))
+        
