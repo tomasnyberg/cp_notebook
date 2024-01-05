3cf6ab5b8fd3191496cc01ab0839905ebfd212ee
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-01-05 21:32:37 +0100

    upsovle C :(

--- codeforces/913/C.py
+++ codeforces/913/C.py
@@ -1,3 +1,14 @@
 import sys
+from functools import lru_cache
+from collections import Counter
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[2::2]:
+    counts = dict(Counter(line))
+    for c in counts:
+        if counts[c] > len(line) // 2:
+            print(counts[c] - (len(line) - counts[c]))
+            break
+    else:
+        print(0 if len(line) % 2 == 0 else 1)
