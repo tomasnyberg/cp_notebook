e5a9f720fe8503c20ebd4f04ce7a4de82e563697
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-01-05 19:42:22 +0100

    solve A2

--- codeforces/913/A.py
+++ codeforces/913/A.py
@@ -1,3 +1,13 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[1:]:
+    c, num = list(line)
+    num = int(num)
+    for i in range(1, 9):
+        if i == num: continue
+        print(c + str(i))
+    for col in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']:
+        if c == col: continue
+        print(col + str(num))
