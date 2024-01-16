ee546a9fac186aca9c8c39358534044caa6ee4db
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-01-16 18:41:23 +0100

    solve B

--- codeforces/920/B.py
+++ codeforces/920/B.py
@@ -1,3 +1,25 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for ii in range(1, len(lines), 3):
+    have = lines[ii+1]
+    want = lines[ii+2]
+    needed = 0
+    cats = 0
+    for x, y in zip(have, want):
+        if x == y:
+            continue
+        if x == '0':
+            needed += 1
+        else:
+            cats += 1
+    result = 0
+    if cats > needed:
+        result += cats - needed
+    else:
+        temp = needed - cats
+        result += needed - cats
+        needed -= temp
+    result += needed
+    print(result)
