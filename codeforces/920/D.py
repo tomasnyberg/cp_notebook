ead768e3e5edfe666a32c2006d8886d3a3444fd3
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-01-16 18:52:08 +0100

    solve D

--- codeforces/920/D.py
+++ codeforces/920/D.py
@@ -1,3 +1,29 @@
 import sys
+from collections import deque
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for ii in range(1, len(lines), 3):
+    xs = list(map(int, lines[ii+1].split()))
+    ys = list(map(int, lines[ii+2].split()))
+    xs.sort()
+    ys.sort()
+    xs = deque(xs)
+    ys = deque(ys)
+    # print(xs)
+    # print(ys)
+    result = 0
+    while xs:
+        bigdiff = xs[-1] - ys[0]
+        smalldiff = ys[-1] - xs[0]
+        if bigdiff > smalldiff:
+            result += bigdiff
+            xs.pop()
+            ys.popleft()
+        else:
+            result += smalldiff
+            xs.popleft()
+            ys.pop()
+    print(result)
+
+
