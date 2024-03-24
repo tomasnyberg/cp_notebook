73ceea425562e6ad543fe83444877b7148d45189
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-03-24 15:29:29 +0100

    solve A

--- codeforces/935/A.py
+++ codeforces/935/A.py
@@ -1,3 +1,12 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+import math
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[1:]:
+    a, b, c = map(int, line.split())
+    missing = (3 - (b % 3)) % 3
+    if c < missing:
+        print(-1)
+        continue
+    print(a + math.ceil((b+c)/3))
