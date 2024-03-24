7f46a0d5eb1c484c21364ec24579c0bb18b1ddd8
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-03-24 16:28:33 +0100

    solve D

--- codeforces/935/D.py
+++ codeforces/935/D.py
@@ -1,3 +1,23 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+
+for ii in range(1, len(lines), 3):
+    n, m = map(int, lines[ii].split())
+    a = list(map(int, lines[ii+1].split()))
+    b = list(map(int, lines[ii+2].split()))
+    smallest = [10**18, -1]
+    taken = 0
+    extra = 0
+    for i in range(n-1,-1,-1):
+        taken += min(a[i], b[i])
+        if (i+1) <= m:
+            smallest = min(smallest, [a[i] + extra, i])
+            extra += b[i]
+    idx = smallest[1]
+    for i in range(idx):
+        taken -= min(a[i], b[i])
+    # print(taken, smallest)
+    print(taken - min(a[idx], b[idx]) + a[idx])
+
