90b1b19831af359c3c08808eb3187d41a113acb9
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-03-24 19:09:21 +0100

    remove stupid stuff from E solution

--- codeforces/935/E.py
+++ codeforces/935/E.py
@@ -1,21 +1,14 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
-# TODO Remember to add int wrapping if using dict
 
 for ii in range(1, len(lines), 2):
     n, x = map(int, lines[ii].split())
     a = list(map(int, lines[ii+1].split()))
-    a = [10**9] + a
     idx = a.index(x)
-    l = 1
-    r = n+1
-    moves = []
-    notseen = set([i for i in range(1, n+1)])
-    while True:
-        if r - l == 1:
-            break
+    l = 0
+    r = n
+    while r-l != 1:
         m = (l + r) // 2
-        notseen.discard(m)
         if a[m] <= x:
             l = m
         else:
@@ -24,7 +17,4 @@ for ii in range(1, len(lines), 2):
         print(0)
     else:
         print(1)
-        print(l, idx)
-    
-
-        
+        print(l + 1, idx + 1)
