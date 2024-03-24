d31ce2cf2b42ebd4fafb99eac20b74a1b91ea4fc
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-03-24 15:38:39 +0100

    solve B

--- codeforces/935/B.py
+++ codeforces/935/B.py
@@ -1,3 +1,8 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[1:]:
+    a, b, m = map(int, line.split())
+    a, b = min(a, b), max(a, b)
+    print(m // a + m//b + 2)
