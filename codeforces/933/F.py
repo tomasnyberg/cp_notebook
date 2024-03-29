633070cb38abd827b1592951364b7fb2a8d49e2b
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-03-29 16:58:02 +0100

    close on F.py just wrong on one sample

---
+++ codeforces/933/F.py
@@ -0,0 +1,44 @@
+import sys
+from bisect import bisect_right
+lines = list(map(str.strip, sys.stdin.readlines()))
+# lines = open('./codeforces/933/in').read().strip().split('\n')
+# TODO Remember to add int wrapping if using dict
+
+for ii in range(1, len(lines), 4):
+    n, m, k = map(int, lines[ii].split())
+    a = list(map(int, lines[ii + 1].split()))
+    b = list(map(int, lines[ii + 2].split()))
+    c = list(map(int, lines[ii + 3].split()))
+    maxd = [0,-1]
+    for i in range(1, len(a)):
+        if a[i] - a[i-1] > maxd[0]:
+            maxd = [a[i] - a[i-1], i]
+    b = sorted(set(b))
+    c = list(sorted(set(c)))
+    low = 0
+    high = maxd[0]
+    lb = a[maxd[1] - 1]
+    rb = a[maxd[1]]
+    def check(mid):
+        for x in b:
+            first_bound = lb + mid
+            second_bound = rb - mid
+            left = min(first_bound, second_bound)
+            right = max(first_bound, second_bound)
+            target = left - x
+            idx = bisect_right(c, target)
+            if idx < len(c) and c[idx] + x <= right:
+                return True
+        return False
+    while low < high:
+        mid = (low + high) // 2
+        if check(mid):
+            high = mid
+        else:
+            low = mid + 1
+    a.append(a[maxd[1]-1] + low)
+    a.sort()
+    maxd = 0
+    for i in range(1, len(a)):
+        maxd = max(maxd, a[i] - a[i-1])
+    print(maxd)
