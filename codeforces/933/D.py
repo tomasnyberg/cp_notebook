19951708224cf7664adb08ea503e1d00bdea7ad4
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-03-29 15:56:12 +0100

    solve D

--- codeforces/933/D.py
+++ codeforces/933/D.py
@@ -1,3 +1,30 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+# 0 == clockwise
+# 1 == counter-clockwise
+# 2 == either
+def possible(n, players, dist, dir):
+    possible = set()
+    for start in players:
+        if dir in [0,2]:
+            possible.add((start + dist) % n)
+        if dir in [1,2]:
+            possible.add((start - dist) % n)
+    return possible
+
+ii = 1
+while ii < len(lines):
+    n, m, x = map(int, lines[ii].split())
+    x-=1
+    ii += 1
+    players = set([x])
+    for _ in range(m):
+        dist, dir = lines[ii].split()
+        dir = int(dir) if dir != '?' else 2
+        players = possible(n, players, int(dist), dir)
+        ii+=1
+    print(len(players))
+    print(*list(map(lambda x: x+1, sorted(players))))
+
