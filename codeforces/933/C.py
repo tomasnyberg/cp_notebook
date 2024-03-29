0193dda233d7c721d505433cc4a015744eccd1ac
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-03-29 15:34:59 +0100

    solve C

--- codeforces/933/C.py
+++ codeforces/933/C.py
@@ -1,3 +1,19 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for s in lines[2::2]:
+    result = 0
+    i = 0
+    while i < len(s):
+        if s[i:i+5] == "mapie":
+            result += 1
+            i += 5
+        elif s[i:i+3] == "map" or s[i:i+3] == "pie":
+            result += 1
+            i += 3
+        else:
+            i+=1
+    print(result)
+        
+
