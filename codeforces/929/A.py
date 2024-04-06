5ff6f7fae8b8d2fda8185668cb715aa7c8121866
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-04-06 08:56:21 +0200

    solve A

--- codeforces/929/A.py
+++ codeforces/929/A.py
@@ -1,3 +1,8 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split()))
+    nums = [-x if x < 0 else x for x in nums]
+    print(sum(nums))
