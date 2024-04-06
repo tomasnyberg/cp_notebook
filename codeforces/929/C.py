ecbb198ec5657bfff9db6b026438472eecb537d6
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-04-06 09:09:50 +0200

    solve C

--- codeforces/929/C.py
+++ codeforces/929/C.py
@@ -1,3 +1,15 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[1:]:
+    a,b,l = map(int, line.split())
+    result = set()
+    for x in range(21):
+        for y in range(21):
+            if a**x * b**y > l:
+                break
+            if l % (a**x * b**y) == 0:
+                result.add(l // (a**x * b**y))
+    print(len(result))
+
