621dcdd761aaf40190a3e49b27aaa65de03df1cb
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-05-04 10:46:15 +0200

    solve A

--- codeforces/938/A.py
+++ codeforces/938/A.py
@@ -1,3 +1,10 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[1:]:
+    n, a, b = map(int, line.split())
+    if b / 2 < a:
+        print(n//2*b + n%2*a)
+    else:
+        print(n*a)
