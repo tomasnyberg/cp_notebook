9686d3153ce696d360eac87ca1e577b80defd4a2
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-05-04 11:22:18 +0200

    add anti dict hack (zzzz)

--- codeforces/938/D.py
+++ codeforces/938/D.py
@@ -2,18 +2,29 @@ import sys
 from collections import Counter
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+from random import getrandbits
+
+RANDOM = getrandbits(32)
+
+class Wrapper(int):
+    def __init__(self, x):
+        int.__init__(x)
+    def __hash__(self):
+        return super(Wrapper, self).__hash__() ^ RANDOM
+
 
 for ii in range(1, len(lines), 3):
     n, m, k = map(int, lines[ii].split())
     a = list(map(int, lines[ii+1].split()))
     b = list(map(int, lines[ii+2].split()))
+    a = [Wrapper(x) for x in a]
+    b = [Wrapper(x) for x in b]
     targets = dict(Counter(b))
     negative = 0
     left = 0
     right = 0
     result = 0
     while right < len(a):
-        # print(targets, left, right)
         if a[right] in targets:
             targets[a[right]] -= 1
             if targets[a[right]] >= 0:
