c9d8c0a3c68144a948d3bdc3d1c48938f9d17727
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-05-18 11:28:57 +0200

    solve G

--- codeforces/943/G.py
+++ codeforces/943/G.py
@@ -2,22 +2,34 @@ import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
 
+def z_function(target, s):
+    prefixed = target + s
+    z = [0] * len(prefixed)
+    left = right = 0
+    for i in range(1, len(prefixed)):
+        if i < right:
+            z[i] = min(right - i, z[i-left])
+        while i + z[i] < len(prefixed) and prefixed[i + z[i]] == prefixed[z[i]]:
+            z[i] += 1
+        if i + z[i] > right:
+            left = i
+            right = i + z[i]
+    return z[len(target):]
 
 for ii in range(1, len(lines), 2):
     n, k, _ = map(int, lines[ii].split())
     s = lines[ii+1]
     def check(mid):
         target = s[:mid]
-        curr = 0
+        Z = z_function(target, s)
+        i = 0
         count = 0
-        for c in s:
-            if c == target[curr]:
-                curr += 1
-                if curr == mid:
-                    count += 1
-                    curr = 0
+        while i < n:
+            if Z[i] >= mid:
+                count += 1
+                i+=mid
             else:
-                curr = 0
+                i+=1
         return count >= k
     low = 1
     high = n + 1
