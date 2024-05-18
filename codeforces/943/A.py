9ed26e398c0e96f544f058d2bb068d2d10dcd107
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-05-18 08:38:24 +0200

    solve A

--- codeforces/943/A.py
+++ codeforces/943/A.py
@@ -1,3 +1,11 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+from math import gcd
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[1:]:
+    x = int(line)
+    result = [-1,-1]
+    for y in range(1, x):
+        result = max(result, [gcd(x, y) + y, y])
+    print(result[1])
