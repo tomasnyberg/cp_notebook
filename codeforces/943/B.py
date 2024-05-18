83bcc889264cd9f366023c37d709dea38aab8fc7
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-05-18 08:40:17 +0200

    solve B

--- codeforces/943/B.py
+++ codeforces/943/B.py
@@ -1,3 +1,14 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for ii in range(1, len(lines), 3):
+    a = lines[ii+1]
+    b = lines[ii+2]
+    i = 0
+    for c in b:
+        if c == a[i]:
+            i += 1
+        if i == len(a):
+            break
+    print(i)
