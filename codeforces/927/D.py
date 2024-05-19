70c6d701ca2c362d94b8d8d6bcb48b3e6cc6f94f
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-05-19 15:25:16 +0200

    solve D (finally....)

--- codeforces/927/D.py
+++ codeforces/927/D.py
@@ -16,48 +16,26 @@ for ii in range(1, len(lines), 3):
             return False
         return a[0] > b[0]
     cards = lines[ii+2].split()
-    def find_soln(preds):
-        curr = (1 << n) - 1
-        printed = 0
-        to_print = []
-        while curr != 0:
-            for bit in range(n):
-                if (1 << bit & curr) and not (1 << bit & preds[curr]):
-                    to_print.append(cards[bit])
-                    curr = preds[curr]
-                    if len(to_print) == 2:
-                        print(" ".join(to_print[::-1]))
-                        to_print = []
-                    
-    def find_played(state_a, state_b):
-        for bit in range(n):
-            if (1 << bit & state_a) and not (1 << bit & state_b):
-                return cards[bit]
-        raise ValueError("No played card found")
-    seen = set()
-    preds = {}
-    dq = deque([0])
-    while dq:
-        state = dq.popleft()
-        if state in seen:
+    orig_cards = lines[ii+2].split()
+    cards = [(10**9 if c[1] == trump else 0, c[0], c[1], c) for c in cards]
+    cards.sort()
+    taken = [False]*n
+    played = []
+    for i in range(n):
+        if taken[i]:
             continue
-        if state == (1<<n)-1:
-            find_soln(preds)
-            break
-        seen.add(state)
-        to_beat = state.bit_count() % 2 == 1
-        for i in range(n):
-            if (state & (1<<i)) == 0:
-                other_state = state | (1<<i)
-                if other_state not in preds:
-                    if to_beat:
-                        prev_card = find_played(state, preds[state])
-                        if not beats(cards[i], prev_card):
-                            continue
-                    preds[other_state] = state
-                    dq.append(other_state)
+        for j in range(i+1, n):
+            if taken[j]:
+                continue
+            # print(cards[i][3], cards[j][3], beats(cards[j][3], cards[i][3]))
+            if beats(cards[j][3], cards[i][3]):
+                taken[i] = True
+                taken[j] = True
+                played.append((cards[i][3], cards[j][3]))
+                break
+    # print(taken)
+    if all(taken):
+        for xs in played:
+            print(*xs)
     else:
         print("IMPOSSIBLE")
-    # print(seen)
-    
-
