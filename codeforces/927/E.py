65895aba542fdbd647ceb8734817301e2af104d5
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-05-19 19:09:18 +0200

    upsolve E

--- codeforces/927/E.py
+++ codeforces/927/E.py
@@ -1,30 +1,24 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
-import random
-sys.set_int_max_str_digits(5*10**5)
 # TODO Remember to add int wrapping if using dict
 
-def test(n):
-    result = 0
-    for i in range(n, 0,-1):
-        result += 1
-        stred = list(str(i))
-        count = 0
-        while stred and stred[-1] == '0':
-            count += 1
-            stred.pop()
-        result += count
+def cum_sum(nums):
+    curr = 0
+    result = [0]*len(nums)
+    for idx, num in enumerate(nums):
+        curr += num
+        result[idx] = curr
     return result
 
 for line in lines[2::2]:
-    result = 0
-    to_add = 0
-    for c in line:
-        to_add *= 10
-        to_add += int(c)
-        result += to_add
-    print(result)
+    xs = list(map(int, line.lstrip('0')))
+    CS = cum_sum(xs)
+    result = [0]*(len(xs))
+    result[0] = xs[0]
+    for i in range(len(xs)-1,0,-1):
+        result[i] += CS[i]
+        result[i-1] += result[i] // 10
+        result[i] %= 10
+    print(''.join(map(str, result)).lstrip('0'))
 
 
-
-        
