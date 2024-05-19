174dab33124af9ab8270cad3b4bf5dcef03fad30
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-05-19 13:54:01 +0200

    solve A

--- codeforces/927/A.py
+++ codeforces/927/A.py
@@ -1,3 +1,13 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[2::2]:
+    score = 0
+    for i in range(len(line)):
+        if i+1 < len(line) and line[i] == '*' and line[i+1] == '*':
+            break
+        score += 1 if line[i] == '@' else 0
+    print(score)
+
+    
