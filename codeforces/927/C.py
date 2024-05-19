ecdff4ac5fda50de53a13c13bab138965127ec4e
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-05-19 14:11:01 +0200

    solve C

--- codeforces/927/C.py
+++ codeforces/927/C.py
@@ -1,3 +1,24 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
+from collections import deque
 # TODO Remember to add int wrapping if using dict
+
+for ii in range(1, len(lines), 3):
+    n, m = map(int, lines[ii].split())
+    a = list(map(int, lines[ii + 1].split()))
+    a = deque(a)
+    seq = lines[ii+2]
+    removals = []
+    for c in seq:
+        if c == 'L':
+            removed = a.popleft()
+        else:
+            removed = a.pop()
+        removals.append(removed)
+    result = []
+    prod = 1
+    for x in removals[::-1]:
+        prod *= x
+        prod %= m
+        result.append(prod)
+    print(' '.join(map(str, result[::-1])))
