8b2a26572a3c16848b61ce895a9cc526dbb4d278
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-05-19 14:01:01 +0200

    solve B

--- codeforces/927/B.py
+++ codeforces/927/B.py
@@ -1,3 +1,11 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[2::2]:
+    xs = list(map(int, line.split()))
+    curr = 0
+    for x in xs:
+        to_wait = x - curr % x
+        curr += to_wait
+    print(curr)
