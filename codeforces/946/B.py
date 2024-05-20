cbbab888fcadd3727f5b97f174700b643af54825
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-05-20 16:17:34 +0200

    solve B

--- codeforces/946/B.py
+++ codeforces/946/B.py
@@ -1,3 +1,10 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[2::2]:
+    alphabetical = list(sorted(set(line)))
+    indices = {c: i for i, c in enumerate(alphabetical)}
+    for c in line:
+        print(alphabetical[len(alphabetical) - indices[c] - 1], end='')
+    print()
