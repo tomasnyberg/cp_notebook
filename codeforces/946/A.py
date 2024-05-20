1e0154b206326c5293be45c69486e0c08e4b1d15
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-05-20 16:14:06 +0200

    solve A

--- codeforces/946/A.py
+++ codeforces/946/A.py
@@ -1,3 +1,13 @@
+import math
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[1:]:
+    x, y = map(int, line.split())
+    min_by_two = math.ceil(y/2)
+    extra_available = min_by_two * 7 + (4 if y % 2 == 1 else 0)
+    if extra_available >= x:
+        print(min_by_two)
+    else:
+        print(min_by_two + math.ceil((x - extra_available) / 15))
