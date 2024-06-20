982bd9400bcd7967c79bae82973ad7ec7df9743b
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-06-20 09:59:49 +0200

    solve B and C

--- codeforces/950/B.py
+++ codeforces/950/B.py
@@ -1,3 +1,22 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for ii in range(1, len(lines), 2):
+    n, f, k = map(int, lines[ii].split())
+    nums = list(map(int, lines[ii+1].split()))
+    fnum = nums[f-1]
+    precounts = nums.count(fnum)
+    nums.sort(reverse=True)
+    # print(nums)
+    nums = nums[k:]
+    # print(nums)
+    postcounts = nums.count(fnum)
+    # print("fnum", fnum)
+    # print("k", k, "precounts", precounts, "postcounts", postcounts)
+    if postcounts == 0:
+        print("YES")
+    elif postcounts == precounts:
+        print("NO")
+    else:
+        print("Maybe")
--- codeforces/950/C.py
+++ codeforces/950/C.py
@@ -1,3 +1,44 @@
+from random import getrandbits
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+
+RANDOM = getrandbits(32)
+
+
+class Wrapper(int):
+    def __init__(self, x):
+        int.__init__(x)
+
+    def __hash__(self):
+        return super(Wrapper, self).__hash__() ^ RANDOM
+
+
+for ii in range(1, len(lines), 5):
+    a = list(map(int, lines[ii+1].split()))
+    b = list(map(int, lines[ii+2].split()))
+    ds = list(map(int, lines[ii+4].split()))
+    for xs in [a, b, ds]:
+        for i, x in enumerate(xs):
+            xs[i] = Wrapper(x)
+    bset = set(b)
+    needed = {}
+    for i, (x, y) in enumerate(zip(a, b)):
+        if x != y:
+            if y not in needed:
+                needed[y] = []
+            needed[y].append(i)
+    bad = 0
+    for x in ds:
+        if x in needed:
+            a[needed[x].pop()] = x
+            if not needed[x]:
+                del needed[x]
+            bad = 0
+        elif x in bset:
+            bad = 0
+        else:
+            if not needed:
+                bad += 1
+    print("YES" if not needed and bad == 0 else "NO")
