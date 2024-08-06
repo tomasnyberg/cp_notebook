5edbe923fb3f8bb94244b645ec61cfa75f4b2e73
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-08-06 19:34:02 +0200

    solve B

--- codeforces/925/B.py
+++ codeforces/925/B.py
@@ -1,5 +1,23 @@
 import sys
 if sys.argv[-1] == '--debug':
-   sys.stdin = open('in')
+    sys.stdin = open('in')
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[2::2]:
+    nums = list(map(int, line.split()))
+    expected = sum(nums) // len(nums)
+    extra = 0
+    for num in nums:
+        if num < expected:
+            if expected - num > extra:
+                print("NO")
+                break
+            extra -= expected - num
+        else:
+            extra += num - expected
+    else:
+        if extra == 0:
+            print("YES")
+        else:
+            print("NO")
