cdb0fc7fbf92b8b4821f1a8b0e035b1779272fe4
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-08-31 13:54:03 +0200

    solve A

--- codeforces/966/A.py
+++ codeforces/966/A.py
@@ -3,3 +3,11 @@ if sys.argv[-1] == '--debug':
     sys.stdin = open('in')
 lines = list(map(str.strip, sys.stdin.readlines()))
 # TODO Remember to add int wrapping if using dict
+
+for line in lines[1:]:
+    n = line
+    remaining = n[2:]
+    if len(n) >= 3 and n[:2] == '10' and remaining[0] != '0' and int(remaining) > 1:
+        print("YES")
+    else:
+        print("NO")
