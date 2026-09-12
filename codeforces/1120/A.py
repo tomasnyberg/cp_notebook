1f8b83837ac1150e6347db25fdc8fbb1c0242c42
tomasnyberg <tomas.nyberg7335@gmail.com>
2026-09-12 15:46:28 +0100

    uuh actually solve A

--- codeforces/1120/A.py
+++ codeforces/1120/A.py
@@ -6,17 +6,8 @@ lines = list(map(str.strip, sys.stdin.readlines()))
 
 for line in lines[2::2]:
     nums = list(map(int, line.split(" ")))
-    while len(nums) > 1:
-        for turn in [1,0]:
-            if len(nums) == 1:
-                break
-            for i in range(1, len(nums)):
-                if nums[i]+nums[i-1] == 1:
-                    nums[i] = turn
-                    nums.pop(i-1)
-                    break
-            else:
-                nums.pop()
-    print("Bessie" if nums[0] == 1 else "Elsie")
+    ones = sum(nums)
+    zeroes = len(nums) - ones
+    print("Bessie" if ones>=zeroes else "Elsie")
 
 
